148e1b5c32e6da0eb61218960578fcd5fc65f0f9
Jannis Pohlmann <contact@jannispohlmann.de>
2018-04-30 10:18:40 +0200

    thegraph-standalone-node: Add Sentry integration and instructions

--- thegraph-standalone-node/src/main.rs
+++ thegraph-standalone-node/src/main.rs
@@ -1,3 +1,23 @@
+#[macro_use]
+extern crate sentry;
+
+use sentry::integrations::panic::register_panic_handler;
+use std::env;
+
 fn main() {
-    unimplemented!();
+    // Set up Sentry, with release tracking and panic handling;
+    // fall back to an empty URL, which will result in no errors being reported
+    let sentry_url = env::var_os("THEGRAPH_SENTRY_URL")
+        .or(Some("".into()))
+        .unwrap();
+    let _sentry = sentry::init((
+        sentry_url,
+        sentry::ClientOptions {
+            release: sentry_crate_release!(),
+            ..Default::default()
+        },
+    ));
+    register_panic_handler();
+
+    println!("Welcome to The Graph!");
 }
