afb20f7ac69df49a5bedf6b8e3ed9b404e96ff82
Jannis Pohlmann <contact@jannispohlmann.de>
2018-05-07 13:30:36 +0200

    thegraph/common: Add utility function to create a structured logger

---
+++ thegraph/src/common/util/log.rs
@@ -0,0 +1,12 @@
+use slog;
+use slog_term;
+use slog_async;
+
+use slog::Drain;
+
+pub fn logger() -> slog::Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    slog::Logger::root(drain, o!())
+}
--- thegraph/src/common/util/mod.rs
+++ thegraph/src/common/util/mod.rs
@@ -1,2 +1,5 @@
 /// Utilities for working with streams.
 pub mod stream;
+
+/// Logging facilities based on `slog`.
+pub mod log;
