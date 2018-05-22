ffc1f0634b31380ea1d5c3526bd8298db37d2467
Jannis Pohlmann <contact@jannispohlmann.de>
2018-05-22 23:17:42 +0200

    thegraph: Store GraphQL query variables in Query objects
    This requires some deserialization magic: when we receive a request,
    we'll need to deserialize a variable map into a HashMap<String,
    graphql_parser::query::Value> for query execution. Since Value is
    a remote type, we're using the mechanism described on
    https://serde.rs/remote-derive.html to deserialize JSON values
    into the different Value enum variants.

--- thegraph/src/data/query.rs
+++ thegraph/src/data/query.rs
@@ -2,18 +2,87 @@ use futures::sync::oneshot;
 use graphql_parser::Pos;
 use graphql_parser::query;
 use serde::ser::*;
+use std::cmp::PartialEq;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
+use std::ops::{Deref, DerefMut};
 use std::string::FromUtf8Error;
 
 use data::schema::Schema;
+
+#[derive(Deserialize)]
+#[serde(untagged, remote = "query::Value")]
+enum GraphQLValue {
+    String(String),
+}
+
+/// Variable value for a GraphQL query.
+#[derive(Debug, Deserialize)]
+pub struct QueryVariableValue(#[serde(with = "GraphQLValue")] query::Value);
+
+impl Deref for QueryVariableValue {
+    type Target = query::Value;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for QueryVariableValue {
+    fn deref_mut(&mut self) -> &mut query::Value {
+        &mut self.0
+    }
+}
+
+impl PartialEq for QueryVariableValue {
+    fn eq(&self, other: &QueryVariableValue) -> bool {
+        self.0 == other.0
+    }
+}
+
+impl<'a> From<&'a str> for QueryVariableValue {
+    fn from(s: &'a str) -> Self {
+        QueryVariableValue(query::Value::String(s.to_string()))
+    }
+}
+
+/// Variable values for a GraphQL query.
+#[derive(Debug, Deserialize)]
+pub struct QueryVariables(HashMap<String, QueryVariableValue>);
+
+impl QueryVariables {
+    pub fn new() -> Self {
+        QueryVariables(HashMap::new())
+    }
+}
+
+impl Deref for QueryVariables {
+    type Target = HashMap<String, QueryVariableValue>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for QueryVariables {
+    fn deref_mut(&mut self) -> &mut HashMap<String, QueryVariableValue> {
+        &mut self.0
+    }
+}
+
+impl PartialEq for QueryVariables {
+    fn eq(&self, other: &QueryVariables) -> bool {
+        self.0 == other.0
+    }
+}
 
 /// A GraphQL query as submitted by a client, either directly or through a subscription.
 #[derive(Debug)]
 pub struct Query {
     pub schema: Schema,
     pub document: query::Document,
+    pub variables: Option<QueryVariables>,
     pub result_sender: oneshot::Sender<QueryResult>,
 }
 
