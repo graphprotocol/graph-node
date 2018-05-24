87f8a6affaf0343418fe47b8c8f9fd712614ef00
Jannis Pohlmann <contact@jannispohlmann.de>
2018-05-24 23:39:54 +0200

    thegraph-core: Move StoreResolver into its own module

---
+++ thegraph-core/src/query/resolver.rs
@@ -0,0 +1,136 @@
+use graphql_parser::query as gqlq;
+use graphql_parser::schema as gqls;
+use slog;
+use std::collections::HashMap;
+use std::sync::Arc;
+
+use thegraph::components::store::*;
+use thegraph::prelude::Store;
+
+use super::execution;
+
+/// A resolver that fetches entities from a `Store`.
+#[derive(Clone)]
+pub struct StoreResolver {
+    logger: slog::Logger,
+    store: Arc<Store>,
+}
+
+impl StoreResolver {
+    pub fn new(logger: &slog::Logger, store: Arc<Store>) -> Self {
+        StoreResolver {
+            logger: logger.new(o!("component" => "StoreResolver")),
+            store,
+        }
+    }
+}
+
+impl execution::Resolver for StoreResolver {
+    fn resolve_entities(
+        &self,
+        _parent: &Option<gqlq::Value>,
+        entity: &String,
+        _arguments: HashMap<&gqlq::Name, gqlq::Value>,
+    ) -> gqlq::Value {
+        self.store
+            .find(StoreQuery {
+                entity: entity.to_owned(),
+                filters: vec![],
+                order_by: None,
+                order_direction: None,
+                range: None,
+            })
+            .map(|entities| {
+                gqlq::Value::List(
+                    entities
+                        .into_iter()
+                        .map(|e| e.into())
+                        .collect::<Vec<gqlq::Value>>(),
+                )
+            })
+            .unwrap_or(gqlq::Value::Null)
+    }
+
+    fn resolve_entity(
+        &self,
+        _parent: &Option<gqlq::Value>,
+        entity: &String,
+        _arguments: HashMap<&gqlq::Name, gqlq::Value>,
+    ) -> gqlq::Value {
+        self.store
+            .get(StoreKey {
+                entity: entity.to_owned(),
+                id: "1".to_string(),
+            })
+            .map(|entity| entity.into())
+            .unwrap_or(gqlq::Value::Null)
+    }
+
+    fn resolve_enum_value(
+        &self,
+        enum_type: &gqls::EnumType,
+        value: Option<&gqlq::Value>,
+    ) -> gqlq::Value {
+        match value {
+            Some(gqlq::Value::Enum(name)) => enum_type
+                .values
+                .iter()
+                .find(|enum_value| &enum_value.name == name)
+                .map_or(gqlq::Value::Null, |enum_value| {
+                    gqlq::Value::Enum(enum_value.name.clone())
+                }),
+            _ => gqlq::Value::Null,
+        }
+    }
+
+    fn resolve_scalar_value(
+        &self,
+        _scalar_type: &gqls::ScalarType,
+        value: Option<&gqlq::Value>,
+    ) -> gqlq::Value {
+        value
+            .map(|value| value.clone())
+            .unwrap_or(gqlq::Value::Null)
+    }
+
+    fn resolve_enum_values(
+        &self,
+        enum_type: &gqls::EnumType,
+        value: Option<&gqlq::Value>,
+    ) -> gqlq::Value {
+        match value {
+            Some(gqlq::Value::List(values)) => gqlq::Value::List(
+                values
+                    .iter()
+                    .map(|value| self.resolve_enum_value(enum_type, Some(value)))
+                    .collect::<Vec<gqlq::Value>>(),
+            ),
+            _ => gqlq::Value::Null,
+        }
+    }
+
+    fn resolve_scalar_values(
+        &self,
+        scalar_type: &gqls::ScalarType,
+        value: Option<&gqlq::Value>,
+    ) -> gqlq::Value {
+        match value {
+            Some(gqlq::Value::List(values)) => gqlq::Value::List(
+                values
+                    .iter()
+                    .map(|value| self.resolve_scalar_value(scalar_type, Some(value)))
+                    .collect::<Vec<gqlq::Value>>(),
+            ),
+            _ => gqlq::Value::Null,
+        }
+    }
+
+    fn resolve_abstract_type<'a>(
+        &self,
+        _schema: &'a gqls::Document,
+        _abstract_type: &gqls::TypeDefinition,
+        _object_value: &gqlq::Value,
+    ) -> Option<&'a gqls::ObjectType> {
+        None
+    }
+}
--- thegraph-core/src/query/runner.rs
+++ thegraph-core/src/query/runner.rs
@@ -1,138 +1,13 @@
 use futures::prelude::*;
 use futures::sync::mpsc::{channel, Receiver, Sender};
-use graphql_parser::query as gqlq;
-use graphql_parser::schema as gqls;
 use slog;
-use std::collections::HashMap;
 use std::sync::Arc;
 use tokio_core::reactor::Handle;
 
-use thegraph::components::store::*;
 use thegraph::prelude::{Query, QueryRunner as QueryRunnerTrait, Store};
 
 use super::execution;
-
-/// A resolver that fetches entities from a `Store`.
-#[derive(Clone)]
-struct StoreResolver {
-    store: Arc<Store>,
-}
-
-impl StoreResolver {
-    pub fn new(store: Arc<Store>) -> Self {
-        StoreResolver { store }
-    }
-}
-
-impl execution::Resolver for StoreResolver {
-    fn resolve_entities(
-        &self,
-        _parent: &Option<gqlq::Value>,
-        entity: &String,
-        _arguments: HashMap<&gqlq::Name, gqlq::Value>,
-    ) -> gqlq::Value {
-        self.store
-            .find(StoreQuery {
-                entity: entity.to_owned(),
-                filters: vec![],
-                order_by: None,
-                order_direction: None,
-                range: None,
-            })
-            .map(|entities| {
-                gqlq::Value::List(
-                    entities
-                        .into_iter()
-                        .map(|e| e.into())
-                        .collect::<Vec<gqlq::Value>>(),
-                )
-            })
-            .unwrap_or(gqlq::Value::Null)
-    }
-
-    fn resolve_entity(
-        &self,
-        _parent: &Option<gqlq::Value>,
-        entity: &String,
-        _arguments: HashMap<&gqlq::Name, gqlq::Value>,
-    ) -> gqlq::Value {
-        self.store
-            .get(StoreKey {
-                entity: entity.to_owned(),
-                id: "1".to_string(),
-            })
-            .map(|entity| entity.into())
-            .unwrap_or(gqlq::Value::Null)
-    }
-
-    fn resolve_enum_value(
-        &self,
-        enum_type: &gqls::EnumType,
-        value: Option<&gqlq::Value>,
-    ) -> gqlq::Value {
-        match value {
-            Some(gqlq::Value::Enum(name)) => enum_type
-                .values
-                .iter()
-                .find(|enum_value| &enum_value.name == name)
-                .map_or(gqlq::Value::Null, |enum_value| {
-                    gqlq::Value::Enum(enum_value.name.clone())
-                }),
-            _ => gqlq::Value::Null,
-        }
-    }
-
-    fn resolve_scalar_value(
-        &self,
-        _scalar_type: &gqls::ScalarType,
-        value: Option<&gqlq::Value>,
-    ) -> gqlq::Value {
-        value
-            .map(|value| value.clone())
-            .unwrap_or(gqlq::Value::Null)
-    }
-
-    fn resolve_enum_values(
-        &self,
-        enum_type: &gqls::EnumType,
-        value: Option<&gqlq::Value>,
-    ) -> gqlq::Value {
-        match value {
-            Some(gqlq::Value::List(values)) => gqlq::Value::List(
-                values
-                    .iter()
-                    .map(|value| self.resolve_enum_value(enum_type, Some(value)))
-                    .collect::<Vec<gqlq::Value>>(),
-            ),
-            _ => gqlq::Value::Null,
-        }
-    }
-
-    fn resolve_scalar_values(
-        &self,
-        scalar_type: &gqls::ScalarType,
-        value: Option<&gqlq::Value>,
-    ) -> gqlq::Value {
-        match value {
-            Some(gqlq::Value::List(values)) => gqlq::Value::List(
-                values
-                    .iter()
-                    .map(|value| self.resolve_scalar_value(scalar_type, Some(value)))
-                    .collect::<Vec<gqlq::Value>>(),
-            ),
-            _ => gqlq::Value::Null,
-        }
-    }
-
-    fn resolve_abstract_type<'a>(
-        &self,
-        _schema: &'a gqls::Document,
-        _abstract_type: &gqls::TypeDefinition,
-        _object_value: &gqlq::Value,
-    ) -> Option<&'a gqls::ObjectType> {
-        None
-    }
-}
+use super::resolver::StoreResolver;
 
 /// Common query runner implementation for The Graph.
 pub struct QueryRunner<S> {
@@ -169,7 +44,7 @@ where
         self.runtime.spawn(stream.for_each(move |query| {
             let options = execution::ExecutionOptions {
                 logger: logger.clone(),
-                resolver: StoreResolver::new(store.clone()),
+                resolver: StoreResolver::new(&logger, store.clone()),
             };
             let result = execution::execute(&query, options);
 
