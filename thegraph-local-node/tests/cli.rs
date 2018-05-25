1f8f51137d397f4adf08a8dbcdc9d86c7337bd2a
Ford Nickels <ford@thegraph.com>
2018-05-24 18:08:46 -0700

    thegraph-local-node: Create simple integration test for CLI

---
+++ thegraph-local-node/tests/cli.rs
@@ -0,0 +1,25 @@
+extern crate assert_cli;
+
+static WITHOUT_ARGS_OUTPUT: &'static str = "error: The following required arguments were not provided:
+    --db <URL>
+
+USAGE:
+    thegraph-local-node --db <URL>
+
+For more information try --help
+";
+
+#[cfg(test)]
+mod integration {
+    use assert_cli;
+    use WITHOUT_ARGS_OUTPUT;
+
+    #[test]
+    fn starting_local_node_without_args() {
+        assert_cli::Assert::main_binary()
+            .fails()
+            .and()
+            .stderr().is(WITHOUT_ARGS_OUTPUT)
+            .unwrap();
+    }
+}
