c0ea17718a748d56fcf8242d1b9a262bae944be3
Ford Nickels <ford@thegraph.com>
2018-05-30 01:43:00 -0700

    thegraph-store-postgres-diesel: Update diesel schema and structs

--- thegraph-store-postgres-diesel/migrations/2018052400102_create_entities/up.sql
+++ thegraph-store-postgres-diesel/migrations/2018052400102_create_entities/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS entities (
      id INT NOT NULL,
-     data_source VARCHAR,
-     entity VARCHAR,
+     data_source VARCHAR NOT NULL,
+     entity VARCHAR NOT NULL,
      data jsonb NOT NULL,
      PRIMARY KEY (id, data_source, entity)
  );
