20ae0543c1e5675ed0babbf7f7f7cd174e88c4a8
Ford Nickels <ford@thegraph.com>
2018-05-31 21:28:24 -0700

    thegraph: Implement Bool variant to data::store::Value

--- thegraph/src/data/store.rs
+++ thegraph/src/data/store.rs
@@ -1,5 +1,4 @@
 use graphql_parser::query;
-use serde;
 use std::collections::{BTreeMap, HashMap};
 use std::ops::{Deref, DerefMut};
 
@@ -11,7 +10,8 @@ pub type Attribute = String;
 pub enum Value {
     String(String),
     Int(i32),
-    Float(f32)
+    Float(f32),
+    Bool(bool)
 
 }
 
@@ -20,6 +20,7 @@ impl Into<query::Value> for Value {
         match self {
             Value::String(s) => query::Value::String(s.to_string()),
             Value::Float(f) => query::Value::Float(f.into()),
+            Value::Bool(b) => query::Value::Boolean(b),
             _ => unimplemented!()
         }
     }
@@ -31,6 +32,7 @@ impl From<query::Value> for Value {
             query::Value::String(s) => Value::String(s),
             query::Value::Int(i) => Value::Int(i.as_i64().expect("Unable to parse graphql_parser::query::Number into i64") as i32),
             query::Value::Float(f) => Value::Float(f as f32),
+            query::Value::Boolean(b) => Value::Bool(b),
             _ => unimplemented!(),
         }
     }
@@ -42,6 +44,7 @@ impl<'a> From<&'a query::Value> for Value {
             query::Value::String(s) => Value::String(s.to_owned()),
             query::Value::Int(i) => Value::Int(i.to_owned().as_i64().expect("Unable to parse graphql_parser::query::Number into i64") as i32),
             query::Value::Float(f) => Value::Float(f.to_owned() as f32),
+            query::Value::Boolean(b) => Value::Bool(b.to_owned()),
             _ => unimplemented!(),
         }
     }
