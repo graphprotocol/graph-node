43de2029d59e270e2d0202b5c32ed833199e67a2
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-01 18:05:19 +0200

    thegraph-runtime-nodejs: Rework the runtime adapter

--- thegraph-runtime-nodejs/src/adapter.rs
+++ thegraph-runtime-nodejs/src/adapter.rs
@@ -1,17 +1,19 @@
 use futures::future;
 use futures::prelude::*;
 use futures::sync::mpsc::{channel, Receiver, Sender};
+use serde_json;
 use slog;
 use std::fs;
 use std::path::Path;
 use std::process::{Child, Command};
 use std::sync::{Arc, Mutex};
-use tempdir::TempDir;
+use std::thread;
+use tempfile::{tempdir, TempDir};
 use tokio_core::reactor::Handle;
 
 use thegraph::components::data_sources::RuntimeAdapterEvent;
 use thegraph::components::store::StoreKey;
-use thegraph::prelude::{*, RuntimeAdapter as RuntimeAdapterTrait};
+use thegraph::prelude::{Entity, RuntimeAdapter as RuntimeAdapterTrait, Value, *};
 use thegraph::util::stream::StreamError;
 
 use server;
@@ -29,20 +31,24 @@ pub struct RuntimeAdapter {
     config: RuntimeAdapterConfig,
     runtime: Handle,
     event_sink: Arc<Mutex<Option<Sender<RuntimeAdapterEvent>>>>,
-    temp_dir: Option<TempDir>,
+    temp_dir: TempDir,
     child_process: Option<Child>,
 }
 
 impl RuntimeAdapter {
     /// Creates a new runtiem adapter for a Node.js based data source runtime.
     pub fn new(logger: &slog::Logger, runtime: Handle, config: RuntimeAdapterConfig) -> Self {
+        // Create temporary directory for the runtime
+        let temp_dir = tempdir()
+            .expect("Failed to create temporary directory for the Node.js runtime adapter");
+
         // Create the runtime adapter
         let runtime_adapter = RuntimeAdapter {
             logger: logger.new(o!("component" => "RuntimeAdapter")),
             config,
             runtime,
+            temp_dir,
             event_sink: Arc::new(Mutex::new(None)),
-            temp_dir: None,
             child_process: None,
         };
 
@@ -66,90 +72,114 @@ impl RuntimeAdapterTrait for RuntimeAdapter {
     fn start(&mut self) {
         info!(self.logger, "Prepare runtime");
 
-        // Create a temporary directory
-        self.temp_dir = Some(
-            TempDir::new("nodejs-runtime")
-                .expect("Failed to create temporary directory for the Node.js runtime"),
-        );
-
-        // Get the temporary path
-        let temp_dir = self.temp_dir.as_ref().unwrap();
-
-        debug!(self.logger, "Copy runtime source files");
+        let temp_dir_str = self.temp_dir
+            .path()
+            .to_str()
+            .expect("Invalid temporary directory");
 
         // Copy runtime sources into the temporary directory
+        debug!(self.logger, "Copy runtime source files");
         for filename in ["package.json", "index.js", "db.js"].into_iter() {
             fs::copy(
                 Path::new(self.config.runtime_source_dir.as_str()).join(filename),
-                temp_dir.path().join(filename),
+                self.temp_dir.path().join(filename),
             ).expect(format!("Failed to copy data source runtime file: {}", filename).as_str());
         }
 
-        debug!(self.logger, "Inject data source files into the runtime");
-
-        // Write the data source definition to the temporary directory
-        let data_source_definition_path = temp_dir.path().join("data-source-definition.json");
-        fs::write(
-            data_source_definition_path.clone(),
-            self.config.data_source_definition.as_str(),
-        ).expect("Failed to write mapping.js");
-
-        // TODO Write all other files (mapping.js, ABIs etc.) to the temporary directory
-
-        debug!(self.logger, "Run `npm install`");
-
         // Run `npm install` in the temporary directory
+        debug!(self.logger, "Install NPM dependencies");
         Command::new("npm")
             .arg("install")
-            .current_dir(temp_dir.path().clone())
+            .current_dir(temp_dir_str)
             .output()
             .expect("Failed to run `npm install`");
 
-        debug!(self.logger, "Start the Node.js data source runtime");
+        // Create a channel to receive JSON updates from the adapter server and
+        // start an HTTP server for the runtime to send events to
+        debug!(self.logger, "Start adapter server");
+        let (runtime_event_sender, runtime_event_receiver) = channel(10);
+        self.runtime.spawn(server::start_server(
+            self.logger.clone(),
+            runtime_event_sender,
+            "127.0.0.1:7500",
+        ));
 
         // Spawn the Node.js runtime process; Node needs to be installed on the machine
         // or this will fail
+        debug!(self.logger, "Start the Node.js data source runtime");
         let mut child = Command::new("node")
-            .current_dir(temp_dir.path().clone())
+            .current_dir(temp_dir_str)
             .arg("index.js")
-            .arg(data_source_definition_path.as_path())
-            .arg(temp_dir.path().clone())
-            .arg(self.config.json_rpc_url.as_str())
+            .arg(self.config.data_source_definition.as_str())
+            .arg(temp_dir_str)
+            .arg("http://127.0.0.1:7500/")
             .spawn()
             .expect("Failed to start Node.js date source runtime");
 
-        debug!(self.logger, "Start adapter server");
+        // Process forwrd incoming runtime events
+        let receiver_logger = self.logger.clone();
+        let event_sink = self.event_sink
+            .lock()
+            .unwrap()
+            .clone()
+            .expect("Node.js runtime adapter started without being connected");
+        self.runtime.spawn(
+            runtime_event_receiver
+                .for_each(move |runtime_event| {
+                    let entity_id = match runtime_event.data.get("id") {
+                        Some(Value::String(ref s)) => Some(s.to_owned()),
+                        _ => None,
+                    };
 
-        // Start an HTTP server for the runtime to send events to
-        server::start_server(self.config.json_rpc_url.as_str());
+                    // Skip the event if the entity ID is missing or not a string
+                    if entity_id == None {
+                        error!(receiver_logger,
+                           "Field [\"data\", \"id\"] missing or invalid in runtime event";
+                           "event" => format!("{:?}", runtime_event));
+                        return Err(());
+                    }
 
-        // Process forwrd incoming data source events
-        let event_sink = self.event_sink.clone();
-        self.runtime.spawn(future::lazy(move || {
-            let event_sink = event_sink
-                .lock()
-                .unwrap()
-                .clone()
-                .expect("Node.js runtime adapter started without being connected");
+                    let event = match runtime_event.operation.as_str() {
+                        "add" => RuntimeAdapterEvent::EntityAdded(
+                            "memefactory".to_string(),
+                            StoreKey {
+                                entity: runtime_event.entity.to_owned(),
+                                id: entity_id.unwrap(),
+                            },
+                            runtime_event.data,
+                        ),
+                        "update" => RuntimeAdapterEvent::EntityChanged(
+                            "memefactory".to_string(),
+                            StoreKey {
+                                entity: runtime_event.entity.to_owned(),
+                                id: entity_id.unwrap(),
+                            },
+                            runtime_event.data,
+                        ),
+                        "remove" => RuntimeAdapterEvent::EntityRemoved(
+                            "memefactory".to_string(),
+                            StoreKey {
+                                entity: runtime_event.entity.to_owned(),
+                                id: entity_id.unwrap(),
+                            },
+                        ),
+                        _ => unimplemented!("Unknown runtime event operation"),
+                    };
 
-            event_sink
-                .clone()
-                .send(RuntimeAdapterEvent::EntityAdded(
-                    "memefactory".to_string(),
-                    StoreKey {
-                        entity: "user".to_string(),
-                        id: "1".to_string(),
-                    },
-                    Entity::new(),
-                ))
-                .wait()
-                .unwrap();
-
-            Ok(())
-        }));
+                    event_sink
+                        .clone()
+                        .send(event)
+                        .wait()
+                        .expect("Failed to forward runtime adapter event");
+                    Ok(())
+                })
+                .and_then(|_| Ok(())),
+        );
     }
 
-    fn stop(&mut self) {}
+    fn stop(&mut self) {
+        info!(self.logger, "Stop");
+    }
 
     fn event_stream(&mut self) -> Result<Receiver<RuntimeAdapterEvent>, StreamError> {
         info!(self.logger, "Setting up event stream");
--- thegraph-runtime-nodejs/src/server.rs
+++ thegraph-runtime-nodejs/src/server.rs
@@ -1,51 +1,116 @@
 use futures::future;
 use futures::prelude::*;
+use futures::sync::mpsc::Sender;
 use hyper;
 use hyper::server::conn::AddrIncoming;
 use hyper::service::Service;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use serde_json;
+use slog;
 
-#[derive(Default)]
-struct RuntimeAdapterService;
+use super::event::RuntimeEvent;
+use super::request::{RuntimeRequest, RuntimeRequestError};
+
+struct RuntimeAdapterService {
+    logger: slog::Logger,
+    event_sender: Sender<RuntimeEvent>,
+}
+
+impl RuntimeAdapterService {
+    pub fn new(logger: slog::Logger, event_sender: Sender<RuntimeEvent>) -> Self {
+        RuntimeAdapterService {
+            logger,
+            event_sender,
+        }
+    }
+}
 
 impl Service for RuntimeAdapterService {
     type ReqBody = Body;
     type ResBody = Body;
-    type Error = hyper::Error;
-    type Future = Box<Future<Item = Response<Self::ReqBody>, Error = hyper::Error> + Send>;
+    type Error = RuntimeRequestError;
+    type Future = Box<Future<Item = Response<Self::ReqBody>, Error = Self::Error> + Send>;
 
-    fn call(&mut self, _req: Request<Self::ReqBody>) -> Self::Future {
-        let response = match _req.method() {
-            &Method::POST => {
-                // TODO: Parse body and forward to store
+    fn call(&mut self, request: Request<Self::ReqBody>) -> Self::Future {
+        let read_logger = self.logger.clone();
+        let deserialize_logger = self.logger.clone();
+        let send_logger = self.logger.clone();
+
+        let event_sender = self.event_sender.clone();
+
+        match request.method() {
+            &Method::POST => Box::new({
+                let logger = self.logger.clone();
+
+                // Read the request body into a single chunk
+                request
+                    .into_body()
+                    .concat2()
+                    .map_err(RuntimeRequestError::from)
+                    .and_then(RuntimeRequest::new)
+                    .and_then(move |event| {
+                        event_sender
+                            .clone()
+                            .send(event)
+                            .map_err(|e| panic!("Failed to forward runtime event: {}", e))
+                    })
+                    .then(move |result| {
+                        if let Err(ref e) = result {
+                            error!(logger, "Failed to handle runtime event";
+                                   "error" => format!("{:?}", e));
+                        }
+
+                        // Once we're done with the above, send a response back
+                        match result {
+                            Ok(_) => future::ok(
+                                Response::builder()
+                                    .status(StatusCode::CREATED)
+                                    .body(Body::from("OK"))
+                                    .unwrap(),
+                            ),
+                            Err(e) => future::ok(
+                                Response::builder()
+                                    .status(StatusCode::BAD_REQUEST)
+                                    .body(Body::from(format!("{}", e)))
+                                    .unwrap(),
+                            ),
+                        }
+                    })
+            }),
+            _ => Box::new(future::ok(
                 Response::builder()
-                    .status(StatusCode::CREATED)
-                    .body(Body::from("OK"))
-                    .unwrap()
-            }
-            _ => Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(Body::from("Not found"))
-                .unwrap(),
-        };
-
-        Box::new(future::ok(response))
+                    .status(StatusCode::NOT_FOUND)
+                    .body(Body::from("Not found"))
+                    .unwrap(),
+            )),
+        }
     }
 }
 
-pub fn start_server(addr: &str) {
+/// Starts the runtime adpater's HTTP server to handle events from the runtime.
+pub fn start_server(
+    logger: slog::Logger,
+    event_sender: Sender<RuntimeEvent>,
+    addr: &str,
+) -> Box<Future<Item = (), Error = ()> + Send> {
+    // Parse the server address
     let addr = addr.parse().expect(
         "Invalid network address provided \
          for the Node.js runtime adapter server",
     );
 
-    let new_service = || {
-        let service = RuntimeAdapterService::default();
-        future::ok::<RuntimeAdapterService, hyper::Error>(service)
+    // Create a new service factory
+    let new_service = move || {
+        future::ok::<RuntimeAdapterService, hyper::Error>(RuntimeAdapterService::new(
+            logger.clone(),
+            event_sender.clone(),
+        ))
     };
 
-    Server::bind(&addr)
-        .serve(new_service)
-        .wait()
-        .expect("Failed to start Node.js runtime adapter server");
+    // Start serving requests
+    Box::new(
+        Server::bind(&addr)
+            .serve(new_service)
+            .map_err(|e| panic!("Failed to start runtime adapter server: {}", e)),
+    )
 }
