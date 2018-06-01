3a3943db4fe9ba78c4059f158c58908accaf2f8c
Ford Nickels <ford@thegraph.com>
2018-05-31 17:23:15 -0700

    thegraph-store-postgres-diesel: Implement Float not equals filter

--- thegraph-store-postgres-diesel/tests/store.rs
+++ thegraph-store-postgres-diesel/tests/store.rs
@@ -295,7 +295,7 @@ fn find_entities_not_equal_string() {
         assert!(returned_name.is_some());
         assert_ne!(&test_value, returned_name.unwrap());
 
-        // There should be 3 users returned in results
+        // There should be two users returned in results
         assert_eq!(2, returned_entities.len());
     })
 }
@@ -359,7 +359,7 @@ fn find_entities_less_than_string() {
         assert!(returned_name.is_some());
         assert_ne!(&test_value, returned_name.unwrap());
 
-        //There should be 3 users returned in results
+        //There should be two users returned in results
         assert_eq!(2, returned_entities.len());
     })
 }
@@ -491,5 +491,37 @@ fn find_entities_equal_float() {
 
         // There should be one user returned in results
         assert_eq!(1, returned_entities.len());
+    })
+}
+
+#[test]
+fn find_entities_not_equal_float() {
+    run_test(|| {
+        let core = Core::new().unwrap();
+        let logger = logger();
+        let url = postgres_test_url();
+        let new_store = DieselStore::new(StoreConfig { url }, &logger, core.handle());
+        let this_query = StoreQuery {
+            entity: String::from("user"),
+            filter: Some(StoreFilter::And(vec![StoreFilter::Not(
+                "age".to_string(),
+                Value::Float(67 as f64),
+            )])),
+            order_by: Some(String::from("name")),
+            order_direction: Some(StoreOrder::Descending),
+            range: None,
+        };
+        let result = new_store.find(this_query);
+        assert!(result.is_ok());
+
+        // Check if the first user in the result vector is "Shaqueeena"
+        let returned_entities = result.unwrap();
+        let returned_name = returned_entities[0].get(&"name".to_string());
+        let test_value = Value::String("Shaqueeena".to_string());
+        assert!(returned_name.is_some());
+        assert_eq!(&test_value, returned_name.unwrap());
+
+        // There should be two users returned in results
+        assert_eq!(2, returned_entities.len());
     })
 }
