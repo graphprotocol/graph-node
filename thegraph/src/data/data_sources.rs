a71fdcbc78d049732196b684aef520303f00c4d0
Ford Nickels <ford@thegraph.com>
2018-06-05 01:38:31 -0700

    thegraph: Create structs to process/represent yaml schema file

--- thegraph/src/data/data_sources.rs
+++ thegraph/src/data/data_sources.rs
@@ -1,1 +1,47 @@
-
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct SourceDefinition {
+    pub spec_version: String,
+    pub schema: SourcePath,
+    pub datasets: SourceDataSet,
+}
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct SourcePath {
+    pub path: String,
+}
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct SourceDataSet {
+    pub data: SourceData,
+    pub mapping: SourceMapping,
+}
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct SourceData {
+    pub kind: String,
+    pub name: String,
+    pub address: String,
+    pub structure: SourceDataABI,
+}
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct SourceDataABI {
+    pub abi: SourcePath,
+}
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct SourceMapping {
+    pub kind: String,
+    #[serde(rename = "apiVersion")]
+    pub api_version: String,
+    pub language: String,
+    pub entities: Vec<String>,
+    pub config: MappingConfig,
+    pub source: SourcePath,
+}
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct MappingConfig {
+    pub name: String,
+    pub value: MappingConfigValue,
+}
+#[derive(Debug, PartialEq, Deserialize)]
+pub struct MappingConfigValue {
+    pub path: String,
+    #[serde(rename = "contentType")]
+    pub content_type: String,
+}
