0b1f019aee7dae762caa2c4ed39158a0fe81bcbd
Ford Nickels <ford@thegraph.com>
2018-06-05 01:36:32 -0700

    thegraph: Implement POC data source deserialize & load schema

--- thegraph/src/components/data_sources.rs
+++ thegraph/src/components/data_sources.rs
@@ -1,7 +1,16 @@
 use futures::sync::mpsc::Receiver;
 
+use data::data_sources::SourceDefinition;
 use data::schema::Schema;
 use util::stream::StreamError;
+
+use graphql_parser;
+use serde_yaml;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 
 /// Events emitted by [DataSourceProvider](trait.DataSourceProvider.html) implementations.
 #[derive(Clone, Debug)]
@@ -30,4 +39,36 @@ pub trait DataSourceProvider {
     /// Receiver from whith others can read schema-only events emitted by the data source provider.
     /// Can only be called once. Any consecutive call will result in a StreamError.
     fn schema_event_stream(&mut self) -> Result<Receiver<SchemaEvent>, StreamError>;
+}
+
+/// Common trait for data source definition schema.
+pub trait DataSourceDefinition {
+    fn new(definition: serde_yaml::Value) -> Schema {
+        // Deserialize yaml file into SourceDefinition struct
+        let schema_definition: SourceDefinition = serde_yaml::from_value(definition).unwrap();
+
+        // Get definition file from filesystem and return string
+        let schema_path = Path::new(&schema_definition.schema.path);
+        let display_schema_path = schema_path.display();
+
+        let mut schema_file = match File::open(&schema_path) {
+            Err(why) => panic!(
+                "couldn't open schema at {}: {}",
+                display_schema_path,
+                why.description()
+            ),
+            Ok(file) => file,
+        };
+        let mut schema_string = String::new();
+        schema_file.read_to_string(&mut schema_string).unwrap();
+
+        // Parse graphql schema string into graphql Document
+        // and place into Schema struct
+        let graphql_schema = Schema {
+            id: "test".to_string(),
+            document: graphql_parser::parse_schema(&schema_string).unwrap(),
+        };
+
+        graphql_schema
+    }
 }
