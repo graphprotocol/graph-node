e8acf2d54c5c3e6cfb7ce8c284e99e0020600eb9
Ford Nickels <ford@thegraph.com>
2018-06-05 23:49:08 -0700

    thegraph-core: Add serde crates

--- thegraph-core/src/lib.rs
+++ thegraph-core/src/lib.rs
@@ -5,10 +5,15 @@ extern crate slog;
 extern crate thegraph;
 extern crate thegraph_graphql_utils;
 extern crate tokio_core;
+extern crate serde;
+extern crate serde_yaml;
+#[macro_use]
+extern crate serde_derive;
 
 mod query;
 mod schema;
 mod store;
+mod data_sources;
 
 pub use query::QueryRunner;
 pub use schema::SchemaProvider;
