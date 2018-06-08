fd9a1d902e48df9d56474af33915c5c523d8c03f
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-08 04:33:17 +0200

    thegraph: Improve and export the DataSourceDefinition struct

--- thegraph/src/lib.rs
+++ thegraph/src/lib.rs
@@ -34,6 +34,7 @@ pub mod prelude {
     pub use components::server::GraphQLServer;
     pub use components::store::{Store, StoreFilter, StoreOrder, StoreQuery, StoreRange};
 
+    pub use data::data_sources::DataSourceDefinition;
     pub use data::query::{Query, QueryError, QueryExecutionError, QueryResult, QueryVariableValue,
                           QueryVariables};
     pub use data::schema::Schema;
