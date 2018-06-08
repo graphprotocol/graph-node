4da46550e7a4a3d57bf8c0f13dd60eadad53a33b
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-08 04:33:17 +0200

    thegraph: Improve and export the DataSourceDefinition struct

--- thegraph/src/data/data_sources.rs
+++ thegraph/src/data/data_sources.rs
@@ -1,42 +1,50 @@
+use serde_yaml;
+
 #[derive(Debug, PartialEq, Deserialize)]
-pub struct SourcePath {
+pub struct Location {
     pub path: String,
 }
 
 #[derive(Debug, PartialEq, Deserialize)]
-pub struct SourceDataSet {
-    pub data: SourceData,
-    pub mapping: SourceMapping,
+pub struct DataStructure {
+    abi: String,
 }
+
 #[derive(Debug, PartialEq, Deserialize)]
-pub struct SourceData {
+pub struct Data {
     pub kind: String,
     pub name: String,
     pub address: String,
-    pub structure: SourceDataABI,
+    pub structure: DataStructure,
 }
+
 #[derive(Debug, PartialEq, Deserialize)]
-pub struct SourceDataABI {
-    pub abi: SourcePath,
+pub struct MappingConfigValue {
+    pub name: String,
+    pub value: serde_yaml::Value,
 }
+
 #[derive(Debug, PartialEq, Deserialize)]
-pub struct SourceMapping {
+pub struct Mapping {
     pub kind: String,
     #[serde(rename = "apiVersion")]
     pub api_version: String,
     pub language: String,
     pub entities: Vec<String>,
-    pub config: MappingConfig,
-    pub source: SourcePath,
+    pub config: Vec<MappingConfigValue>,
+    pub source: Location,
 }
+
 #[derive(Debug, PartialEq, Deserialize)]
-pub struct MappingConfig {
-    pub name: String,
-    pub value: MappingConfigValue,
+pub struct DataSet {
+    pub data: Data,
+    pub mapping: Mapping,
 }
+
 #[derive(Debug, PartialEq, Deserialize)]
-pub struct MappingConfigValue {
-    pub path: String,
-    #[serde(rename = "contentType")]
-    pub content_type: String,
+pub struct DataSourceDefinition {
+    #[serde(rename = "specVersion")]
+    pub spec_version: String,
+    pub schema: String,
+    pub datasets: Vec<DataSet>,
 }
