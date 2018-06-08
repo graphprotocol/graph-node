2a6b3e229e15c14eb8fc11c1e8c2708a8720583e
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-08 04:50:00 +0200

    thegraph-local-node: Add a LocalDataSourceProvider implementation
    This data source provider is special in that it takes in a specific
    local path to a data source definition file and then loads only
    that data source.

---
+++ thegraph-local-node/src/lib.rs
@@ -0,0 +1,11 @@
+extern crate futures;
+extern crate graphql_parser;
+#[macro_use]
+extern crate slog;
+extern crate thegraph;
+extern crate thegraph_core;
+extern crate tokio_core;
+
+mod data_sources;
+
+pub use self::data_sources::LocalDataSourceProvider;
