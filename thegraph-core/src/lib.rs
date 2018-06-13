fdabf1c1e17938e6f22e2bbf75b8bb2915368a88
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-13 12:29:04 +0200

    thegraph-core: Remove compiler warnings

--- thegraph-core/src/lib.rs
+++ thegraph-core/src/lib.rs
@@ -7,8 +7,6 @@ extern crate serde_yaml;
 extern crate thegraph;
 extern crate thegraph_graphql_utils;
 extern crate tokio_core;
-#[macro_use]
-extern crate serde_derive;
 
 mod data_sources;
 mod query;
