0bb074f47a9a86d7025d520be960491ed6006564
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-14 23:34:29 +0200

    thegraph: Add an EthereumWatcher trait with a few related dummy types

--- thegraph/src/components/ethereum/watcher.rs
+++ thegraph/src/components/ethereum/watcher.rs
@@ -1,65 +1,49 @@
-use ethabi::{Bytes, EventParam};
-use ethereum_types::{Address, H256};
+use futures::prelude::*;
 use futures::sync::mpsc::Receiver;
 
-/// A request for the state of a contract at a specific block hash and address.
 pub struct EthereumContractStateRequest {
-    pub address: Address,
-    pub block_hash: H256,
+    pub address: String,
+    pub block_hash: String,
 }
 
-/// An error that can occur when trying to obtain the state of a contract.
 pub enum EthereumContractStateError {
     Failed,
 }
 
-/// Representation of an Ethereum contract state.
 pub struct EthereumContractState {
-    pub address: Address,
-    pub block_hash: H256,
-    pub data: Bytes,
+    pub address: String,
+    pub block_hash: String,
+    pub data: String,
 }
 
-/// A range to allow event subscriptions to limit the block numbers to consider.
 pub struct BlockNumberRange {
-    pub from: Option<u64>,
-    pub to: Option<u64>,
+    pub from: Option<i64>,
+    pub to: Option<i64>,
 }
 
-/// A subscription to a specific contract address, event signature and block range.
 pub struct EthereumEventSubscription {
-    /// An ID that uniquely identifies the subscription (e.g. a GUID).
-    pub subscription_id: String,
-    pub address: Address,
-    pub event_signature: H256,
+    pub unique_id: String,
+    pub address: String,
+    pub event: String,
     pub range: BlockNumberRange,
 }
 
-/// An event logged for a specific contract address and event signature.
 pub struct EthereumEvent {
-    pub address: Address,
-    pub event_signature: H256,
-    pub block_hash: H256,
-    pub params: Vec<EventParam>,
+    pub address: String,
+    pub event: String,
+    pub block_hash: String,
 }
 
-/// Common trait for components that watch and manage access to Ethereum.
-///
-/// Implementations may be implemented against an in-process Ethereum node
-/// or a remote node over RPC.
 pub trait EthereumWatcher {
-    /// Obtain the state of a smart contract.
     fn contract_state(
         &mut self,
         request: EthereumContractStateRequest,
-    ) -> Result<EthereumContractState, EthereumContractStateError>;
+    ) -> Box<Future<Item = EthereumContractState, Error = EthereumContractStateError>>;
 
-    /// Subscribe to an event of a smart contract.
     fn subscribe_to_event(
         &mut self,
         subscription: EthereumEventSubscription,
     ) -> Receiver<EthereumEvent>;
 
-    /// Cancel a specific event subscription. Returns true when the subscription existed before.
-    fn unsubscribe_from_event(&mut self, subscription_id: String) -> bool;
+    fn unsubscribe_from_event(&mut self, unique_id: String) -> bool;
 }
