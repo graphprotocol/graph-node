88f6ac6e661fd48f81d24cd119a35e5a260cf4eb
Ford Nickels <ford@thegraph.com>
2018-06-20 00:10:28 -0700

    thegraph-runtime: Add fn resolvers skeleton for RuntimeAdapterEvents

--- thegraph-runtime/src/interpreter.rs
+++ thegraph-runtime/src/interpreter.rs
@@ -1,11 +1,31 @@
 use parity_wasm;
 use std::env::current_dir;
-use wasmi::{Error, Externals, FuncRef, ImportsBuilder,
+use thegraph::components::store as StoreComponents;
+use thegraph::data::store as StoreData;
+use wasmi::{Error, Externals, FuncInstance, FuncRef, ImportsBuilder,
             Module, ModuleImportResolver, ModuleInstance, ModuleRef, RuntimeArgs, RuntimeValue, Signature, Trap, ValueType};
 
 // Set indexes for Store functions that will be exported with module
+const ADD_FUNC_INDEX: usize = 0;
+const UPDATE_FUNC_INDEX: usize = 1;
+const DELETE_FUNC_INDEX: usize = 2;
 
 pub struct HostExternals {}
+
+// Put StoreKey into linear memory and return a u32 describing location (pointer)
+pub fn _storekey_to_memory(_key: StoreComponents::StoreKey) -> RuntimeValue {
+    unimplemented!();
+}
+
+// Get StoreKey from wasm pointer
+pub fn storekey_from_wasm(_pointer: u32) -> StoreComponents::StoreKey {
+    unimplemented!();
+}
+
+// Get Entity from wasm pointer
+pub fn entity_from_wasm(_pointer: u32) -> StoreData::Entity {
+    unimplemented!();
+}
 
 // pub fn runtime_event_sender() -
 
@@ -13,9 +33,51 @@ impl Externals for HostExternals {
     fn invoke_index(
         &mut self,
         index: usize,
-        _args: RuntimeArgs,
+        args: RuntimeArgs,
     ) -> Result<Option<RuntimeValue>, Trap> {
         match index {
+            ADD_FUNC_INDEX => {
+                // Input: StoreKey and Entity
+                let storekey_ptr: u32 = args.nth_checked(0)?;
+                let _storekey: StoreComponents::StoreKey = storekey_from_wasm(storekey_ptr);
+
+                let entity_ptr: u32 = args.nth_checked(1)?;
+                let _entity: StoreData::Entity = entity_from_wasm(entity_ptr);
+
+                // Create add entity store and add to sink
+
+                // Placeholder for function
+                let result = storekey_ptr;
+
+                Ok(Some(RuntimeValue::I32(result as i32)))
+            }
+            UPDATE_FUNC_INDEX => {
+                // Input: StoreKey and Entity
+                let storekey_ptr: u32 = args.nth_checked(0)?;
+                let _storekey: StoreComponents::StoreKey = storekey_from_wasm(storekey_ptr);
+
+                let entity_ptr: u32 = args.nth_checked(1)?;
+                let _entity: StoreData::Entity = entity_from_wasm(entity_ptr);
+
+                // Create update entity store event and add to sink
+
+                // Placeholder for function
+                let result = storekey_ptr;
+
+                Ok(Some(RuntimeValue::I32(result as i32)))
+            }
+            DELETE_FUNC_INDEX => {
+                // Input: StoreKey
+                let storekey_ptr: u32 = args.nth_checked(0)?;
+                let _storekey: StoreComponents::StoreKey = storekey_from_wasm(storekey_ptr);
+
+                // Create delete entity store event and add to sink
+
+                // Placeholder for function
+                let result = storekey_ptr;
+
+                Ok(Some(RuntimeValue::I32(result as i32)))
+            }
             _ => panic!("Unimplemented function at {}", index),
         }
     }
@@ -24,6 +86,9 @@ impl Externals for HostExternals {
 impl HostExternals {
     fn _check_signature(&self, index: usize, signature: &Signature) -> bool {
         let (params, ret_ty): (&[ValueType], Option<ValueType>) = match index {
+            ADD_FUNC_INDEX => (&[ValueType::I32, ValueType::I32], Some(ValueType::I32)),
+            UPDATE_FUNC_INDEX => (&[ValueType::I32, ValueType::I32], Some(ValueType::I32)),
+            DELETE_FUNC_INDEX => (&[ValueType::I32], Some(ValueType::I32)),
             _ => return false,
         };
         signature.params() == params && signature.return_type() == ret_ty
@@ -35,6 +100,18 @@ pub struct RuntimeModuleImportResolver;
 impl ModuleImportResolver for RuntimeModuleImportResolver {
     fn resolve_func(&self, field_name: &str, _signature: &Signature) -> Result<FuncRef, Error> {
         let func_ref = match field_name {
+            "add" => FuncInstance::alloc_host(
+                Signature::new(&[ValueType::I32, ValueType::I32][..], Some(ValueType::I32)),
+                ADD_FUNC_INDEX,
+            ),
+            "update" => FuncInstance::alloc_host(
+                Signature::new(&[ValueType::I32, ValueType::I32][..], Some(ValueType::I32)),
+                UPDATE_FUNC_INDEX,
+            ),
+            "delete" => FuncInstance::alloc_host(
+                Signature::new(&[ValueType::I32][..], Some(ValueType::I32)),
+                DELETE_FUNC_INDEX,
+            ),
             _ => {
                 return Err(Error::Instantiation(format!(
                     "Export {} not found",
