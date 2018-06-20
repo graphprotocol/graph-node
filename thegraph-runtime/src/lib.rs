31b6baa5a74acd318eb16190cbace130101ed9fb
Ford Nickels <ford@thegraph.com>
2018-06-19 21:34:58 -0700

    thegraph-runtime: Add parity-wasm and wasmi crates

--- thegraph-runtime/src/lib.rs
+++ thegraph-runtime/src/lib.rs
@@ -1,7 +1,7 @@
+extern crate ethereum_types;
 extern crate futures;
 #[macro_use]
 extern crate slog;
-extern crate ethereum_types;
 extern crate parity_wasm;
 extern crate thegraph;
 extern crate tokio_core;
