4cba6fbf0644a7c324191a00c6325ca6aa5410d8
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-22 21:22:09 +0200

    thegraph, thegraph-core: Add a new RuntimeManager trait and implementation

--- thegraph-core/src/data_sources/mod.rs
+++ thegraph-core/src/data_sources/mod.rs
@@ -1,3 +1,5 @@
 mod loader;
+mod manager;
 
 pub use self::loader::DataSourceDefinitionLoader;
+pub use self::manager::RuntimeManager;
--- thegraph/src/components/data_sources/mod.rs
+++ thegraph/src/components/data_sources/mod.rs
@@ -1,7 +1,9 @@
 mod host;
 mod loader;
+mod manager;
 mod provider;
 
 pub use self::host::{RuntimeHost, RuntimeHostEvent};
 pub use self::loader::{DataSourceDefinitionLoader, DataSourceDefinitionLoaderError};
+pub use self::manager::RuntimeManager;
 pub use self::provider::{DataSourceProvider, DataSourceProviderEvent, SchemaEvent};
