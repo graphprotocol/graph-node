7a6685c22c1c974395321b4eede7fe9279345fd9
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-22 21:21:29 +0200

    thegraph, thegraph-mock: Include a DataSourceDefinition in DataSourceProviderEvents

--- thegraph/src/components/data_sources/provider.rs
+++ thegraph/src/components/data_sources/provider.rs
@@ -1,15 +1,17 @@
-use data::schema::Schema;
 use futures::sync::mpsc::Receiver;
 use serde_yaml;
+
+use data::data_sources::DataSourceDefinition;
+use data::schema::Schema;
 use util::stream::StreamError;
 
 /// Events emitted by [DataSourceProvider](trait.DataSourceProvider.html) implementations.
 #[derive(Clone, Debug)]
 pub enum DataSourceProviderEvent {
     /// A data source was added to the provider.
-    DataSourceAdded(&'static str),
+    DataSourceAdded(DataSourceDefinition),
     /// A data source was removed from the provider.
-    DataSourceRemoved(&'static str),
+    DataSourceRemoved(DataSourceDefinition),
 }
 
 /// Schema-only events emitted by a [DataSourceProvider](trait.DataSourceProvider.html).
@@ -30,13 +32,4 @@ pub trait DataSourceProvider {
     /// Receiver from whith others can read schema-only events emitted by the data source provider.
     /// Can only be called once. Any consecutive call will result in a StreamError.
     fn schema_event_stream(&mut self) -> Result<Receiver<SchemaEvent>, StreamError>;
-}
-
-/// Common trait for data source definition schema.
-pub trait DataSourceDefinition {
-    // Deserialize serde_yaml into data source definition struct
-    fn build_definition(definition: serde_yaml::Value) -> Self;
-
-    // Extract graphql schema from data source definition struct
-    fn extract_schema(&self) -> Schema;
 }
