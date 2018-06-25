4a0f83bde0b8e10f652ea02f789ab712115b8582
Ford Nickels <ford@thegraph.com>
2018-06-25 00:13:07 -0700

    thegraph-core: Create RuntimeHostBuilder trait

--- thegraph/src/components/data_sources/mod.rs
+++ thegraph/src/components/data_sources/mod.rs
@@ -3,7 +3,7 @@ mod loader;
 mod manager;
 mod provider;
 
-pub use self::host::{RuntimeHost, RuntimeHostEvent};
+pub use self::host::{RuntimeHost, RuntimeHostEvent, RuntimeHostBuilder};
 pub use self::loader::{DataSourceDefinitionLoader, DataSourceDefinitionLoaderError};
 pub use self::manager::RuntimeManager;
 pub use self::provider::{DataSourceProvider, DataSourceProviderEvent, SchemaEvent};
