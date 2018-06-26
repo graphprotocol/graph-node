eee3fcc382e79713e55fdb81d14fe51565e7b449
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-26 11:34:46 +0200

    thegraph-core: Rework the RuntimeManager to hold less shared state

--- thegraph-core/src/data_sources/manager.rs
+++ thegraph-core/src/data_sources/manager.rs
@@ -8,66 +8,69 @@ use thegraph::components::data_sources::DataSourceProviderEvent;
 use thegraph::components::data_sources::RuntimeHostEvent;
 use thegraph::prelude::*;
 
-pub struct RuntimeManager<S, T>
-where
-    S: Store,
-    T: RuntimeHostBuilder,
-{
+pub struct RuntimeManager {
     logger: Logger,
-    runtime: Handle,
-    store: Arc<Mutex<S>>,
-    runtime_host_builder: Arc<Mutex<T>>,
-    runtime_hosts: Arc<Mutex<Vec<T::Host>>>,
     input: Sender<DataSourceProviderEvent>,
 }
 
-impl<S, T> RuntimeManager<S, T>
-where
-    S: Store + 'static,
-    T: RuntimeHostBuilder + 'static,
-{
+impl RuntimeManager where {
     /// Creates a new runtime manager.
-    pub fn new(logger: &Logger, runtime: Handle, store: Arc<Mutex<S>>, host_builder: T) -> Self {
+    pub fn new<S, T>(
+        logger: &Logger,
+        runtime: Handle,
+        store: Arc<Mutex<S>>,
+        host_builder: T,
+    ) -> Self
+    where
+        S: Store + 'static,
+        T: RuntimeHostBuilder + 'static,
+    {
         let logger = logger.new(o!("component" => "RuntimeManager"));
 
         // Create channel for receiving data source provider events.
         let (data_source_sender, data_source_receiver) = channel(100);
 
-        let mut manager = RuntimeManager {
-            logger,
+        // Handle incoming events from the data source provider.
+        Self::handle_data_source_events(
+            logger.clone(),
             runtime,
             store,
+            host_builder,
+            data_source_receiver,
+        );
+
+        RuntimeManager {
+            logger,
             input: data_source_sender,
-            runtime_host_builder: Arc::new(Mutex::new(host_builder)),
-            runtime_hosts: Arc::new(Mutex::new(vec![])),
-        };
-
-        // Handle incoming events from the data source provider.
-        manager.handle_data_source_events(data_source_receiver);
-
-        manager
+        }
     }
 
     /// Handle incoming events from data source providers.
-    fn handle_data_source_events(&mut self, receiver: Receiver<DataSourceProviderEvent>) {
-        let runtime = self.runtime.clone();
-        let runtime_hosts = self.runtime_hosts.clone();
-        let runtime_host_builder = self.runtime_host_builder.clone();
-        let store = self.store.clone();
-        let logger = self.logger.clone();
+    fn handle_data_source_events<S, T>(
+        logger: Logger,
+        runtime: Handle,
+        store: Arc<Mutex<S>>,
+        mut host_builder: T,
+        receiver: Receiver<DataSourceProviderEvent>,
+    ) where
+        S: Store + 'static,
+        T: RuntimeHostBuilder + 'static,
+    {
+        let mut runtime_hosts = vec![];
 
-        self.runtime.spawn(receiver.for_each(move |event| {
-            let store = store.clone();
-
+        runtime.clone().spawn(receiver.for_each(move |event| {
             match event {
                 DataSourceProviderEvent::DataSourceAdded(definition) => {
                     info!(logger, "Data source created, host runtime";
                           "location" => &definition.location);
 
                     // Create a new runtime host for the data source definition
-                    let mut new_host = runtime_host_builder.lock().unwrap().build(definition);
+                    let mut new_host = host_builder.build(definition);
 
-                    // Forward events from the runtime host to the store
+                    // Forward events from the runtime host to the store; this
+                    // Tokio task will terminate when the corresponding data source
+                    // is removed and the host and its event sender are dropped
+                    let store = store.clone();
                     runtime
                         .clone()
                         .spawn(
@@ -114,29 +117,13 @@ where
                                 }),
                         );
 
-                    // Start the hosted runtime
-                    new_host.start();
-
                     // Add the new host to the list of managed runtime hosts
-                    runtime_hosts.lock().unwrap().push(new_host);
+                    runtime_hosts.push(new_host);
                 }
                 DataSourceProviderEvent::DataSourceRemoved(ref definition) => {
-                    let mut hosts = runtime_hosts.lock().unwrap();
-
-                    {
-                        // Find the runtime host for the data source
-                        let host = hosts
-                            .iter_mut()
-                            .find(|host| host.data_source_definition() == definition);
-
-                        // If we have a runtime host, stop it now
-                        if host.is_some() {
-                            host.unwrap().stop();
-                        }
-                    }
-
-                    // Destroy all runtime hosts for this data source
-                    hosts.retain(|host| host.data_source_definition() != definition);
+                    // Destroy all runtime hosts for this data source; this will
+                    // also terminate the host's event stream
+                    runtime_hosts.retain(|host| host.data_source_definition() != definition);
                 }
             }
 
@@ -145,11 +132,7 @@ where
     }
 }
 
-impl<S, T> EventConsumer<DataSourceProviderEvent> for RuntimeManager<S, T>
-where
-    S: Store,
-    T: RuntimeHostBuilder,
-{
+impl EventConsumer<DataSourceProviderEvent> for RuntimeManager {
     type EventSink = Box<Sink<SinkItem = DataSourceProviderEvent, SinkError = ()>>;
 
     /// Get the wrapped event sink.
