88dd7191b219ed8c6f2b7aa470f143703de931a9
Leonardo Yvens Schwarzstein <leoyvens@gmail.com>
2018-06-27 08:27:48 -0300

    thegraph-runtime: Implement conversion to/from `U256` and `ArrayBuffer`
    Part of #119.

--- thegraph-runtime/src/asc_abi/to_from.rs
+++ thegraph-runtime/src/asc_abi/to_from.rs
@@ -23,6 +23,19 @@ impl<T: AscValue> FromAscObj<ArrayBuffer<T>> for [T; 20] {
     }
 }
 
+impl<T: AscValue> FromAscObj<ArrayBuffer<T>> for [T; 4] {
+    fn from_asc_obj<H: AscHeap>(array_buffer: &ArrayBuffer<T>, _: &H) -> Self {
+        assert_eq!(
+            array_buffer.content.len(),
+            4,
+            "wrong ArrayBuffer length, expected 4"
+        );
+        let mut array: [T; 4] = [T::default(); 4];
+        array.copy_from_slice(&array_buffer.content);
+        array
+    }
+}
+
 impl ToAscObj<ArrayBuffer<u8>> for ethereum_types::H160 {
     fn to_asc_obj<H: AscHeap>(&self, heap: &H) -> ArrayBuffer<u8> {
         self.0.to_asc_obj(heap)
@@ -30,15 +43,8 @@ impl ToAscObj<ArrayBuffer<u8>> for ethereum_types::H160 {
 }
 
 impl FromAscObj<ArrayBuffer<u8>> for ethereum_types::H160 {
-    fn from_asc_obj<H: AscHeap>(array_buffer: &ArrayBuffer<u8>, _: &H) -> Self {
-        assert_eq!(
-            array_buffer.content.len(),
-            20,
-            "wrong ArrayBuffer length, expected 20"
-        );
-        let mut array: [u8; 20] = [0; 20];
-        array.copy_from_slice(&array_buffer.content);
-        ethereum_types::H160(array)
+    fn from_asc_obj<H: AscHeap>(array_buffer: &ArrayBuffer<u8>, heap: &H) -> Self {
+        ethereum_types::H160(<[u8; 20]>::from_asc_obj(array_buffer, heap))
     }
 }
 
@@ -51,5 +57,17 @@ impl ToAscObj<AscString> for str {
 impl FromAscObj<AscString> for String {
     fn from_asc_obj<H: AscHeap>(asc_string: &AscString, _: &H) -> Self {
         String::from_utf16(&asc_string.content).expect("asc string was not UTF-16")
+    }
+}
+
+impl ToAscObj<ArrayBuffer<u64>> for ethereum_types::U256 {
+    fn to_asc_obj<H: AscHeap>(&self, heap: &H) -> ArrayBuffer<u64> {
+        self.0.to_asc_obj(heap)
+    }
+}
+
+impl FromAscObj<ArrayBuffer<u64>> for ethereum_types::U256 {
+    fn from_asc_obj<H: AscHeap>(array_buffer: &ArrayBuffer<u64>, heap: &H) -> Self {
+        ethereum_types::U256(<[u64; 4]>::from_asc_obj(array_buffer, heap))
     }
 }
--- thegraph-runtime/wasm_test/abi_classes.ts
+++ thegraph-runtime/wasm_test/abi_classes.ts
@@ -4,16 +4,15 @@ export { allocate_memory, free_memory };
 
 type Address = ArrayBuffer;
 
+const array_buffer_header_size = 8;
+
 // Clone the address to a new buffer, add 1 to the first and last bytes of the
 // address and return the new address.
 export function test_address(address: Address): Address {
   let new_address = address.slice();
-  
-  // ArrayBuffer header size.
-  let header_size = 8;
 
   // Add 1 to the first and last bytes.
-  let first_byte_ptr = changetype<usize>(new_address) + header_size;
+  let first_byte_ptr = changetype<usize>(new_address) + array_buffer_header_size;
   let first_byte = load<u8>(first_byte_ptr);
   store<u8>(first_byte_ptr, first_byte + 1);
 
@@ -22,6 +21,25 @@ export function test_address(address: Address): Address {
   let last_byte = load<u8>(last_byte_ptr);
   store<u8>(last_byte_ptr, last_byte + 1);
   return new_address
+}
+
+type Uint = ArrayBuffer;
+
+// Clone the Uint to a new buffer, add 1 to the first and last `u64`s and return
+// the new Uint.
+export function test_uint(address: Uint): ArrayBuffer {
+  let new_uint = address.slice();
+
+  // Add 1 to the first and last `u64`s.
+  let first_u64_ptr = changetype<usize>(new_uint) + array_buffer_header_size;
+  let first_u64 = load<u64>(first_u64_ptr);
+  store<u8>(first_u64_ptr, first_u64 + 1);
+
+  // An address has 4 u64s.
+  let last_u64_ptr = first_u64_ptr + 3*sizeof<u64>();
+  let last_u64 = load<u64>(last_u64_ptr);
+  store<u8>(last_u64_ptr, last_u64 + 1);
+  return new_uint
 }
 
 // Return the string repeated twice.
