bbad09ea6b123121ddb92cf16b1757d8af09fd56
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-28 20:59:48 +0200

    thegraph-runtime: Identify event handler and call it via WasmiModule

--- thegraph-runtime/src/host.rs
+++ thegraph-runtime/src/host.rs
@@ -11,6 +11,7 @@ use uuid::Uuid;
 
 use thegraph::components::data_sources::RuntimeHostEvent;
 use thegraph::components::ethereum::*;
+use thegraph::data::data_sources::DataSet;
 use thegraph::prelude::{
     RuntimeHost as RuntimeHostTrait, RuntimeHostBuilder as RuntimeHostBuilderTrait, *,
 };
@@ -28,18 +29,18 @@ where
 {
     logger: Logger,
     runtime: Handle,
-    ethereum_watcher: Arc<Mutex<T>>,
+    ethereum_adapter: Arc<Mutex<T>>,
 }
 
 impl<T> RuntimeHostBuilder<T>
 where
     T: EthereumAdapter,
 {
-    pub fn new(logger: &Logger, runtime: Handle, ethereum_watcher: Arc<Mutex<T>>) -> Self {
+    pub fn new(logger: &Logger, runtime: Handle, ethereum_adapter: Arc<Mutex<T>>) -> Self {
         RuntimeHostBuilder {
             logger: logger.new(o!("component" => "RuntimeHostBuilder")),
             runtime,
-            ethereum_watcher,
+            ethereum_adapter,
         }
     }
 }
@@ -48,13 +49,13 @@ impl<T> RuntimeHostBuilderTrait for RuntimeHostBuilder<T>
 where
     T: EthereumAdapter,
 {
-    type Host = RuntimeHost<T>;
+    type Host = RuntimeHost;
 
     fn build(&mut self, data_source_definition: DataSourceDefinition) -> Self::Host {
         RuntimeHost::new(
             &self.logger,
             self.runtime.clone(),
-            self.ethereum_watcher.clone(),
+            self.ethereum_adapter.clone(),
             RuntimeHostConfig {
                 data_source_definition,
             },
@@ -62,28 +63,21 @@ where
     }
 }
 
-pub struct RuntimeHost<T>
-where
-    T: EthereumAdapter,
-{
+pub struct RuntimeHost {
     config: RuntimeHostConfig,
-    logger: Logger,
-    runtime: Handle,
     output: Option<Receiver<RuntimeHostEvent>>,
-    ethereum_watcher: Arc<Mutex<T>>,
-    module: WasmiModule,
 }
 
-impl<T> RuntimeHost<T>
-where
-    T: EthereumAdapter,
-{
-    pub fn new(
+impl RuntimeHost {
+    pub fn new<T>(
         logger: &Logger,
         runtime: Handle,
-        ethereum_watcher: Arc<Mutex<T>>,
+        ethereum_adapter: Arc<Mutex<T>>,
         config: RuntimeHostConfig,
-    ) -> Self {
+    ) -> Self
+    where
+        T: EthereumAdapter,
+    {
         let logger = logger.new(o!("component" => "RuntimeHost"));
 
         // Create channel for sending runtime host events
@@ -111,56 +105,63 @@ where
                 data_source_id: String::from("TODO: DATA SOURCE ID"),
                 runtime: runtime.clone(),
                 event_sink: event_sender,
-                ethereum_watcher: ethereum_watcher.clone(),
+                ethereum_adapter: ethereum_adapter.clone(),
             },
         );
 
-        let mut host = RuntimeHost {
-            config,
+        Self::subscribe_to_events(
             logger,
             runtime,
-            output: Some(event_receiver),
-            ethereum_watcher,
+            config.data_source_definition.clone(),
             module,
-        };
+            ethereum_adapter,
+        );
 
-        host.subscribe_to_events();
-        host
+        RuntimeHost {
+            config: config,
+            output: Some(event_receiver),
+        }
     }
 
     /// Subscribe to all smart contract events of the first data set
     /// in the data source definition.
     ///
     /// NOTE: We'll add support for multiple datasets soon.
-    fn subscribe_to_events(&mut self) {
+    fn subscribe_to_events<T>(
+        logger: Logger,
+        runtime: Handle,
+        data_source: DataSourceDefinition,
+        module: WasmiModule,
+        ethereum_adapter: Arc<Mutex<T>>,
+    ) where
+        T: EthereumAdapter,
+    {
+        info!(logger, "Subscribe to events");
+
+        let dataset = data_source
+            .datasets
+            .first()
+            .expect("Data source must contain at least one data set")
+            .clone();
+
+        // Obtain the contract address of the first data set
+        let address = Address::from_str(dataset.data.address.as_str())
+            .expect("Failed to parse contract address");
+
+        // Load the main dataset contract
+        let contract_abi_path = dataset
+            .mapping
+            .abis
+            .iter()
+            .find(|abi| abi.name == dataset.data.structure.abi)
+            .map(|entry| data_source.resolve_path(&entry.source.path))
+            .expect("No ABI entry found for the main contract of the dataset");
+        let contract_abi_file = fs::File::open(contract_abi_path)
+            .expect("Contract ABI file does not exist or could not be opend for reading");
+        let contract = Contract::load(contract_abi_file).expect("Failed to parse contract ABI");
+
         // Prepare subscriptions for the events
         let subscription_results: Vec<EthereumEventSubscription> = {
-            let dataset = self.config
-                .data_source_definition
-                .datasets
-                .first()
-                .expect("Data source must contain at least one data set");
-
-            // Obtain the contract address of the first data set
-            let address = Address::from_str(dataset.data.address.as_str())
-                .expect("Failed to parse contract address");
-
-            // Load the main dataset contract
-            let contract_abi_path = dataset
-                .mapping
-                .abis
-                .iter()
-                .find(|abi| abi.name == dataset.data.structure.abi)
-                .map(|entry| {
-                    self.config
-                        .data_source_definition
-                        .resolve_path(&entry.source.path)
-                })
-                .expect("No ABI entry found for the main contract of the dataset");
-            let contract_abi_file = fs::File::open(contract_abi_path)
-                .expect("Contract ABI file does not exist or could not be opend for reading");
-            let contract = Contract::load(contract_abi_file).expect("Failed to parse contract ABI");
-
             // Prepare subscriptions for all events
             dataset
                 .mapping
@@ -188,30 +189,67 @@ where
                 .collect()
         };
 
+        // Protect the module for concurrent access
+        let protected_module = Arc::new(Mutex::new(module));
+        let protected_dataset = Arc::new(dataset);
+
         // Subscribe to the events now
         for subscription in subscription_results.into_iter() {
-            self.subscribe_to_event(subscription);
+            Self::subscribe_to_event(
+                logger.clone(),
+                runtime.clone(),
+                protected_module.clone(),
+                ethereum_adapter.clone(),
+                protected_dataset.clone(),
+                subscription,
+            );
         }
     }
 
-    fn subscribe_to_event(&mut self, subscription: EthereumEventSubscription) {
-        debug!(self.logger, "Subscribe to event"; "event" => format!("{:#?}", subscription));
+    fn subscribe_to_event<T>(
+        logger: Logger,
+        runtime: Handle,
+        module: Arc<Mutex<WasmiModule>>,
+        ethereum_adapter: Arc<Mutex<T>>,
+        dataset: Arc<DataSet>,
+        subscription: EthereumEventSubscription,
+    ) where
+        T: EthereumAdapter,
+    {
+        info!(logger, "Subscribe to event";
+              "name" => format!("{:#?}", subscription.event.name));
 
-        let receiver = self.ethereum_watcher
+        let receiver = ethereum_adapter
             .lock()
             .unwrap()
             .subscribe_to_event(subscription);
 
-        self.runtime.spawn(receiver.for_each(|event| {
-            self.module.handle_ethereum_event(event);
-        }));
+        let error_logger = logger.clone();
+        runtime.spawn(
+            receiver
+                .for_each(move |event| {
+                    let event_handler = dataset
+                        .mapping
+                        .event_handlers
+                        .iter()
+                        .find(|event_handler| {
+                            util::ethereum::string_to_h256(event_handler.event.as_str())
+                                == event.event_signature
+                        })
+                        .expect("Received an Ethereum event not mentioned in the data set");
+
+                    module
+                        .lock()
+                        .unwrap()
+                        .handle_ethereum_event(event_handler.handler.as_str(), event);
+                    Ok(())
+                })
+                .map_err(move |e| error!(error_logger, "Event subscription failed: {}", e)),
+        );
     }
 }
 
-impl<T> EventProducer<RuntimeHostEvent> for RuntimeHost<T>
-where
-    T: EthereumAdapter,
-{
+impl EventProducer<RuntimeHostEvent> for RuntimeHost {
     type EventStream = Receiver<RuntimeHostEvent>;
 
     fn take_event_stream(&mut self) -> Option<Self::EventStream> {
@@ -219,10 +257,7 @@ where
     }
 }
 
-impl<T> RuntimeHostTrait for RuntimeHost<T>
-where
-    T: EthereumAdapter,
-{
+impl RuntimeHostTrait for RuntimeHost {
     fn data_source_definition(&self) -> &DataSourceDefinition {
         &self.config.data_source_definition
     }
