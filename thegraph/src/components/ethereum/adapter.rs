c89ad28dab400884e73c393472d603bc4bb92a5f
Arseniy Klempner <adklempner@gmail.com>
2018-06-27 19:32:16 -0500

    Implements EthereumAdapter function for calling contracts through web3

--- thegraph/src/components/ethereum/adapter.rs
+++ thegraph/src/components/ethereum/adapter.rs
@@ -1,6 +1,6 @@
-use ethabi::{Bytes, Event, EventParam, LogParam};
+use ethabi::{Bytes, Event, EventParam, Function, LogParam, Token};
 use ethereum_types::{Address, H256};
-use futures::Stream;
+use futures::{Future, Stream};
 use web3::error::Error as Web3Error;
 use web3::types::BlockNumber;
 
@@ -20,6 +20,18 @@ pub struct EthereumContractState {
     pub address: Address,
     pub block_hash: H256,
     pub data: Bytes,
+}
+
+pub struct EthereumContractCallRequest {
+    pub address: Address,
+    pub block_number: Option<BlockNumber>,
+    pub function: Function,
+    pub args: Vec<Token>,
+}
+
+#[derive(Debug)]
+pub enum EthereumContractCallError {
+    Failed,
 }
 
 /// A range to allow event subscriptions to limit the block numbers to consider.
@@ -59,6 +71,11 @@ pub trait EthereumAdapter {
         &mut self,
         request: EthereumContractStateRequest,
     ) -> Result<EthereumContractState, EthereumContractStateError>;
+
+    fn contract_call(
+        &mut self,
+        request: EthereumContractCallRequest,
+    ) -> Box<Future<Item = Vec<Token>, Error = EthereumContractCallError>>;
 
     /// Subscribe to an event of a smart contract.
     fn subscribe_to_event(
