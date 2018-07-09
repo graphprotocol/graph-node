ca64637955b04162e3f1489e610d7f111b934e97
Ford Nickels <ford@thegraph.com>
2018-07-09 16:10:05 -0700

    thegraph-store-postgres-diesel: Create history tables
    One history table that tracks transactions and
    a second table for row level changes

--- thegraph-store-postgres-diesel/migrations/2018052400102_create_entities/up.sql
+++ thegraph-store-postgres-diesel/migrations/2018052400102_create_entities/up.sql
@@ -1,7 +1,35 @@
+--SEQUENCES
+DROP SEQUENCE IF EXISTS id_seq;
+CREATE SEQUENCE id_seq
+  INCREMENT BY 1
+  MINVALUE 0
+  MAXVALUE 2147483647
+  START WITH 1
+  CACHE 1
+  NO CYCLE
+  OWNED BY NONE;
+
+--TABLES
 CREATE TABLE IF NOT EXISTS entities (
      id VARCHAR NOT NULL,
      data_source VARCHAR NOT NULL,
      entity VARCHAR NOT NULL,
      data jsonb NOT NULL,
+     history_id INTEGER DEFAULT NEXTVAL('id_seq') UNIQUE NOT NULL,
      PRIMARY KEY (id, data_source, entity)
+ );
+
+CREATE TABLE IF NOT EXISTS table_event_history (
+    id SERIAL PRIMARY KEY,
+    transaction_id BIGINT,
+    transaction_time TIMESTAMP,
+    op_id SMALLINT
+);
+
+CREATE TABLE IF NOT EXISTS row_history (
+     id SERIAL PRIMARY KEY,
+     event_id BIGINT,
+     history_id INTEGER,
+     data_before JSONB,
+     data_after JSONB
  );
