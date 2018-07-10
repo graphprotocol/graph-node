c147798f5fcdb19d770cd5f81182eb1d098b7cf6
Ford Nickels <ford@thegraph.com>
2018-07-10 01:41:14 -0700

    thegraph-store-postgres-diesel: Create revert transaction fn
    Use revert_row_event and rever_transaction to revert all row events
    for given transaction
    also include unimplemented revert_transactions and revert_blocks

---
+++ thegraph-store-postgres-diesel/migrations/2018-07-10-062730_create_revert_procedures/down.sql
@@ -0,0 +1,4 @@
+DROP FUNCTION revert_row_event(INTEGER, INTEGER);
+DROP FUNCTION revert_transaction(INTEGER);
+DROP FUNCTION revert_transactions(INTEGER, INTEGER);
+DROP FUNCTION revert_blocks(VARCHAR, VARCHAR);
