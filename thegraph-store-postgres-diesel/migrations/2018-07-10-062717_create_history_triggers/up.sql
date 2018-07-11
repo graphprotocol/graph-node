915b4efd4504d845a6ae066ea20613d28197ebde
Ford Nickels <ford@thegraph.com>
2018-07-10 23:02:16 -0700

    thegraph-store-postgres-diesel: Add fields: block_hash and reverted
    Reverted field added to row_history table: a flag to mark history rows
    as representing a revert operation not the result of an ethereum event
    block_hash field added to entities table: will be needed to specify
    actions by block
    Also made some changes to improve readablility and detail of comments

--- thegraph-store-postgres-diesel/migrations/2018-07-10-062717_create_history_triggers/up.sql
+++ thegraph-store-postgres-diesel/migrations/2018-07-10-062717_create_history_triggers/up.sql
@@ -1,4 +1,9 @@
---STORED PROCEDURES
+/**************************************************************
+* CREATE TRIGGER FUNCTIONS
+**************************************************************/
+
+-- Writes metadata of each transaction on the entities table
+-- data written to table_event_history table */
 CREATE OR REPLACE FUNCTION log_transaction()
     RETURNS TRIGGER AS
 $$
@@ -11,6 +16,7 @@ BEGIN
         WHEN 'DELETE' THEN operation_id := 2;
     END CASE;
 
+    -- Insert postgres transaction info into table_event_history
     INSERT INTO table_event_history
         (transaction_id, transaction_time, op_id)
     VALUES
@@ -20,27 +26,34 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- Writes row level metadata and before & after state of `data` to row_history
+-- Called when after_update_trigger is fired.
 CREATE OR REPLACE FUNCTION log_update()
     RETURNS trigger AS
 $$
 DECLARE
     e_id INTEGER;
     b_hash VARCHAR;
+    is_reversion BOOLEAN;
 BEGIN
+    -- Get corresponding table event id
     SELECT
         id INTO e_id
     FROM table_event_history
     WHERE
         transaction_id = txid_current();
-
+    is_reversion := FALSE;
+    -- Log row metadata and changes
     INSERT INTO row_history
-        (event_id, entity_id, data_source, entity, data_before, data_after)
+        (event_id, entity_id, data_source, entity, data_before, data_after, reversion)
     VALUES
-        (e_id, OLD.id, OLD.data_source, OLD.entity, OLD.data, NEW.data);
+        (e_id, OLD.id, OLD.data_source, OLD.entity, OLD.data, NEW.data, is_reversion);
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
+-- Writes out newly inserted row to row_history
+-- Called when after_update_trigger is fired.
 CREATE OR REPLACE FUNCTION log_insert()
     RETURNS trigger AS
 $$
@@ -48,12 +61,14 @@ DECLARE
     e_id INTEGER;
     b_hash VARCHAR;
 BEGIN
+    -- Get corresponding table event id
     SELECT
         id INTO e_id
     FROM table_event_history
     WHERE
         transaction_id = txid_current();
 
+    -- Log inserted row
     INSERT INTO row_history
         (event_id, entity_id, data_source, entity, data_before, data_after)
     VALUES
@@ -62,6 +77,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- Writes deleted row to row_history
+-- Called when after_update_trigger is fired.
 CREATE OR REPLACE FUNCTION log_delete()
     RETURNS trigger AS
 $$
@@ -69,12 +86,14 @@ DECLARE
     e_id INTEGER;
     b_hash VARCHAR;
 BEGIN
+    -- Get corresponding table event id
     SELECT
         id INTO e_id
     FROM table_event_history
     WHERE
         transaction_id = txid_current();
 
+    -- Log content of deleted row
     INSERT INTO row_history
         (event_id, entity_id, data_source, entity, data_before, data_after)
     VALUES
@@ -83,7 +102,9 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
---TRIGGERS
+/**************************************************************
+* CREATE TRIGGERS
+**************************************************************/
 CREATE TRIGGER before_transaction_trigger
     BEFORE INSERT OR UPDATE OR DELETE
     ON entities
