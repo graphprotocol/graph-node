775aaf8b689c9325ee6feb5303141319092909cc
Leonardo Yvens Schwarzstein <leoyvens@gmail.com>
2018-07-10 22:38:25 -0300

    thegraph, -core: Take `Link` insteado of `str` in `cat`

--- thegraph/src/components/link_resolver.rs
+++ thegraph/src/components/link_resolver.rs
@@ -1,17 +1,19 @@
 use failure;
 use futures::prelude::*;
+use data::data_sources::Link;
 use ipfs_api;
 use std::error::Error;
 
 /// Resolves links to data source definitions and resources referenced by them.
 pub trait LinkResolver {
     /// Fetches the link contents as bytes.
-    fn cat(&self, link: &str) -> Box<Future<Item = Vec<u8>, Error = Box<Error>>>;
+    fn cat(&self, link: &Link) -> Box<Future<Item = Vec<u8>, Error = Box<Error>>>;
 }
 
 impl LinkResolver for ipfs_api::IpfsClient {
     /// Currently supports only links of the form `/ipfs/ipfs_hash`
-    fn cat(&self, link: &str) -> Box<Future<Item = Vec<u8>, Error = Box<Error>>> {
+    fn cat(&self, link: &Link) -> Box<Future<Item = Vec<u8>, Error = Box<Error>>> {
+        let link = &link.link;
         // Verify that the link is in the expected form `/ipfs/hash`.
         if !link.starts_with("/ipfs/") {
             return Box::new(
