59ba45d7e59d1f49d64812a556fd01ced69a7ff9
Ford Nickels <ford@thegraph.com>
2018-07-10 23:05:20 -0700

    thegraph-store-postgres-diesel: Add revert_transaction_group function
    Also some cleanup: more descriptive commments, better function naming
    (do not simply add s for plural form of sql procedure names), use
    variable names in function rather than $1, $2, $3 etc...

--- thegraph-store-postgres-diesel/migrations/2018-07-10-062730_create_revert_procedures/down.sql
+++ thegraph-store-postgres-diesel/migrations/2018-07-10-062730_create_revert_procedures/down.sql
@@ -1,4 +1,8 @@
+/**************************************************************
+* DROP FUNCTIONS
+**************************************************************/
+DROP FUNCTION jsonb_diff_val(JSONB, JSONB);
 DROP FUNCTION revert_row_event(INTEGER, INTEGER);
 DROP FUNCTION revert_transaction(INTEGER);
-DROP FUNCTION revert_transactions(INTEGER, INTEGER);
-DROP FUNCTION revert_blocks(VARCHAR, VARCHAR);
+DROP FUNCTION revert_transaction_group(INTEGER[]);
+DROP FUNCTION revert_block(VARCHAR);
