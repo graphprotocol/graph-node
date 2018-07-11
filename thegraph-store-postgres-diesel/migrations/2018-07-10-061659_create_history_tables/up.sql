6cc2a74e4f61d40e874f91075e9deea29b95719c
Ford Nickels <ford@thegraph.com>
2018-07-10 23:02:16 -0700

    thegraph-store-postgres-diesel: Add fields: block_hash and reverted
    Reverted field added to row_history table: a flag to mark history rows
    as representing a revert operation not the result of an ethereum event
    block_hash field added to entities table: will be needed to specify
    actions by block
    Also made some changes to improve readablility and detail of comments

--- thegraph-store-postgres-diesel/migrations/2018-07-10-061659_create_history_tables/up.sql
+++ thegraph-store-postgres-diesel/migrations/2018-07-10-061659_create_history_tables/up.sql
@@ -1,4 +1,7 @@
--- TABLES
+/**************************************************************
+* CREATE TABLES
+**************************************************************/
+-- Stores the metadata from each sql transaction
 CREATE TABLE IF NOT EXISTS table_event_history (
     id SERIAL PRIMARY KEY,
     transaction_id BIGINT,
@@ -6,6 +9,7 @@ CREATE TABLE IF NOT EXISTS table_event_history (
     op_id SMALLINT
 );
 
+-- Stores the row level data and changes (1 or more per transaction)
 CREATE TABLE IF NOT EXISTS row_history (
      id SERIAL PRIMARY KEY,
      event_id BIGINT NOT NULL,
@@ -13,10 +17,14 @@ CREATE TABLE IF NOT EXISTS row_history (
      data_source VARCHAR NOT NULL,
      entity VARCHAR NOT NULL,
      data_before JSONB,
-     data_after JSONB
+     data_after JSONB,
+     reversion BOOLEAN NOT NULL DEFAULT FALSE
  );
 
---FOREIGN KEYS
+ /**************************************************************
+ * ADD FOREIGN KEYS
+ **************************************************************/
+ -- Define relationship between table_event_history and row_history
 ALTER TABLE row_history
     ADD CONSTRAINT row_history_table_event_id_fk
     FOREIGN KEY (event_id)
