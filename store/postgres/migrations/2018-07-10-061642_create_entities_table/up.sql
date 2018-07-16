ec465ec7db0894f1e827b17678101495a62a43d8
Ford Nickels <ford@thegraph.com>
2018-07-16 16:55:36 -0700

    thegraph-store-postgres-diesel: Write reversion flag; Track block hash

--- store/postgres/migrations/2018-07-10-061642_create_entities_table/up.sql
+++ store/postgres/migrations/2018-07-10-061642_create_entities_table/up.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS entities (
      id VARCHAR NOT NULL,
      data_source VARCHAR NOT NULL,
      entity VARCHAR NOT NULL,
-     block_hash VARCHAR(40) DEFAULT NULL,
      data jsonb NOT NULL,
+     latest_block_hash VARCHAR(40) DEFAULT NULL,
      PRIMARY KEY (id, data_source, entity)
  );
