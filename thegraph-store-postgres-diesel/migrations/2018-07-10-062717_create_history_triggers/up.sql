812f4ff253d15123479baa971f1b5e4c1f7ac981
Ford Nickels <ford@thegraph.com>
2018-07-16 13:50:42 -0700

    thegraph-store-postgres-diesel: Rename history tables, rename fields
    Tables:
    row_history --> entity_history
    table_event_data --> event_meta_data
    Fields:
    transaction_id --> db_transaction_id
    e_id --> event_id

--- thegraph-store-postgres-diesel/migrations/2018-07-10-062717_create_history_triggers/up.sql
+++ thegraph-store-postgres-diesel/migrations/2018-07-10-062717_create_history_triggers/up.sql
@@ -6,7 +6,7 @@
 * LOG TRANSACTION
 *
 * Writes metadata of each transaction on the entities table
-* Data is written to table_event_history
+* Data is written to event_meta_data
 * Called when before_transaction_trigger is fired
 **************************************************************/
 CREATE OR REPLACE FUNCTION log_transaction()
@@ -21,9 +21,9 @@ BEGIN
         WHEN 'DELETE' THEN operation_id := 2;
     END CASE;
 
-    -- Insert postgres transaction info into table_event_history
-    INSERT INTO table_event_history
-        (transaction_id, transaction_time, op_id)
+    -- Insert postgres transaction info into event_meta_data
+    INSERT INTO event_meta_data
+        (db_transaction_id, transaction_time, op_id)
     VALUES
         (txid_current(), statement_timestamp(), operation_id);
 
@@ -34,29 +34,28 @@ $$ LANGUAGE plpgsql;
 /**************************************************************
 * LOG UPDATE
 *
-* Writes row level metadata and before & after state of `data` to row_history
+* Writes row level metadata and before & after state of `data` to entity_history
 * Called when after_update_trigger is fired.
 **************************************************************/
 CREATE OR REPLACE FUNCTION log_update()
     RETURNS trigger AS
 $$
 DECLARE
-    e_id INTEGER;
-    b_hash VARCHAR;
+    event_id INTEGER;
     is_reversion BOOLEAN;
 BEGIN
     -- Get corresponding table event id
     SELECT
-        id INTO e_id
-    FROM table_event_history
+        id INTO event_id
+    FROM event_meta_data
     WHERE
-        transaction_id = txid_current();
+        db_transaction_id = txid_current();
     is_reversion := FALSE;
     -- Log row metadata and changes
-    INSERT INTO row_history
+    INSERT INTO entity_history
         (event_id, entity_id, data_source, entity, data_before, data_after, reversion)
     VALUES
-        (e_id, OLD.id, OLD.data_source, OLD.entity, OLD.data, NEW.data, is_reversion);
+        (event_id, OLD.id, OLD.data_source, OLD.entity, OLD.data, NEW.data, is_reversion);
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
@@ -64,28 +63,27 @@ $$ LANGUAGE plpgsql;
 /**************************************************************
 * LOG INSERT
 *
-* Writes out newly inserted row to row_history
+* Writes out newly inserted row to entity_history
 * Called when after_insert_trigger is fired.
 **************************************************************/
 CREATE OR REPLACE FUNCTION log_insert()
     RETURNS trigger AS
 $$
 DECLARE
-    e_id INTEGER;
-    b_hash VARCHAR;
+    event_id INTEGER;
 BEGIN
     -- Get corresponding table event id
     SELECT
-        id INTO e_id
-    FROM table_event_history
+        id INTO event_id
+    FROM event_meta_data
     WHERE
-        transaction_id = txid_current();
+        db_transaction_id = txid_current();
 
     -- Log inserted row
-    INSERT INTO row_history
+    INSERT INTO entity_history
         (event_id, entity_id, data_source, entity, data_before, data_after)
     VALUES
-        (e_id, NEW.id, NEW.data_source, NEW.entity, NULL, NEW.data);
+        (event_id, NEW.id, NEW.data_source, NEW.entity, NULL, NEW.data);
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
@@ -93,28 +91,27 @@ $$ LANGUAGE plpgsql;
 /**************************************************************
 * LOG DELETE
 *
-* Writes deleted row to row_history
+* Writes deleted row to entity_history
 * Called when after_delete_trigger is fired.
 **************************************************************/
 CREATE OR REPLACE FUNCTION log_delete()
     RETURNS trigger AS
 $$
 DECLARE
-    e_id INTEGER;
-    b_hash VARCHAR;
+    event_id INTEGER;
 BEGIN
     -- Get corresponding table event id
     SELECT
-        id INTO e_id
-    FROM table_event_history
+        id INTO event_id
+    FROM event_meta_data
     WHERE
-        transaction_id = txid_current();
+        db_transaction_id = txid_current();
 
     -- Log content of deleted row
-    INSERT INTO row_history
+    INSERT INTO entity_history
         (event_id, entity_id, data_source, entity, data_before, data_after)
     VALUES
-        (e_id, OLD.id, OLD.data_source, OLD.entity, OlD.data, NULL);
+        (event_id, OLD.id, OLD.data_source, OLD.entity, OlD.data, NULL);
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
