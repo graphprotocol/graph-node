858c4f6de700edaec79ea49c028b85d9e84759b3
Ford Nickels <ford@thegraph.com>
2018-07-16 15:01:21 -0700

    thegraph-store-postgres-diesel: Create revert block fn and rerun fns

--- store/postgres/migrations/2018-07-10-062730_create_revert_procedures/up.sql
+++ store/postgres/migrations/2018-07-10-062730_create_revert_procedures/up.sql
@@ -126,15 +126,141 @@ END;
 $$ LANGUAGE plpgsql;
 
 /**************************************************************
+* RERUN ROW EVENT
+*
+* Rerun a specific row level event
+* Parameters: entity_history pkey (id) and operation type
+**************************************************************/
+CREATE OR REPLACE FUNCTION rerun_row_event(input_entity_history_id INTEGER, input_operation_id INTEGER)
+    RETURNS VOID AS
+$$
+DECLARE
+    target_entity_id VARCHAR;
+    target_data_source VARCHAR;
+    target_entity VARCHAR;
+    target_data_after JSONB;
+BEGIN
+    SELECT
+        entity_id,
+        data_source,
+        entity,
+        data_before,
+        data_after
+    INTO
+        target_entity_id,
+        target_data_source,
+        target_entity,
+        target_data_after
+    FROM entity_history
+    WHERE entity_history.id = input_entity_history_id;
+
+    CASE
+        -- INSERT or UPDATE case
+        WHEN input_operation_id IN (0,1) THEN
+            -- Re insert row
+            -- If row exists perform update
+            BEGIN
+                EXECUTE
+                    'INSERT INTO entities (data_source, entity, id, data)
+                        VALUES ($1, $2, $3, $4)
+                        ON CONFLICT (data_source, entity, id) DO UPDATE
+                        SET data = $4'
+                USING
+                    target_data_source,
+                    target_entity,
+                    target_entity_id,
+                    target_data_after;
+            END;
+
+        -- DELETE case
+        WHEN input_operation_id = 2 THEN
+            -- Insert deleted row if not exists
+            -- If row exists perform update
+            BEGIN
+                EXECUTE
+                    'DELETE FROM entities WHERE (
+                        data_source = $1 AND
+                        entity = $2 AND
+                        id = $3)'
+                USING
+                    target_data_source,
+                    target_entity;
+                    target_entity_id,
+            END;
+    END CASE;
+END;
+$$ LANGUAGE plpgsql;
+
+/**************************************************************
+* RERUN ENTITY
+*
+* Rerun all events for a specific entity
+* avoiding any revert or uncled events
+* Parameters: entity pkey -> (entity_id, data_source, entity)
+              event_id of revert event
+**************************************************************/
+CREATE OR REPLACE FUNCTION rerun_entity(
+    input_event_id INTEGER, input_data_source VARCHAR, input_entity VARCHAR, input_entity_id VARCHAR)
+    RETURNS VOID AS
+$$
+BEGIN
+     FOR row IN
+        SELECT
+            entity_history.id as id,
+            event_meta_data.op_id as op_id
+        FROM entity_history
+        WHERE (
+            entity_history.id = input_entity_id AND
+            entity_history.event_id = input_event_id AND
+            entity_history.data_source = input_data_source
+            AND
+            entity_history.event_id > input_event_id
+            AND
+            entity_history.reversion = FALSE )
+        ORDER BY entity_history.id ASC
+        LOOP
+            PERFORM rerun_row_event(row.id, row.op_id);
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+/**************************************************************
 * REVERT BLOCK
 *
 * Revert the row store events related to a particular block
+* Rerun all of an entities changes that come after the row store events related to that block
 * Parameters: block_hash
 **************************************************************/
 CREATE OR REPLACE FUNCTION revert_block(block_hash VARCHAR)
     RETURNS VOID AS
 $$
+DECLARE
+    row RECORD;
+    target_data_source VARCHAR;
+    target_entity VARCHAR;
+    target_entity_id VARCHAR;
 BEGIN
-    NULL;
+    FOR row IN
+        SELECT
+            entity_history.event_id as event_id,
+            entity_history.data_source as data_source,
+            entity_history.entity as entity,
+            entity_history.entity_id as entity_id
+        INTO
+            event_id
+        FROM entity_history
+        JOIN entities ON (
+            entity_history.data_source = entities.data_source AND
+            entity_history.entity = entities.entity AND
+            entity_history.entity_id = entities.id )
+        GROUP BY
+            entity_history.event_id
+        ORDER BY
+            entity_history.event_id DESC
+        LOOP
+            PERFORM revert_transaction(row.event_id);
+    END LOOP;
+    PERFORM rerun_entity(row.event_id, row.data_source, row.entity, row.entity_id);
 END;
 $$ LANGUAGE plpgsql;
+
