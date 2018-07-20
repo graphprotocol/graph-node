c81e8f447e62909b1c462c82b6a6423de014b3bb
Ford Nickels <ford@thegraph.com>
2018-07-19 17:55:15 -0700

    thegraph-store-postgres-diesel: Generalize block hash as event source

--- store/postgres/migrations/2018-07-10-061659_create_history_tables/up.sql
+++ store/postgres/migrations/2018-07-10-061659_create_history_tables/up.sql
@@ -4,10 +4,10 @@
 -- Stores the metadata from each SQL transaction
 CREATE TABLE IF NOT EXISTS event_meta_data (
     id SERIAL PRIMARY KEY,
-    db_transaction_id BIGINT NOT NULL,
+    db_transaction_id BIGINT NOT NULL UNIQUE,
     db_transaction_time TIMESTAMP NOT NULL,
     op_id SMALLINT NOT NULL,
-    block_hash VARCHAR(40) DEFAULT NULL
+    source VARCHAR(40) DEFAULT NULL
 );
 
 -- Stores the row level data and changes (1 or more per SQL transaction)
