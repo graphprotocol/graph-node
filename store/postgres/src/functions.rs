f81f0779a705e222a1023163d670414f95acdc60
Ford Nickels <ford@thegraph.com>
2018-07-19 19:40:52 -0700

    thegraph-store-postgres-diesel: Use set_config() to save event_source

--- store/postgres/src/functions.rs
+++ store/postgres/src/functions.rs
@@ -3,3 +3,11 @@ use diesel::sql_types::*;
 sql_function! {
     fn revert_block_group(block_hashes: Array<Text>);
 }
+
+sql_function! {
+    fn current_setting(setting_name: Text) -> Text;
+}
+
+sql_function! {
+    fn set_config(setting_name: Text, new_value: Text, is_local: Bool);
+}
