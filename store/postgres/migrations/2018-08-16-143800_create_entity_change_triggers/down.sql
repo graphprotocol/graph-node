75785f34bf666be60242d15c075330cc701d9b7b
Jannis Pohlmann <contact@jannispohlmann.de>
2018-08-16 15:02:09 +0200

    store/postgres: Add triggers that emit entity change notifications
    These are broadcast on an `entity_changes` channel as JSON objects.

---
+++ store/postgres/migrations/2018-08-16-143800_create_entity_change_triggers/down.sql
@@ -0,0 +1,15 @@
+/**************************************************************
+ * DROP TRIGGERS
+ **************************************************************/
+
+DROP TRIGGER entity_added;
+DROP TRIGGER entity_updated;
+DROP TRIGGER entity_removed;
+
+/**************************************************************
+ * DROP FUNCTIONS
+ **************************************************************/
+
+DROP FUNCTION notify_entity_added;
+DROP FUNCTION notify_entity_updated;
+DROP FUNCTION notify_entity_removed;
