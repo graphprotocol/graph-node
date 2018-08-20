c749dff551452cf9dc734f685adf0b475003f174
Leonardo Yvens Schwarzstein <leoyvens@gmail.com>
2018-08-20 16:46:31 -0300

    core/tests: Fix and un-ignore test, install IPFS on Travis
    This test depends on IPFS so it was ignored,
    install IPFS on Travis so we can run it on CI.

--- core/tests/tests.rs
+++ core/tests/tests.rs
@@ -21,7 +21,6 @@ use std::time::Duration;
 use std::time::Instant;
 
 #[test]
-#[ignore]
 fn multiple_data_sources_per_subgraph() {
     struct MockEthereumAdapter {
         received_subscriptions: Vec<String>,
@@ -56,61 +55,75 @@ fn multiple_data_sources_per_subgraph() {
             .map_err(|err| eprintln!("error adding to IPFS {}", err))
     }
 
-    // Replace "link to" placeholders in the subgraph manifest with hashes
-    // of files just added into a local IPFS daemon on port 5001.
-    let resolver = Arc::new(IpfsClient::default());
-    let mut subgraph_string =
-        std::fs::read_to_string("tests/subgraph-two-datasources/two-datasources.yaml").unwrap();
-    for file in &[
-        "abis/ExampleContract.json",
-        "abis/ExampleContract2.json",
-        "empty.wasm",
-        "schema.graphql",
-    ] {
-        let link = add(
-            &resolver,
-            read_to_string(format!("tests/subgraph-two-datasources/{}", file)).unwrap(),
-        ).wait()
-            .unwrap();
-        subgraph_string =
-            subgraph_string.replace(&format!("link to {}", file), &format!("/ipfs/{}", link));
-    }
-    let subgraph_link = add(&resolver, subgraph_string).wait().unwrap();
+    let mut runtime = tokio::runtime::Runtime::new().unwrap();
+    runtime
+        .block_on(futures::lazy(|| {
+            // Replace "link to" placeholders in the subgraph manifest with hashes
+            // of files just added into a local IPFS daemon on port 5001.
+            let resolver = Arc::new(IpfsClient::default());
+            let mut subgraph_string = std::fs::read_to_string(
+                "tests/subgraph-two-datasources/two-datasources.yaml",
+            ).unwrap();
+            for file in &[
+                "abis/ExampleContract.json",
+                "abis/ExampleContract2.json",
+                "empty.wasm",
+                "schema.graphql",
+            ] {
+                let link = add(
+                    &resolver,
+                    read_to_string(format!("tests/subgraph-two-datasources/{}", file)).unwrap(),
+                ).wait()
+                    .unwrap();
+                subgraph_string = subgraph_string
+                    .replace(&format!("link to {}", file), &format!("/ipfs/{}", link));
+            }
 
-    let logger = logger();
-    let eth_adapter = Arc::new(Mutex::new(MockEthereumAdapter {
-        received_subscriptions: vec![],
-    }));
-    let host_builder = RuntimeHostBuilder::new(&logger, eth_adapter.clone(), resolver.clone());
+            let subgraph_link = add(&resolver, subgraph_string).wait().unwrap();
 
-    let fake_store = Arc::new(Mutex::new(FakeStore));
-    let manager = RuntimeManager::new(&logger, fake_store, host_builder);
+            let logger = Logger::root(slog::Discard, o!());
+            let eth_adapter = Arc::new(Mutex::new(MockEthereumAdapter {
+                received_subscriptions: vec![],
+            }));
+            let host_builder =
+                RuntimeHostBuilder::new(&logger, eth_adapter.clone(), resolver.clone());
 
-    // Load a subgraph with two data sets, one listening for `ExampleEvent`
-    // and the other for `ExampleEvent2`.
-    let subgraph = SubgraphManifest::resolve(
-        Link {
-            link: subgraph_link,
-        },
-        resolver,
-    ).wait()
-        .expect("failed to load subgraph");
+            let fake_store = Arc::new(Mutex::new(FakeStore));
+            let manager = RuntimeManager::new(&logger, fake_store, host_builder);
 
-    // Send the new subgraph to the manager.
-    manager
-        .event_sink()
-        .send(SubgraphProviderEvent::SubgraphAdded(subgraph))
-        .wait()
+            // Load a subgraph with two data sets, one listening for `ExampleEvent`
+            // and the other for `ExampleEvent2`.
+            SubgraphManifest::resolve(
+                Link {
+                    link: subgraph_link,
+                },
+                resolver,
+            ).map_err(|e| panic!("subgraph resolve error {:?}", e))
+                .and_then(move |subgraph| {
+                    // Send the new subgraph to the manager.
+                    manager
+                        .event_sink()
+                        .send(SubgraphProviderEvent::SubgraphAdded(subgraph))
+                })
+                .and_then(move |_| {
+                    // If we subscribed to both events, then we're handling multiple data sets.
+                    // Wait for thirty seconds for that to happen, otherwise fail the test.
+                    let start_time = Instant::now();
+                    let max_wait = Duration::from_secs(30);
+                    loop {
+                        let subscriptions = &eth_adapter.lock().unwrap().received_subscriptions;
+                        if subscriptions.contains(&"ExampleEvent".to_owned())
+                            && subscriptions.contains(&"ExampleEvent2".to_owned())
+                        {
+                            break;
+                        }
+                        if Instant::now().duration_since(start_time) > max_wait {
+                            panic!("Test failed, events subscribed to: {:?}", subscriptions)
+                        }
+                        ::std::thread::yield_now();
+                    }
+                    Ok(())
+                })
+        }))
         .unwrap();
-
-    // If we subscribed to both events, then we're handling multiple data sets.
-    // Wait for one second for that to happen, otherwise fail the test.
-    let start_time = Instant::now();
-    let max_wait = Duration::from_secs(1);
-    while eth_adapter.lock().unwrap().received_subscriptions != ["ExampleEvent", "ExampleEvent2"] {
-        ::std::thread::yield_now();
-        if Instant::now().duration_since(start_time) > max_wait {
-            panic!("Test failed, events not subscribed to.")
-        }
-    }
 }
