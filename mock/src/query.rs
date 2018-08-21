a0d63ca4c77cafad9ed93cc424a319e2e551588d
Jannis Pohlmann <contact@jannispohlmann.de>
2018-08-21 16:57:41 +0200

    graph, mock, core: Require QueryRunner to be called to run queries
    This is changes how components interact with the QueryRunner. Instead
    of pushing queries with result senders into a stream processed by
    the QueryRunner, other components now call the QueryRunner directly
    and get a query result future back in return.

--- mock/src/query.rs
+++ mock/src/query.rs
@@ -1,64 +1,43 @@
-use futures::sync::mpsc::{channel, Receiver, Sender};
+use futures::future;
+use futures::prelude::*;
 use graphql_parser::query as gqlq;
 use std::collections::BTreeMap;
 
 use graph::prelude::*;
 
 /// A mock `QueryRunner`.
-pub struct MockQueryRunner<S> {
+pub struct MockQueryRunner {
     logger: slog::Logger,
-    query_sink: Sender<Query>,
-    _store: Arc<S>,
 }
 
-impl<S> MockQueryRunner<S>
-where
-    S: Store + Sized + 'static,
-{
+impl MockQueryRunner {
     /// Creates a new mock `QueryRunner`.
-    pub fn new(logger: &slog::Logger, store: S) -> Self {
-        let (sink, stream) = channel(100);
-        let runner = MockQueryRunner {
+    pub fn new(logger: &slog::Logger) -> Self {
+        MockQueryRunner {
             logger: logger.new(o!("component" => "MockQueryRunner")),
-            query_sink: sink,
-            _store: Arc::new(store),
-        };
-        runner.run_queries(stream);
-        runner
-    }
-
-    /// Spawns a Tokio task to run any queries received through the given stream.
-    fn run_queries(&self, stream: Receiver<Query>) {
-        info!(self.logger, "Preparing to run queries");
-
-        let logger = self.logger.clone();
-
-        tokio::spawn(stream.for_each(move |query| {
-            info!(logger, "Running query"; "query" => format!("{:?}", query));
-
-            // Here we would access the store.
-            let mut data = BTreeMap::new();
-            data.insert(
-                String::from("allUsers"),
-                gqlq::Value::String("placeholder".to_string()),
-            );
-            data.insert(
-                String::from("allItems"),
-                gqlq::Value::String("placeholder".to_string()),
-            );
-            let data = gqlq::Value::Object(data);
-
-            query
-                .result_sender
-                .send(QueryResult::new(Some(data)))
-                .expect("Failed to send query result back");
-            Ok(())
-        }));
+        }
     }
 }
 
-impl<S> QueryRunner for MockQueryRunner<S> {
-    fn query_sink(&mut self) -> Sender<Query> {
-        self.query_sink.clone()
+impl QueryRunner for MockQueryRunner {
+    fn run_query(
+        &self,
+        query: Query,
+    ) -> Box<Future<Item = QueryResult, Error = QueryError> + Send> {
+        info!(self.logger, "Run query"; "query" => format!("{:?}", query));
+
+        // Here we would access the store.
+        let mut data = BTreeMap::new();
+        data.insert(
+            String::from("allUsers"),
+            gqlq::Value::String("placeholder".to_string()),
+        );
+        data.insert(
+            String::from("allItems"),
+            gqlq::Value::String("placeholder".to_string()),
+        );
+        let data = gqlq::Value::Object(data);
+
+        Box::new(future::ok(QueryResult::new(Some(data))))
     }
 }
