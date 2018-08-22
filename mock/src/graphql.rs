f4a360caf5068f18565a2dcb43f806adc0a7634d
Jannis Pohlmann <contact@jannispohlmann.de>
2018-08-22 18:03:07 +0200

    core, mock: Implement subscriptions in the core/mock GraphQLRunner

--- mock/src/graphql.rs
+++ mock/src/graphql.rs
@@ -1,3 +1,6 @@
+use futures::future;
+use futures::prelude::*;
+use futures::sync::mpsc::channel;
 use graphql_parser::query as gqlq;
 use std::collections::BTreeMap;
 
@@ -17,11 +20,8 @@ impl MockGraphQLRunner {
     }
 }
 
-impl QueryRunner for MockQueryRunner {
-    fn run_query(
-        &self,
-        query: Query,
-    ) -> Box<Future<Item = QueryResult, Error = QueryError> + Send> {
+impl GraphQLRunner for MockGraphQLRunner {
+    fn run_query(&self, query: Query) -> QueryResultFuture {
         info!(self.logger, "Run query"; "query" => format!("{:?}", query));
 
         // Here we would access the store.
@@ -37,5 +37,11 @@ impl QueryRunner for MockQueryRunner {
         let data = gqlq::Value::Object(data);
 
         Box::new(future::ok(QueryResult::new(Some(data))))
+    }
+
+    fn run_subscription(&self, subscription: Subscription) -> SubscriptionResultFuture {
+        info!(self.logger, "Run subscription"; "subscription" => format!("{:?}", subscription));
+        let (_, receiver) = channel(2);
+        Box::new(future::ok(SubscriptionResult::new(Box::new(receiver))))
     }
 }
