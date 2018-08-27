d67a0c0f065ca263e38483b7632f883cd084679a
Ford Nickels <ford@thegraph.com>
2018-08-27 13:50:41 -0700

    graph/util/log: Add reusable register_panic_hook function

--- graph/src/util/log.rs
+++ graph/src/util/log.rs
@@ -1,6 +1,7 @@
 use slog;
 use slog_async;
 use slog_term;
+use std::panic;
 
 use slog::Drain;
 
@@ -16,4 +17,19 @@ pub fn guarded_logger() -> (slog::Logger, slog_async::AsyncGuard) {
     let drain = slog_term::CompactFormat::new(decorator).build().fuse();
     let (drain, guard) = slog_async::Async::new(drain).build_with_guard();
     (slog::Logger::root(drain.fuse(), o!()), guard)
+}
+
+pub fn register_panic_hook(panic_logger: slog::Logger) {
+    panic::set_hook(Box::new(move |panic_info| {
+        let panic_payload = panic_info.payload().downcast_ref::<String>();
+        let panic_location = if let Some(location) = panic_info.location() {
+            format!("{}:{}", location.file(), location.line().to_string())
+        } else {
+            "NA".to_string()
+        };
+        error!(panic_logger, "Node error";
+            "error" => panic_payload.clone(),
+            "location" => panic_location.clone()
+           );
+    }));
 }
