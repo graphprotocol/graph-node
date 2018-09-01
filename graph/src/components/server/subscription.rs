04afeab882ce2ed7e70289d102f1a45a00ff7472
Jannis Pohlmann <contact@jannispohlmann.de>
2018-09-01 14:04:25 +0200

    graph: Add a SubscriptionServer trait

--- graph/src/components/server/subscription.rs
+++ graph/src/components/server/subscription.rs
@@ -1,1 +1,15 @@
+use futures::prelude::*;
 
+use components::subgraph::SchemaEvent;
+use components::EventConsumer;
+
+/// Common trait for GraphQL subscription servers.
+pub trait SubscriptionServer: EventConsumer<SchemaEvent> {
+    type ServeError;
+
+    /// Returns a Future that, when spawned, brings up the GraphQL subscription server.
+    fn serve(
+        &mut self,
+        port: u16,
+    ) -> Result<Box<Future<Item = (), Error = ()> + Send>, Self::ServeError>;
+}
