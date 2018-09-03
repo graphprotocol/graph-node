3bc0a6b8745f467818455312ea3c7c58e668cc22
Jannis Pohlmann <contact@jannispohlmann.de>
2018-09-03 16:16:20 +0200

    graphql: Add validate_schema() function and SchemaValidationError
    This function should be used to reject subgraphs with invalid schemas.

--- graphql/src/lib.rs
+++ graphql/src/lib.rs
@@ -4,6 +4,10 @@ extern crate graphql_parser;
 extern crate indexmap;
 extern crate inflector;
 extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate failure;
 
 /// Utilities for working with GraphQL schemas.
 pub mod schema;
@@ -31,7 +35,7 @@ pub mod prelude {
     pub use super::execution::{ExecutionContext, Resolver};
     pub use super::introspection::{introspection_schema, IntrospectionResolver};
     pub use super::query::{execute_query, QueryExecutionOptions};
-    pub use super::schema::{api_schema, APISchemaError};
+    pub use super::schema::{api_schema, validate_schema, APISchemaError, SchemaValidationError};
     pub use super::store::{build_query, StoreResolver};
     pub use super::subscription::{execute_subscription, SubscriptionExecutionOptions};
     pub use super::values::{object_value, MaybeCoercible};
