480ed674947193948a6d494f03b988ac105c7311
Jannis Pohlmann <contact@jannispohlmann.de>
2018-09-04 13:24:16 +0200

    graph: Move SubgraphRegistry into the subgraph module

--- graph/src/components/subgraph/mod.rs
+++ graph/src/components/subgraph/mod.rs
@@ -1,7 +1,9 @@
 mod host;
 mod manager;
 mod provider;
+mod registry;
 
 pub use self::host::{RuntimeHost, RuntimeHostBuilder, RuntimeHostEvent};
 pub use self::manager::RuntimeManager;
 pub use self::provider::{SchemaEvent, SubgraphProvider, SubgraphProviderEvent};
+pub use self::registry::SubgraphRegistry;
