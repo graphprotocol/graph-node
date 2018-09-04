005dcf23b22ae524a220a65dbad15ba501a08ce5
Jannis Pohlmann <contact@jannispohlmann.de>
2018-09-04 16:20:12 +0200

    server/websocket: Improve comments about connection error handling

--- graph/src/data/subscription/subscription.rs
+++ graph/src/data/subscription/subscription.rs
@@ -1,6 +1,9 @@
 use prelude::Query;
 
 /// A GraphQL subscription made by a client.
+///
+/// At the moment, this only contains the GraphQL query submitted as the
+/// subscription payload.
 #[derive(Clone, Debug)]
 pub struct Subscription {
     /// The GraphQL subscription query.
