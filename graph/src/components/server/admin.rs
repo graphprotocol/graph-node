1f6b3b8868029dd46f38742704f131a9ab4f00fc
Leonardo Yvens Schwarzstein <leoyvens@gmail.com>
2018-09-07 11:06:13 +0200

    graph, server: Refactor `JsonRpcServer`

--- graph/src/components/server/admin.rs
+++ graph/src/components/server/admin.rs
@@ -1,16 +1,11 @@
 use std::io;
 use std::sync::Arc;
 
-use components::subgraph::SubgraphProvider;
 use prelude::Logger;
 
 /// Common trait for JSON-RPC admin server implementations.
-pub trait JsonRpcServer {
+pub trait JsonRpcServer<T> {
     type Server;
 
-    fn serve(
-        port: u16,
-        provider: Arc<impl SubgraphProvider>,
-        logger: Logger,
-    ) -> Result<Self::Server, io::Error>;
+    fn serve(port: u16, provider: Arc<T>, logger: Logger) -> Result<Self::Server, io::Error>;
 }
