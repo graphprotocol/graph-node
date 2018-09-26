0cd3ab117384f0348267333018fc5662f990cb64
Leonardo Yvens Schwarzstein <leoyvens@gmail.com>
2018-09-26 11:18:36 -0300

    graph: Add some methods to `SubgraphRegistry`

--- graph/src/components/subgraph/registry.rs
+++ graph/src/components/subgraph/registry.rs
@@ -15,10 +15,7 @@ impl<T> Clone for SubgraphRegistry<T> {
     }
 }
 
-impl<T> SubgraphRegistry<T>
-where
-    T: Clone + Send,
-{
+impl<T> SubgraphRegistry<T> {
     pub fn new() -> Self {
         SubgraphRegistry {
             names_to_ids: Default::default(),
@@ -26,12 +23,31 @@ where
         }
     }
 
-    pub fn resolve(&self, id_or_name: &String) -> Option<T> {
+    pub fn resolve_by<U, F: FnOnce(&T) -> U>(&self, id_or_name: &String, by: F) -> Option<U> {
+        let names_to_ids = self.names_to_ids.read().unwrap();
+        let ids_to_values = self.ids_to_values.read().unwrap();
+
+        let id = names_to_ids.get(id_or_name).unwrap_or(id_or_name);
+        ids_to_values.get(id).map(by)
+    }
+
+    pub fn resolve(&self, id_or_name: &String) -> Option<T>
+    where
+        T: Clone,
+    {
         let names_to_ids = self.names_to_ids.read().unwrap();
         let ids_to_values = self.ids_to_values.read().unwrap();
 
         let id = names_to_ids.get(id_or_name).unwrap_or(id_or_name);
         ids_to_values.get(id).cloned()
+    }
+
+    pub fn mutate(&self, id_or_name: &String, op: impl FnOnce(&mut T)) {
+        let names_to_ids = self.names_to_ids.read().unwrap();
+        let mut ids_to_values = self.ids_to_values.write().unwrap();
+
+        let id = names_to_ids.get(id_or_name).unwrap_or(id_or_name);
+        ids_to_values.get_mut(id).map(op);
     }
 
     pub fn insert(&mut self, name: Option<String>, id: String, value: T) {
@@ -41,7 +57,15 @@ where
         self.ids_to_values.write().unwrap().insert(id, value);
     }
 
-    pub fn remove(&mut self, id: String) {
+    pub fn remove_name(&mut self, name: String) {
+        let id = self.names_to_ids.write().unwrap().remove(&name);
+
+        if let Some(id) = id {
+            self.ids_to_values.write().unwrap().remove(&id);
+        }
+    }
+
+    pub fn remove_id(&mut self, id: String) {
         self.ids_to_values.write().unwrap().remove(&id);
 
         let names: Vec<_> = self
