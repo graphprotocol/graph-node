f886cb2a60086896196593da44efb9933c55297c
Ford Nickels <ford@thegraph.com>
2018-10-02 11:09:36 -0700

    graph,server: Reduce SubscriptionError enum to single variant

--- graph/src/data/subscription/error.rs
+++ graph/src/data/subscription/error.rs
@@ -5,21 +5,19 @@ use prelude::QueryExecutionError;
 /// Error caused while processing a [Subscription](struct.Subscription.html) request.
 #[derive(Debug, Fail)]
 pub enum SubscriptionError {
-    #[fail(display = "GraphQL error: {}", _0)]
-    GraphQLError(QueryExecutionError),
-    #[fail(display = "GraphQL errors: {:?}", _0)]
-    GraphQLErrorList(Vec<QueryExecutionError>),
+    #[fail(display = "GraphQL error: {:?}", _0)]
+    GraphQLError(Vec<QueryExecutionError>),
 }
 
 impl From<QueryExecutionError> for SubscriptionError {
     fn from(e: QueryExecutionError) -> Self {
-        SubscriptionError::GraphQLError(e)
+        SubscriptionError::GraphQLError(vec![e])
     }
 }
 
 impl From<Vec<QueryExecutionError>> for SubscriptionError {
     fn from(e: Vec<QueryExecutionError>) -> Self {
-        SubscriptionError::GraphQLErrorList(e)
+        SubscriptionError::GraphQLError(e)
     }
 }
 impl Serialize for SubscriptionError {
