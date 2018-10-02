3f0f81611a043fd453341f32ddf7920e5ea2ad41
Tim McLean <tim@timmclean.net>
2018-10-02 12:11:28 -0400

    datasource/ethereum, graph: Less logging in block ingestor

--- datasource/ethereum/src/block_ingestor.rs
+++ datasource/ethereum/src/block_ingestor.rs
@@ -167,28 +167,26 @@ where
     ) -> impl Future<Item = TransactionReceipt, Error = Error> + 'a {
         let web3 = Web3::new(self.web3_transport.clone());
 
-        with_retry(
-            self.logger.clone(),
-            "eth_getTransactionReceipt RPC call".to_owned(),
-            move || {
-                web3.eth()
-                    .transaction_receipt(tx_hash)
-                    .map_err(move |e| {
+        // Infura frequently fails to find transaction receipts for new blocks,
+        // so retry without logging failed attempts.
+        with_retry_no_logging(move || {
+            web3.eth()
+                .transaction_receipt(tx_hash)
+                .map_err(move |e| {
+                    format_err!(
+                        "could not get transaction receipt {} from Ethereum: {}",
+                        tx_hash,
+                        e
+                    )
+                }).and_then(move |receipt_opt| {
+                    receipt_opt.ok_or_else(move || {
                         format_err!(
-                            "could not get transaction receipt {} from Ethereum: {}",
-                            tx_hash,
-                            e
+                            "Ethereum node could not find transaction receipt {}",
+                            tx_hash
                         )
-                    }).and_then(move |receipt_opt| {
-                        receipt_opt.ok_or_else(move || {
-                            format_err!(
-                                "Ethereum node could not find transaction receipt {}",
-                                tx_hash
-                            )
-                        })
                     })
-            },
-        )
+                })
+        })
     }
 
     /// Put some blocks into the block store (if they are not there already), and try to update the
