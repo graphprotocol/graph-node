f4f7f3023959eed60d2061c92b6cddb5d86c82cd
Tim McLean <tim@timmclean.net>
2018-10-06 15:34:57 -0400

    graph: Add RUST_LOG log level filtering support

--- graph/src/lib.rs
+++ graph/src/lib.rs
@@ -16,6 +16,7 @@ extern crate ipfs_api;
 extern crate parity_wasm;
 pub extern crate serde_json;
 extern crate slog_async;
+extern crate slog_envlogger;
 extern crate slog_term;
 extern crate tiny_keccak;
 pub extern crate tokio;
--- graph/src/util/log.rs
+++ graph/src/util/log.rs
@@ -1,6 +1,7 @@
 use backtrace::Backtrace;
 use slog;
 use slog_async;
+use slog_envlogger;
 use slog_term;
 use std::{env, panic};
 
@@ -9,6 +10,7 @@ use slog::Drain;
 pub fn logger() -> slog::Logger {
     let decorator = slog_term::TermDecorator::new().build();
     let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+    let drain = slog_envlogger::new(drain);
     let drain = slog_async::Async::new(drain).build().fuse();
     slog::Logger::root(drain, o!())
 }
