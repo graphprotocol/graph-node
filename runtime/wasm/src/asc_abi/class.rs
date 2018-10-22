b9a8b5448ca44bcceea0c1624e314aff8a5848b6
Leonardo Yvens Schwarzstein <leoyvens@gmail.com>
2018-10-22 11:31:03 -0200

    runtime: Rename `byte_length` to `end_byte`

--- runtime/wasm/src/asc_abi/class.rs
+++ runtime/wasm/src/asc_abi/class.rs
@@ -113,8 +113,9 @@ pub(crate) struct TypedArray<T> {
     pub buffer: AscPtr<ArrayBuffer<T>>,
     /// Byte position in `buffer` of the array start.
     byte_offset: u32,
-    /// Byte position in `buffer` of the array end.
-    byte_length: u32,
+    /// Byte position in `buffer` of the array end, which is 1 byte past the
+    /// last byte of the payload. Called `byteLength` in Asc.
+    end_byte: u32,
 }
 
 impl<T: AscValue> TypedArray<T> {
@@ -123,14 +124,14 @@ impl<T: AscValue> TypedArray<T> {
         TypedArray {
             buffer: AscPtr::alloc_obj(&buffer, heap),
             byte_offset: 0,
-            byte_length: buffer.byte_length,
+            end_byte: buffer.byte_length,
         }
     }
 
     pub(crate) fn to_vec<H: AscHeap>(&self, heap: &H) -> Vec<T> {
         self.buffer.read_ptr(heap).get(
             self.byte_offset,
-            (self.byte_length - self.byte_offset) / size_of::<T>() as u32,
+            (self.end_byte - self.byte_offset) / size_of::<T>() as u32,
         )
     }
 }
