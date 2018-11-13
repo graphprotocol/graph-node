05c0abce0e794dca5ad302e49255f4db7deb4c48
Ford Nickels <ford@thegraph.com>
2018-11-13 09:21:27 -0800

    graph, node: Send shutdown signal from panic logger, shutdown runtime
    Exit process after running shutdown and waiting for 3 seconds to ensure
    complete shutdown of all workers.

--- graph/src/util/log.rs
+++ graph/src/util/log.rs
@@ -1,8 +1,10 @@
 use backtrace::Backtrace;
+use futures::sync::oneshot;
 use slog::{crit, o, Drain, FilterLevel, Logger};
 use slog_async;
 use slog_envlogger;
 use slog_term;
+use std::sync::Mutex;
 use std::{env, panic};
 
 pub fn logger(show_debug: bool) -> Logger {
@@ -35,7 +37,10 @@ pub fn guarded_logger() -> (Logger, slog_async::AsyncGuard) {
     (Logger::root(drain.fuse(), o!()), guard)
 }
 
-pub fn register_panic_hook(panic_logger: Logger) {
+pub fn register_panic_hook(
+    panic_logger: Logger,
+    shutdown_sender: Mutex<Option<oneshot::Sender<()>>>,
+) {
     panic::set_hook(Box::new(move |panic_info| {
         let panic_payload = panic_info
             .payload()
@@ -67,6 +72,16 @@ pub fn register_panic_hook(panic_logger: Logger) {
                     panic_logger, "{}", panic_payload.unwrap();
                     "location" => &panic_location,
                 );
+            }
+        };
+
+        if let Ok(ref mut mutex) = shutdown_sender.lock() {
+            if let Some(sender) = mutex.take() {
+                sender
+                    .send(())
+                    .map(|_| ())
+                    .map_err(|_| ())
+                    .expect("Failed to signal shutdown")
             }
         };
     }));
