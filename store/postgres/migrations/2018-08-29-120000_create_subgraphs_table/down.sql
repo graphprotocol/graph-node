fa0bceb2c3034f74fde0069523bd09cce4d4d595
Jannis Pohlmann <contact@jannispohlmann.de>
2018-12-02 11:47:31 +0100

    store/postgres: Fix subgraph_deployments migration not running

--- store/postgres/migrations/2018-08-29-120000_create_subgraphs_table/down.sql
+++ store/postgres/migrations/2018-08-29-120000_create_subgraphs_table/down.sql
@@ -1,10 +1,1 @@
-DROP TRIGGER after_deployment_insert ON subgraph_deployments;
-DROP TRIGGER after_deployment_update ON subgraph_deployments;
-DROP TRIGGER after_deployment_delete ON subgraph_deployments;
-
-DROP FUNCTION deployment_insert();
-DROP FUNCTION deployment_update();
-DROP FUNCTION deployment_delete();
-
-DROP TABLE subgraph_deployments;
 DROP TABLE subgraphs;
