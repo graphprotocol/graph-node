2b3c1b8e6bf15ba8793b6022340041530dc43d70
Tim McLean <tim@timmclean.net>
2019-01-10 20:25:37 -0500

    store/postgres: Alert user if database version is incompatible

--- store/postgres/migrations/2018-07-10-061642_create_entities_table/up.sql
+++ store/postgres/migrations/2018-07-10-061642_create_entities_table/up.sql
@@ -1,3 +1,17 @@
+-- Track database version.
+-- This value is used to identify when the current database is in a format that
+-- is incompatible with and cannot be upgraded to a new version of graph-node.
+--
+-- See migration 2019-01-07-120000 for an example of how to check the version
+-- number.
+CREATE TABLE db_version (
+    db_version BIGINT PRIMARY KEY
+);
+
+-- Insert current version number
+INSERT INTO db_version VALUES (2);
+
+
 /**************************************************************
 * CREATE TABLE
 **************************************************************/
