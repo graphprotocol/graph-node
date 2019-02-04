7c9ed407c400b0f6b54ce423a57588af74bad1e0
Leonardo Yvens Schwarzstein <leoyvens@gmail.com>
2019-02-04 11:07:22 -0200

    runtime: Document examples of `AscType` derive

--- runtime/derive/src/lib.rs
+++ runtime/derive/src/lib.rs
@@ -16,6 +16,37 @@ pub fn asc_type_derive(input: TokenStream) -> TokenStream {
     }
 }
 
+// Example input:
+// #[repr(C)]
+// #[derive(AscType)]
+// struct AscTypedMapEntry<K, V> {
+//     pub key: AscPtr<K>,
+//     pub value: AscPtr<V>,
+// }
+//
+// Example output:
+// impl<K, V> AscType for AscTypedMapEntry<K, V> {
+//     fn to_asc_bytes(&self) -> Vec<u8> {
+//         let mut bytes = Vec::new();
+//         bytes.extend_from_slice(&self.key.to_asc_bytes());
+//         bytes.extend_from_slice(&self.value.to_asc_bytes());
+//         assert_eq!(&bytes.len(), &size_of::<Self>());
+//         bytes
+//     }
+
+//     #[allow(unused_variables)]
+//     fn from_asc_bytes(asc_obj: &[u8]) -> Self {
+//         assert_eq!(&asc_obj.len(), &size_of::<Self>());
+//         let mut offset = 0;
+//         let field_size = std::mem::size_of::<AscPtr<K>>();
+//         let key = AscType::from_asc_bytes(&asc_obj[offset..(offset + field_size)]);
+//         offset += field_size;
+//         let field_size = std::mem::size_of::<AscPtr<V>>();
+//         let value = AscType::from_asc_bytes(&asc_obj[offset..(offset + field_size)]);
+//         offset += field_size;
+//         Self { key, value }
+//     }
+// }
 fn asc_type_derive_struct(item_struct: ItemStruct) -> TokenStream {
     let struct_name = &item_struct.ident;
     let (impl_generics, ty_generics, where_clause) = item_struct.generics.split_for_impl();
@@ -37,7 +68,7 @@ fn asc_type_derive_struct(item_struct: ItemStruct) -> TokenStream {
     TokenStream::from(quote! {
         impl#impl_generics AscType for #struct_name#ty_generics #where_clause {
             fn to_asc_bytes(&self) -> Vec<u8> {
-               let mut bytes = vec![];
+               let mut bytes = Vec::new();
                 #(bytes.extend_from_slice(&self.#field_names.to_asc_bytes());)*
 
                 // Assert that the struct has no padding.
@@ -64,6 +95,47 @@ fn asc_type_derive_struct(item_struct: ItemStruct) -> TokenStream {
     })
 }
 
+// Example input:
+// #[repr(u32)]
+// #[derive(AscType)]
+// enum JsonValueKind {
+//     Null,
+//     Bool,
+//     Number,
+//     String,
+//     Array,
+//     Object,
+// }
+//
+// Example output:
+// impl AscType for JsonValueKind {
+//     fn to_asc_bytes(&self) -> Vec<u8> {
+//         let discriminant: u32 = match *self {
+//             JsonValueKind::Null => 0u32,
+//             JsonValueKind::Bool => 1u32,
+//             JsonValueKind::Number => 2u32,
+//             JsonValueKind::String => 3u32,
+//             JsonValueKind::Array => 4u32,
+//             JsonValueKind::Object => 5u32,
+//         };
+//         discriminant.to_asc_bytes()
+//     }
+//
+//     fn from_asc_bytes(asc_obj: &[u8]) -> Self {
+//         let mut u32_bytes: [u8; size_of::<u32>()] = [0; size_of::<u32>()];
+//         u32_bytes.copy_from_slice(&asc_obj);
+//         let discr = u32::from_le_bytes(u32_bytes);
+//         match discr {
+//             0u32 => JsonValueKind::Null,
+//             1u32 => JsonValueKind::Bool,
+//             2u32 => JsonValueKind::Number,
+//             3u32 => JsonValueKind::String,
+//             4u32 => JsonValueKind::Array,
+//             5u32 => JsonValueKind::Object,
+//             _ => panic!("value {} is out of range for {}", discr, "JsonValueKind"),
+//         }
+//     }
+// }
 fn asc_type_derive_enum(item_enum: ItemEnum) -> TokenStream {
     let enum_name = &item_enum.ident;
     let enum_name_iter = std::iter::repeat(enum_name);
@@ -95,7 +167,7 @@ fn asc_type_derive_enum(item_enum: ItemEnum) -> TokenStream {
                 u32_bytes.copy_from_slice(&asc_obj);
                 let discr = u32::from_le_bytes(u32_bytes);
                 match discr {
-                    #(#variant_discriminant2 =>#enum_name_iter2::#variant_paths2,)*
+                    #(#variant_discriminant2 => #enum_name_iter2::#variant_paths2,)*
                     _ => panic!("value {} is out of range for {}", discr, stringify!(#enum_name))
                 }
             }
