64262741a2478bbbc936df632026a9ba0c310445
Jannis <jannis@thegraph.com>
2019-04-11 23:34:15 +0200

    graph: Add `DataSourceLoader` trait for loading dynamic data sources

--- graph/src/components/subgraph/mod.rs
+++ graph/src/components/subgraph/mod.rs
@@ -1,6 +1,7 @@
 mod host;
 mod instance;
 mod instance_manager;
+mod loader;
 mod provider;
 mod registrar;
 
@@ -9,5 +10,6 @@ pub use crate::prelude::Entity;
 pub use self::host::{RuntimeHost, RuntimeHostBuilder};
 pub use self::instance::{DataSourceTemplateInfo, ProcessingState, SubgraphInstance};
 pub use self::instance_manager::SubgraphInstanceManager;
+pub use self::loader::DataSourceLoader;
 pub use self::provider::SubgraphAssignmentProvider;
 pub use self::registrar::{SubgraphRegistrar, SubgraphVersionSwitchingMode};
