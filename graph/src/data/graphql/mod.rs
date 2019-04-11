10ab3adb98c54e430eae254fa01c2b1ad0c27f1c
Jannis <jannis@thegraph.com>
2019-04-11 21:53:33 +0200

    graph: Add `TryFromValue` and `ValueMap` helper traits
    `TryFromValue` can be used to try to convert a `graphql_parser::query::Value`
    into a different type. It is like `TryFrom`, just with the `Error` type
    fixed to be `failure::Error`.
    
    `ValueMap` is a helper to pull out optional and required values from a
    collection by name/key, and convert them using `TryFromValue` on the fly.
    
    Both of these are convenient helpers to go from GraphQL results to
    typed entities such as those in `graph::data::subgraph::schema`.

--- graph/src/data/graphql/mod.rs
+++ graph/src/data/graphql/mod.rs
@@ -3,5 +3,16 @@ mod serialization;
 /// Utilities for validating GraphQL schemas.
 pub(crate) mod validation;
 
+/// Utilities for working with GraphQL values.
+mod values;
+
 /// Serializable wrapper around a GraphQL value.
 pub use self::serialization::SerializableValue;
+
+pub use self::values::{
+    // Trait for converting from GraphQL values into other types.
+    TryFromValue,
+
+    // Trait for plucking typed values out of a GraphQL value maps.
+    ValueMap,
+};
