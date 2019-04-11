e6bfa09e7a2853fe3670d94292fa67e25c265b5f
Jannis <jannis@thegraph.com>
2019-04-11 23:39:10 +0200

    core: Add implementation of `DataSourceLoader`

--- core/src/subgraph/mod.rs
+++ core/src/subgraph/mod.rs
@@ -1,9 +1,11 @@
 mod instance;
 mod instance_manager;
+mod loader;
 mod provider;
 mod registrar;
 
 pub use self::instance::SubgraphInstance;
 pub use self::instance_manager::SubgraphInstanceManager;
+pub use self::loader::DataSourceLoader;
 pub use self::provider::SubgraphAssignmentProvider;
 pub use self::registrar::SubgraphRegistrar;
