098171b6b16c163dfbd347981b5978cfe51320a3
Jorge Olivero <jorgeaolivero@gmail.com>
2019-04-17 12:43:46 -0500

    datasource/ethereum: Add extra whitelines for visual clarity

--- datasource/ethereum/src/block_stream.rs
+++ datasource/ethereum/src/block_stream.rs
@@ -1251,6 +1251,7 @@ fn create_log_filter_from_subgraph(manifest: &SubgraphManifest) -> Option<Ethere
                 })
         })
         .collect::<EthereumLogFilter>();
+
     match log_filter.contract_address_and_event_sig_pairs.len() {
         0 => return None,
         _ => return Some(log_filter),
@@ -1274,6 +1275,7 @@ fn create_call_filter_from_subgraph(manifest: &SubgraphManifest) -> Option<Ether
                 })
         })
         .collect::<EthereumCallFilter>();
+
     match call_filter.contract_addresses_function_signatures.len() {
         0 => return None,
         _ => return Some(call_filter),
