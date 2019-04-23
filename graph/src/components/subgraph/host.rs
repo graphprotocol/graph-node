ae81460cd2296bd7a2785ffe1225ee49f4bae14d
Jannis Pohlmann <jannis@thegraph.com>
2019-04-23 23:30:13 +0200

    graph: Add `matches_block` method to `RuntimeHost` trait

--- graph/src/components/subgraph/host.rs
+++ graph/src/components/subgraph/host.rs
@@ -13,6 +13,9 @@ pub trait RuntimeHost: Send + Sync + Debug {
     /// Returns true if the RuntimeHost has a handler for an Ethereum call.
     fn matches_call(&self, call: &EthereumCall) -> bool;
 
+    /// Returns true if the RuntimeHost has a handler for an Ethereum block.
+    fn matches_block(&self, block: &EthereumBlock, call: &EthereumCall) -> bool;
+
     /// Process an Ethereum event and return a vector of entity operations.
     fn process_log(
         &self,
