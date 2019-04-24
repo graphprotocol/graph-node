92c0ef7577da117cd81514bb3c984e60f92df074
Jannis Pohlmann <jannis@thegraph.com>
2019-04-24 04:13:36 +0200

    core: Fix ownership errors in `validate_manifest`

--- core/src/subgraph/validation.rs
+++ core/src/subgraph/validation.rs
@@ -3,49 +3,63 @@ use graph::prelude::*;
 pub fn validate_manifest(
     manifest: SubgraphManifest,
 ) -> Result<SubgraphManifest, SubgraphRegistrarError> {
-    let mut validation_errs: Vec<SubgraphManifestValidationError> = Vec::new();
+    let mut errors: Vec<SubgraphManifestValidationError> = Vec::new();
 
     // Validate that the manifest has a `source` address in each data source
     // which has call or block handlers
     let has_invalid_data_source = manifest.data_sources.iter().any(|data_source| {
         let no_source_address = data_source.source.address.is_none();
-        let has_call_handlers = !data_source.mapping.call_handlers.is_empty();
-        let has_block_handlers = !data_source.mapping.block_handlers.is_empty();
+        let has_call_handlers = !data_source
+            .mapping
+            .call_handlers
+            .as_ref()
+            .map_or(false, |handlers| !handlers.is_empty());
+        let has_block_handlers = !data_source
+            .mapping
+            .block_handlers
+            .as_ref()
+            .map_or(false, |handlers| handlers.is_empty());
+
         no_source_address && (has_call_handlers || has_block_handlers)
     });
+
     if has_invalid_data_source {
-        validation_errs.push(SubgraphManifestValidationError::SourceAddressRequired)
+        errors.push(SubgraphManifestValidationError::SourceAddressRequired)
     }
 
     // Validate that there are no more than one of each type of
     // block_handler in each data source.
     let has_too_many_block_handlers = manifest.data_sources.iter().any(|data_source| {
-        if data_source.mapping.block_handlers.is_empty() {
-            return false;
-        }
-        let mut non_filtered_block_handler_count = 0;
-        let mut call_filtered_block_handler_count = 0;
-        data_source
+        if data_source
             .mapping
             .block_handlers
-            .iter()
-            .for_each(|block_handler| {
+            .as_ref()
+            .map_or(true, |handlers| handlers.is_empty())
+        {
+            return false;
+        }
+
+        let mut non_filtered_block_handler_count = 0;
+        let mut call_filtered_block_handler_count = 0;
+        if let Some(ref handlers) = data_source.mapping.block_handlers {
+            handlers.iter().for_each(|block_handler| {
                 if block_handler.filter.is_none() {
                     non_filtered_block_handler_count += 1
                 } else {
                     call_filtered_block_handler_count += 1
                 }
             });
+        }
         return non_filtered_block_handler_count > 1 || call_filtered_block_handler_count > 1;
     });
+
     if has_too_many_block_handlers {
-        validation_errs.push(SubgraphManifestValidationError::DataSourceBlockHandlerLimitExceeded)
+        errors.push(SubgraphManifestValidationError::DataSourceBlockHandlerLimitExceeded)
     }
 
-    if validation_errs.is_empty() {
+    if errors.is_empty() {
         return Ok(manifest);
     }
-    return Err(SubgraphRegistrarError::ManifestValidationError(
-        validation_errs,
-    ));
+
+    return Err(SubgraphRegistrarError::ManifestValidationError(errors));
 }
