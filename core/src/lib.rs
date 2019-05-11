01f103e361a2c2bdb2f746e4167115d10e74ea83
Jorge Olivero <jorgeaolivero@gmail.com>
2019-05-11 16:11:04 -0500

    core: Remove macro

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,11 +4,10 @@ extern crate graph_graphql;
 #[cfg(test)]
 extern crate graph_mock;
 extern crate graph_runtime_wasm;
-extern crate serde;
-#[macro_use]
-extern crate serde_json;
 extern crate lazy_static;
 extern crate semver;
+extern crate serde;
+extern crate serde_json;
 extern crate serde_yaml;
 extern crate uuid;
 
