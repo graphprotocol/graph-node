218c47532b5a45695f13d4d827065395bc49e94a
Leonardo Yvens <leoyvens@gmail.com>
2019-05-14 17:14:04 -0300

    node, runtime: Simplify panic handling
    Use `panic_handler` in the runtime builider
    to simplify our panic handling logic.

--- graph/src/log/mod.rs
+++ graph/src/log/mod.rs
@@ -1,18 +1,12 @@
-use backtrace::Backtrace;
-use futures::sync::oneshot;
-use slog::{crit, debug, o, Drain, FilterLevel, Logger};
+use slog::{o, Drain, FilterLevel, Logger};
 use slog_async;
 use slog_envlogger;
 use slog_term;
-use std::sync::Mutex;
-use std::time::Duration;
-use std::{env, panic, process, thread};
+use std::env;
 
 pub mod codes;
 pub mod elastic;
 pub mod split;
-
-pub const MAPPING_THREAD_PREFIX: &str = "mapping-thread";
 
 pub fn logger(show_debug: bool) -> Logger {
     let decorator = slog_term::TermDecorator::new().build();
@@ -35,77 +29,4 @@ pub fn logger(show_debug: bool) -> Logger {
         .build();
     let drain = slog_async::Async::new(drain).build().fuse();
     Logger::root(drain, o!())
-}
-
-pub fn guarded_logger() -> (Logger, slog_async::AsyncGuard) {
-    let decorator = slog_term::TermDecorator::new().build();
-    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
-    let (drain, guard) = slog_async::Async::new(drain).build_with_guard();
-    (Logger::root(drain.fuse(), o!()), guard)
-}
-
-pub fn register_panic_hook(panic_logger: Logger, shutdown_sender: oneshot::Sender<()>) {
-    let shutdown_mutex = Mutex::new(Some(shutdown_sender));
-    panic::set_hook(Box::new(move |panic_info| {
-        let panic_payload = panic_info
-            .payload()
-            .downcast_ref::<String>()
-            .cloned()
-            .or_else(|| {
-                panic_info
-                    .payload()
-                    .downcast_ref::<&str>()
-                    .map(|s| s.to_string())
-            });
-
-        let panic_location = if let Some(location) = panic_info.location() {
-            format!("{}:{}", location.file(), location.line().to_string())
-        } else {
-            "NA".to_string()
-        };
-
-        match env::var_os("RUST_BACKTRACE") {
-            Some(ref val) if val != "0" => {
-                crit!(
-                    panic_logger, "{}", panic_payload.unwrap();
-                    "location" => &panic_location,
-                    "backtrace" => format!("{:?}", Backtrace::new()),
-                );
-            }
-            _ => {
-                crit!(
-                    panic_logger, "{}", panic_payload.unwrap();
-                    "location" => &panic_location,
-                );
-            }
-        };
-
-        // Don't kill the process when a mapping thread panics.
-        if thread::current()
-            .name()
-            .filter(|name| name.starts_with(MAPPING_THREAD_PREFIX))
-            .is_some()
-        {
-            return;
-        }
-
-        // Send a shutdown signal to main which will attempt to cleanly shutdown the runtime.
-        match shutdown_mutex.lock().unwrap().take() {
-            Some(sender) => sender
-                .send(())
-                .map(|_| ())
-                .map_err(|_| {
-                    crit!(panic_logger, "Failed to send shutdown signal");
-                    ()
-                })
-                .unwrap_or(()),
-            None => debug!(panic_logger, "Shutdown signal already sent"),
-        }
-
-        // If shutting down the runtime takes too long, exit the process anyways.
-        thread::spawn(|| {
-            thread::sleep(Duration::from_millis(3000));
-            process::exit(1);
-        });
-    }));
 }
