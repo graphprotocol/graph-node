ddb4af4d004c4fa3a2cb38fe9b45b3602df09370
Jannis Pohlmann <jannis@thegraph.com>
2019-06-12 15:34:24 +0200

    runtime/wasm: Bump maximum supported mapping apiVersion to 0.0.3

--- runtime/wasm/src/host.rs
+++ runtime/wasm/src/host.rs
@@ -148,9 +148,9 @@ impl RuntimeHost {
         ));
 
         let api_version = Version::parse(&config.data_source.mapping.api_version)?;
-        if !VersionReq::parse("<= 0.0.2").unwrap().matches(&api_version) {
+        if !VersionReq::parse("<= 0.0.3").unwrap().matches(&api_version) {
             return Err(format_err!(
-                "This Graph Node only supports mapping API versions <= 0.0.2, but subgraph `{}` uses `{}`",
+                "This Graph Node only supports mapping API versions <= 0.0.3, but subgraph `{}` uses `{}`",
                 config.subgraph_id,
                 api_version
             ));
