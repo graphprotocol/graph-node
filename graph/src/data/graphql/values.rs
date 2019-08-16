c6ca03432ad7e55ad1339fe1901ef33298584051
Jannis Pohlmann <jannis@thegraph.com>
2019-08-16 19:25:27 +0200

    graph: Add `ValueList` trait for lists of typed GraphQL values
    Its main use is to take GraphQL `Value` or similar and, if it is a
    list value, pluck all its items out, converting them from a GraphQL
    value to a target time on the go.

--- graph/src/data/graphql/values.rs
+++ graph/src/data/graphql/values.rs
@@ -97,3 +97,33 @@ impl ValueMap for &BTreeMap<String, Value> {
         })
     }
 }
+
+pub trait ValueList {
+    fn get_values<T>(&self) -> Result<Vec<T>, Error>
+    where
+        T: TryFromValue;
+}
+
+impl ValueList for Value {
+    fn get_values<T>(&self) -> Result<Vec<T>, Error>
+    where
+        T: TryFromValue,
+    {
+        match self {
+            Value::List(values) => values.get_values(),
+            _ => Err(format_err!("value is not a list: {:?}", self)),
+        }
+    }
+}
+
+impl ValueList for Vec<Value> {
+    fn get_values<T>(&self) -> Result<Vec<T>, Error>
+    where
+        T: TryFromValue,
+    {
+        self.iter().try_fold(vec![], |mut acc, value| {
+            acc.push(T::try_from_value(value)?);
+            Ok(acc)
+        })
+    }
+}
