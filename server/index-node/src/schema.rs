eab758e3dd0001f48246c302d6f842b37f20fff6
Jannis Pohlmann <jannis@thegraph.com>
2019-09-23 16:05:07 +0200

    server/index-node: Remove another dbg!` statement

--- server/index-node/src/schema.rs
+++ server/index-node/src/schema.rs
@@ -4,7 +4,7 @@ use graph::prelude::*;
 
 lazy_static! {
     pub static ref SCHEMA: Arc<Schema> = {
-        let raw_schema = dbg!(include_str!("./schema.graphql"));
+        let raw_schema = include_str!("./schema.graphql");
         let document = graphql_parser::parse_schema(&raw_schema).unwrap();
         let (interfaces_for_type, types_for_interface) =
             Schema::collect_interfaces(&document).unwrap();
