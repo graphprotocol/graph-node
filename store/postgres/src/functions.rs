740b7b10adcd5ad95e48534921b011be7497e8e5
David Lutterkort <lutter@watzmann.net>
2019-09-26 11:19:46 -0700

    store: Fix usage of sql_function macro
    The syntax we were using has been deprecated, and was causing build
    failures in Travis. Update to the recommended syntax

--- store/postgres/src/functions.rs
+++ store/postgres/src/functions.rs
@@ -1,29 +1,20 @@
 use diesel::sql_types::*;
 
 // Create modules for hosting stored procedures
+sql_function! { fn current_setting(setting_name: Text, missing_ok: Bool) }
+
 sql_function! {
-    current_setting,
-    CurrentSetting,
-    (setting_name: Text, missing_ok: Bool)
-}
-sql_function! {
-    set_config,
-    SetConfig,
-    (setting_name: Text, new_value: Text, is_local: Bool)
-}
-sql_function! {
-    attempt_chain_head_update,
-    AttemptChainHeadUpdate,
-    (net_name: Varchar, ancestor_count: BigInt) -> Array<Varchar>
-}
-sql_function! {
-    lookup_ancestor_block,
-    LookupAncestorBlock,
-    (start_block_hash: Varchar, ancestor_count: BigInt) -> Nullable<Jsonb>
+    fn set_config(setting_name: Text, new_value: Text, is_local: Bool)
 }
 
 sql_function! {
-    pg_notify,
-    PGNotify,
-    (channel: Text, msg: Text)
+    fn attempt_chain_head_update(net_name: Varchar, ancestor_count: BigInt) -> Array<Varchar>
+}
+
+sql_function! {
+    fn lookup_ancestor_block(start_block_hash: Varchar, ancestor_count: BigInt) -> Nullable<Jsonb>
+}
+
+sql_function! {
+    fn pg_notify(channel: Text, msg: Text)
 }
