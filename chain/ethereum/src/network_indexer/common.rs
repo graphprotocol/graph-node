0b03229c1d0be74368ce7570d94278994c76b2a4
Jannis Pohlmann <jannis@thegraph.com>
2019-11-21 18:21:39 +0000

    Squashme: chain/ethereum: split out common things

--- chain/ethereum/src/network_indexer/common.rs
+++ chain/ethereum/src/network_indexer/common.rs
@@ -2,6 +2,7 @@ use graph::prelude::*;
 use web3::types::{Block, TransactionReceipt, H256};
 
 /// Helper type to bundle blocks and their uncles together.
+#[derive(Debug)]
 pub struct BlockWithUncles {
     pub block: EthereumBlock,
     pub uncles: Vec<Option<Block<H256>>>,
