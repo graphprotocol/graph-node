4bab5d132bbbdbe1b85521ed7b8bb64691de2832
Jannis Pohlmann <jannis@thegraph.com>
2019-11-21 18:23:21 +0000

    chain/ethereum: Rework `BlockWriter` so it's reusable

--- chain/ethereum/src/network_indexer/block_writer.rs
+++ chain/ethereum/src/network_indexer/block_writer.rs
@@ -6,11 +6,6 @@ use graph::prelude::*;
 use super::common::*;
 use super::to_entity::*;
 
-/// Internal result type used to thread (BlockWriter, EntityCache) through
-/// the chain of futures when writing network data.
-type BlockWriteResult<S> =
-    Box<dyn Future<Item = (BlockWriter<S>, EntityCache), Error = Error> + Send>;
-
 struct BlockWriterMetrics {
     transaction: Aggregate,
 }
@@ -27,17 +22,9 @@ impl BlockWriterMetrics {
     }
 }
 
-/// Options for configuring a block writer.
-pub struct BlockWriterOptions<'a, S> {
-    pub logger: &'a Logger,
-    pub subgraph_id: SubgraphDeploymentId,
-    pub store: Arc<S>,
-    pub metrics_registry: Arc<dyn MetricsRegistry>,
-}
-
 pub(crate) struct BlockWriter<S> {
-    logger: Logger,
     subgraph_id: SubgraphDeploymentId,
+    logger: Logger,
     store: Arc<S>,
     metrics: Arc<Mutex<BlockWriterMetrics>>,
 }
@@ -46,14 +33,12 @@ impl<S> BlockWriter<S>
 where
     S: Store + ChainStore,
 {
-    pub fn new<'a>(options: BlockWriterOptions<'a, S>) -> Self {
-        let BlockWriterOptions {
-            logger,
-            subgraph_id,
-            store,
-            metrics_registry,
-        } = options;
-
+    pub fn new(
+        subgraph_id: SubgraphDeploymentId,
+        logger: &Logger,
+        store: Arc<S>,
+        metrics_registry: Arc<dyn MetricsRegistry>,
+    ) -> Self {
         let logger = logger.new(o!("component" => "BlockWriter"));
         let metrics = Arc::new(Mutex::new(BlockWriterMetrics::new(
             &subgraph_id,
@@ -67,7 +52,7 @@ where
         }
     }
 
-    pub fn write(self, block: BlockWithUncles) -> impl Future<Item = Self, Error = Error> {
+    pub fn write(&self, block: BlockWithUncles) -> impl Future<Item = (), Error = Error> {
         let hash = block.inner().hash.clone().unwrap();
         let number = block.inner().number.clone().unwrap();
 
@@ -76,7 +61,46 @@ where
             "block_number" => format!("{}", number),
         ));
 
-        debug!(logger, "Write block");
+        let context = WriteContext {
+            logger,
+            subgraph_id: self.subgraph_id.clone(),
+            store: self.store.clone(),
+            cache: EntityCache::new(),
+            metrics: self.metrics.clone(),
+        };
+        context.write(block)
+    }
+}
+
+struct WriteContext<S> {
+    logger: Logger,
+    subgraph_id: SubgraphDeploymentId,
+    store: Arc<S>,
+    cache: EntityCache,
+    metrics: Arc<Mutex<BlockWriterMetrics>>,
+}
+
+/// Internal result type used to thread WriteContext through
+/// the chain of futures when writing network data.
+type WriteContextResult<S> = Box<dyn Future<Item = WriteContext<S>, Error = Error> + Send>;
+
+impl<S> WriteContext<S>
+where
+    S: Store + ChainStore,
+{
+    fn set_entity(mut self, value: impl ToEntity + ToEntityKey) -> WriteContextResult<S> {
+        self.cache.set(
+            value.to_entity_key(self.subgraph_id.clone()),
+            match value.to_entity() {
+                Ok(entity) => entity,
+                Err(e) => return Box::new(future::err(e.into())),
+            },
+        );
+        Box::new(future::ok(self))
+    }
+
+    fn write(self, block: BlockWithUncles) -> impl Future<Item = (), Error = Error> {
+        debug!(self.logger, "Write block");
 
         let block = Arc::new(block);
         let block_for_uncles = block.clone();
@@ -84,55 +108,39 @@ where
 
         Box::new(
             // Add the block and uncle block entities
-            self.set_entity(EntityCache::new(), block.as_ref())
-                .and_then(move |(writer, cache)| {
+            self.set_entity(block.as_ref())
+                .and_then(move |context| {
                     futures::stream::iter_ok::<_, Error>(block_for_uncles.uncles.clone())
-                        .fold((writer, cache), |(writer, cache), ommer| {
-                            writer.set_entity(cache, ommer.unwrap())
-                        })
+                        .fold(context, |context, ommer| context.set_entity(ommer.unwrap()))
                 })
                 // Transact everything into the store
-                .and_then(move |(writer, cache)| {
+                .and_then(move |context| {
+                    let cache = context.cache;
+                    let metrics = context.metrics;
+                    let store = context.store;
+                    let subgraph_id = context.subgraph_id;
+
                     // Transact entity operations into the store
-                    let modifications = match cache.as_modifications(writer.store.as_ref()) {
+                    let modifications = match cache.as_modifications(store.as_ref()) {
                         Ok(mods) => mods,
                         Err(e) => return future::err(e.into()),
                     };
 
                     let started = Instant::now();
                     future::result(
-                        writer
-                            .store
+                        store
                             .transact_block_operations(
-                                writer.subgraph_id.clone(),
+                                subgraph_id.clone(),
                                 EthereumBlockPointer::from(&block_for_store.block),
                                 modifications,
                             )
                             .map_err(|e| e.into())
                             .map(move |_| {
-                                {
-                                    let mut metrics = writer.metrics.lock().unwrap();
-                                    metrics.transaction.update_duration(started.elapsed());
-                                }
-                                writer
+                                let mut metrics = metrics.lock().unwrap();
+                                metrics.transaction.update_duration(started.elapsed());
                             }),
                     )
                 }),
         )
-    }
-
-    fn set_entity(
-        self,
-        mut cache: EntityCache,
-        value: impl ToEntity + ToEntityKey,
-    ) -> BlockWriteResult<S> {
-        cache.set(
-            value.to_entity_key(self.subgraph_id.clone()),
-            match value.to_entity() {
-                Ok(entity) => entity,
-                Err(e) => return Box::new(future::err(e.into())),
-            },
-        );
-        Box::new(future::ok((self, cache)))
     }
 }
