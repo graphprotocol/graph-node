dad5af8d86195e740908532f96a81b5f1bdc4af5
Jannis Pohlmann <jannis@thegraph.com>
2019-11-23 00:50:55 +0000

    Squashme: BlockWithUncles / common

--- chain/ethereum/src/network_indexer/common.rs
+++ chain/ethereum/src/network_indexer/common.rs
@@ -2,7 +2,7 @@ use graph::prelude::*;
 use web3::types::{Block, TransactionReceipt, H256};
 
 /// Helper type to bundle blocks and their uncles together.
-#[derive(Debug)]
+#[derive(Clone, Debug, Default, PartialEq)]
 pub struct BlockWithUncles {
     pub block: EthereumBlock,
     pub uncles: Vec<Option<Block<H256>>>,
