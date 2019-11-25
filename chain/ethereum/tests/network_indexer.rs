13f598016c560b610502c2c80a84c340fe24c61a
Jannis Pohlmann <jannis@thegraph.com>
2019-11-25 23:35:47 +0000

    chain/ethereum: Clean up test setup, add indexer timeout
    The timeout is needed to prevent the indexer from keeping on running
    forever, which it otherwise would because we implement robustness on the
    Ethereum level with retries.

--- chain/ethereum/tests/network_indexer.rs
+++ chain/ethereum/tests/network_indexer.rs
@@ -5,11 +5,12 @@ use diesel::connection::Connection;
 use diesel::pg::PgConnection;
 use std::convert::TryInto;
 use std::sync::Mutex;
+use std::time::Duration;
 
 use graph::mock::*;
 use graph::prelude::*;
 use graph_chain_ethereum::network_indexer::{
-    self as network_indexer, BlockWithUncles, NetworkIndexer, NetworkIndexerEvent,
+    self as network_indexer, BlockWithUncles, NetworkIndexerEvent,
 };
 use graph_core::MetricsRegistry;
 use graph_store_postgres::Store as DieselStore;
@@ -25,53 +26,49 @@ fn remove_test_data(store: Arc<DieselStore>) {
         .expect("Failed to remove entity test data");
 }
 
-// Helper type for setting up tests.
-struct TestSetup {
-    // pub subgraph_id: SubgraphDeploymentId,
-    // pub subgraph_name: SubgraphName,
-    pub logger: Logger,
-    pub adapter: Arc<MockEthereumAdapter>,
-    pub store: Arc<DieselStore>,
-    pub metrics_registry: Arc<MetricsRegistry>,
-    pub indexer: NetworkIndexer,
-}
+// Helper to run network indexer against test chains.
+pub fn run_network_indexer(
+    store: Arc<DieselStore>,
+    start_block: Option<EthereumBlockPointer>,
+    chains: Vec<Vec<BlockWithUncles>>,
+) -> Vec<NetworkIndexerEvent> {
+    // Simulate an Ethereum network using a mock adapter
+    let adapter = create_mock_ethereum_adapter(chains);
 
-impl TestSetup {
-    pub fn new(
-        store: Arc<DieselStore>,
-        start_block: Option<EthereumBlockPointer>,
-        chains: Vec<Vec<BlockWithUncles>>,
-    ) -> Self {
-        // Simulate an Ethereum network using a mock adapter
-        let adapter = create_mock_ethereum_adapter(chains);
+    let subgraph_name = SubgraphName::new("ethereum/testnet").unwrap();
+    let logger = LOGGER.clone();
+    let prometheus_registry = Arc::new(Registry::new());
+    let metrics_registry = Arc::new(MetricsRegistry::new(logger.clone(), prometheus_registry));
 
-        let subgraph_name = SubgraphName::new("ethereum/testnet").unwrap();
-        let logger = LOGGER.clone();
-        let prometheus_registry = Arc::new(Registry::new());
-        let metrics_registry = Arc::new(MetricsRegistry::new(logger.clone(), prometheus_registry));
+    // Create the network indexer
+    let mut indexer = network_indexer::create(
+        subgraph_name.to_string(),
+        &logger,
+        adapter,
+        store.clone(),
+        metrics_registry,
+        start_block,
+    )
+    .wait()
+    .expect("failed to create network indexer");
 
-        // Create the network indexer
-        let indexer = network_indexer::create(
-            subgraph_name.to_string(),
-            &logger,
-            adapter.clone(),
-            store.clone(),
-            metrics_registry.clone(),
-            start_block,
-        )
+    let (event_sink, event_stream) = futures::sync::mpsc::channel(100);
+
+    // Run network indexer and forward its events to the channel
+    tokio::spawn(
+        indexer
+            .take_event_stream()
+            .expect("failed to take stream from indexer")
+            .timeout(Duration::from_secs(2))
+            .map_err(|_| ())
+            .forward(event_sink.sink_map_err(|_| ()))
+            .map(|_| ()),
+    );
+
+    event_stream
+        .collect()
         .wait()
-        .expect("failed to create network indexer");
-
-        Self {
-            // subgraph_id,
-            // subgraph_name,
-            logger,
-            adapter,
-            store,
-            metrics_registry,
-            indexer,
-        }
-    }
+        .expect("failed to get events from the stream")
 }
 
 // Helper to run tests against a clean store.
@@ -189,9 +186,11 @@ fn create_mock_ethereum_adapter(chains: Vec<Vec<BlockWithUncles>>) -> Arc<MockEt
         Box::new(future::result(
             chains
                 .next_chain()
-                .expect("exhausted remote head blocks")
-                .last()
-                .ok_or_else(|| format_err!("empty block chain"))
+                // .expect("exhausted chain versions used in this test; this is ok")
+                .ok_or_else(|| {
+                    format_err!("exhausted chain versions used in this test; this is ok")
+                })
+                .and_then(|chain| chain.last().ok_or_else(|| format_err!("empty block chain")))
                 .map_err(Into::into)
                 .map(|block| block.block.block.clone()),
         ))
@@ -269,29 +268,19 @@ fn create_mock_ethereum_adapter(chains: Vec<Vec<BlockWithUncles>>) -> Arc<MockEt
     Arc::new(adapter)
 }
 
-// GIVEN  a fresh subgraph (local head = None)
+// GIVEN  a fresh subgraph (local head = none)
 // AND    a chain with 10 blocks
 // WHEN   running the `NetworkIndexer`
 // EXPECT 10 `AddBlock` events are emitted, one for each block
 #[test]
-fn tracing_starts_at_genesis() {
+fn indexing_starts_at_genesis() {
     run_test(|store: Arc<DieselStore>| -> Result<(), ()> {
-        // Create blocks for simulating the chain
+        // Create test chain
         let blocks = create_blocks(10, None);
+        let chains = vec![blocks.clone()];
 
-        // Set up the test
-        let mut setup = TestSetup::new(store, None, vec![blocks.clone()]);
-
-        // Run network indexer and forward its events to the channel
-        let events = setup
-            .indexer
-            .take_event_stream()
-            .expect("failed to take stream from indexer")
-            .take(10) // there should only be 7 blocks but we'll try to pull more
-            .fuse()
-            .collect()
-            .wait()
-            .expect("failed to collect events from indexer");
+        // Run network indexer and collect its events
+        let events = run_network_indexer(store, None, chains);
 
         // Assert that the events emitted by the indexer match all
         // blocks _after_ block #2 (because the network subgraph already
@@ -313,26 +302,17 @@ fn tracing_starts_at_genesis() {
 // WHEN   running the `NetworkIndexer`
 // EXPECT 7 `AddBlock` events are emitted, one for each remaining block
 #[test]
-fn tracing_resumes_from_local_head() {
+fn indexing_resumes_from_local_head() {
     run_test(|store: Arc<DieselStore>| -> Result<(), ()> {
-        // Create blocks for simulating the chain
+        // Create test chain
         let blocks = create_blocks(10, None);
+        let chains = vec![blocks.clone()];
 
-        // Set up the test
-        let mut setup = TestSetup::new(store, Some(blocks[2].inner().into()), vec![blocks.clone()]);
+        // Run network indexer and collect its events
+        let events = run_network_indexer(store, Some(blocks[2].inner().into()), chains);
 
-        // Run network indexer and forward its events to the channel
-        let events = setup
-            .indexer
-            .take_event_stream()
-            .expect("failed to take stream from indexer")
-            .take(10)
-            .fuse()
-            .collect()
-            .wait()
-            .expect("failed to collect events from indexer");
-
-        // Assert that the events emitted by the indexer match the blocks 1:1
+        // Assert that the events emitted by the indexer are only
+        // for the blocks #3-#9.
         assert_eq!(
             events,
             blocks[3..]
@@ -346,12 +326,12 @@ fn tracing_resumes_from_local_head() {
     });
 }
 
-// GIVEN  a fresh subgraph (local head = None)
+// GIVEN  a fresh subgraph (local head = none)
 // AND    a chain with 10 blocks
 // WHEN   running the `NetworkIndexer`
 // EXPECT 10 `AddBlock` events are emitted, one for each block
 #[test]
-fn tracing_picks_up_new_remote_head() {
+fn indexing_picks_up_new_remote_head() {
     run_test(|store: Arc<DieselStore>| -> Result<(), ()> {
         // The first time we pull the remote head, there are 10 blocks
         let chain_10 = create_blocks(10, None);
@@ -359,21 +339,13 @@ fn tracing_picks_up_new_remote_head() {
         // The second time we pull the remote head, there are 20 blocks;
         // the first 10 blocks are identical to before, so this simulates
         // 10 new blocks being added to the same chain
-        let chain_20 = create_fork(chain_10.clone(), 10, 10);
+        let chain_20 = create_fork(chain_10.clone(), 9, 20);
 
-        // Set up the test
-        let mut setup = TestSetup::new(store, None, vec![chain_10.clone(), chain_20.clone()]);
+        // Use the two above chains in the test
+        let chains = vec![chain_10.clone(), chain_20.clone()];
 
-        // Run network indexer and forward its events to the channel
-        let events = setup
-            .indexer
-            .take_event_stream()
-            .expect("failed to take stream from indexer")
-            .take(20)
-            .fuse()
-            .collect()
-            .wait()
-            .expect("failed to collect events from indexer");
+        // Run network indexer and collect its events
+        let events = run_network_indexer(store, None, chains);
 
         // Assert that the events emitted by the indexer match the blocks 1:1,
         // despite them requiring two remote head updates
@@ -389,32 +361,21 @@ fn tracing_picks_up_new_remote_head() {
     });
 }
 
-// GIVEN  a fresh subgraph (local head = None)
-// AND    a chain with 10 blocks with a gap (#5 missing)
+// GIVEN  a fresh subgraph (local head = none)
+// AND    a chain with 10 blocks with a gap (#6 missing)
 // WHEN   running the `NetworkIndexer`
-// EXPECT only `AddBlock` events for blocks #0-#4 are emitted
+// EXPECT only `AddBlock` events for blocks #0-#5 are emitted
 #[test]
-fn tracing_fails_if_there_is_a_gap() {
+fn indexing_does_not_move_past_a_gap() {
     run_test(|store: Arc<DieselStore>| -> Result<(), ()> {
-        // Create blocks for simulating the chain
+        // Create test chain
         let mut blocks = create_blocks(10, None);
-
-        // Remove block #5
+        // Remove block #6
         blocks.remove(5);
+        let chains = vec![blocks.clone()];
 
-        // Set up the test
-        let mut setup = TestSetup::new(store, None, vec![blocks.clone()]);
-
-        // Run network indexer and forward its events to the channel
-        let events = setup
-            .indexer
-            .take_event_stream()
-            .expect("failed to take stream from indexer")
-            .take(20)
-            .fuse()
-            .collect()
-            .wait()
-            .expect("failed to collect events from indexer");
+        // Run network indexer and collect its events
+        let events = run_network_indexer(store, None, chains);
 
         // Assert that only blocks #0 - #4 were indexed and nothing more
         assert_eq!(
@@ -430,65 +391,34 @@ fn tracing_fails_if_there_is_a_gap() {
     });
 }
 
-// GIVEN  a fresh subgraph (local head = None)
+// GIVEN  a fresh subgraph (local head = none)
 // AND    10 blocks for one version of the chain
 // AND    20 blocks for a fork of the chain that starts after block #3
 // WHEN   running the `NetworkIndexer`
 // EXPECT 10 `AddBlock` events is emitted for the first branch,
 //        7 `Revert` events are emitted to revert back to block #3
-//        16 `AddBlock` events are emitted for blocks #4-#20 of the fork
+//        17 `AddBlock` events are emitted for blocks #4-#20 of the fork
 #[test]
-fn tracing_handles_single_reorg() {
+fn indexing_handles_single_reorg() {
     run_test(|store: Arc<DieselStore>| -> Result<(), ()> {
-        // Create blocks for the initial chain
+        // Create the initial chain
         let initial_chain = create_blocks(10, None);
 
-        // Create blocks for the forked chain after block #3
+        // Create a forked chain after block #2
         let forked_chain = create_fork(initial_chain.clone(), 2, 20);
 
-        // Set up the test
+        // Run the network indexer and collect its events
         let chains = vec![initial_chain.clone(), forked_chain.clone()];
-        let mut setup = TestSetup::new(store, None, chains);
+        let events = run_network_indexer(store, None, chains);
 
-        debug!(setup.logger, "Original chain:");
-        for block in initial_chain.iter() {
-            debug!(
-                setup.logger,
-                "  {} [{:x}]",
-                block.inner().number.unwrap(),
-                block.inner().hash.unwrap()
-            );
-        }
-
-        debug!(setup.logger, "Forked chain:");
-        for block in forked_chain.iter() {
-            debug!(
-                setup.logger,
-                "  {} [{:x}]",
-                block.inner().number.unwrap(),
-                block.inner().hash.unwrap()
-            );
-        }
-
-        // Run network indexer and forward its events to the channel
-        let events = setup
-            .indexer
-            .take_event_stream()
-            .expect("failed to take stream from indexer")
-            .take(50)
-            .fuse()
-            .collect()
-            .wait()
-            .expect("failed to collect events from indexer");
-
-        // Verify that the following events are emitted in exactly the same
-        // sequence:
-        // 10 `AddBlock` events for blocks #0-#9 of the initial chain
-        // 7 `Revert` events from #9 to #8, ..., #4 to #3 (the fork base)
-        // 14 `AddBlock` events for blocks #4-#20 of the fork
+        // Verify that the following events are emitted:
+        //
+        // - 10 `AddBlock` events for blocks #0 to #9 of the initial chain
+        // - 7 `Revert` events from #9 to #8, ..., #3 to #2 (the fork base)
+        // - 17 `AddBlock` events for blocks #3 to #20 of the fork
         assert_eq!(
             events,
-            // The 10 `AddBlock` events
+            // The 10 `AddBlock` events for the initial version of the chain
             initial_chain
                 .iter()
                 .map(|block| NetworkIndexerEvent::AddBlock(block.inner().into()))
@@ -504,7 +434,7 @@ fn tracing_handles_single_reorg() {
                         })
                 )
                 .chain(
-                    // The 16 `AddBlock` events for the fork
+                    // The 17 `AddBlock` events for the new chain
                     forked_chain[3..]
                         .iter()
                         .map(|block| NetworkIndexerEvent::AddBlock(block.inner().into()))
