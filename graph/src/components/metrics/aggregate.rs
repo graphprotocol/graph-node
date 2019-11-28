68c51b06cdff77bea25401a183f2d7d1bdd103ed
Jannis Pohlmann <jannis@thegraph.com>
2019-11-28 18:12:28 +0000

    graph: Don't require `Aggregate` to be mutable

--- graph/src/components/metrics/aggregate.rs
+++ graph/src/components/metrics/aggregate.rs
@@ -5,27 +5,21 @@ use crate::prelude::*;
 
 pub struct Aggregate {
     /// Number of values.
-    n: u64,
+    count: Box<Gauge>,
 
     /// Sum over all values.
-    sum: f64,
+    sum: Box<Gauge>,
 
     /// Moving average over the values.
-    avg: f64,
+    avg: Box<Gauge>,
 
     /// Latest value.
-    cur: f64,
-
-    /// Metrics for Prometheus.
-    count_gauge: Box<Gauge>,
-    sum_gauge: Box<Gauge>,
-    avg_gauge: Box<Gauge>,
-    cur_gauge: Box<Gauge>,
+    cur: Box<Gauge>,
 }
 
 impl Aggregate {
     pub fn new(name: String, help: &str, registry: Arc<dyn MetricsRegistry>) -> Self {
-        let count_gauge = registry
+        let count = registry
             .new_gauge(
                 format!("{}_count", name),
                 format!("{} (count)", help),
@@ -33,7 +27,7 @@ impl Aggregate {
             )
             .expect(format!("failed to register metric `{}_count`", name).as_str());
 
-        let sum_gauge = registry
+        let sum = registry
             .new_gauge(
                 format!("{}_sum", name),
                 format!("{} (sum)", help),
@@ -41,14 +35,15 @@ impl Aggregate {
             )
             .expect(format!("failed to register metric `{}_sum`", name).as_str());
 
-        let avg_gauge = registry
+        let avg = registry
             .new_gauge(
                 format!("{}_avg", name),
                 format!("{} (avg)", help),
                 HashMap::new(),
             )
             .expect(format!("failed to register metric `{}_avg`", name).as_str());
-        let cur_gauge = registry
+
+        let cur = registry
             .new_gauge(
                 format!("{}_cur", name),
                 format!("{} (cur)", help),
@@ -57,32 +52,30 @@ impl Aggregate {
             .expect(format!("failed to register metric `{}_cur`", name).as_str());
 
         Aggregate {
-            n: 0,
-            sum: 0.0,
-            avg: 0.0,
-            cur: 0.0,
-            count_gauge,
-            sum_gauge,
-            avg_gauge,
-            cur_gauge,
+            count,
+            sum,
+            avg,
+            cur,
         }
     }
 
-    pub fn update(&mut self, x: f64) {
-        // Update aggregate values.
-        self.n += 1;
-        self.sum += x;
-        self.cur = x;
-        self.avg = self.avg + (x - self.avg) / (self.n as f64);
+    pub fn update(&self, x: f64) {
+        // Update count
+        self.count.inc();
+        let n = self.count.get();
 
-        // Update gauges
-        self.count_gauge.set(self.n as f64);
-        self.sum_gauge.set(self.sum);
-        self.avg_gauge.set(self.avg);
-        self.cur_gauge.set(self.cur);
+        // Update sum
+        self.sum.add(x);
+
+        // Update current value
+        self.cur.set(x);
+
+        // Update aggregate value.
+        let avg = self.avg.get();
+        self.avg.set(avg + (x - avg) / n);
     }
 
-    pub fn update_duration(&mut self, x: Duration) {
+    pub fn update_duration(&self, x: Duration) {
         self.update(x.as_secs_f64())
     }
 }
