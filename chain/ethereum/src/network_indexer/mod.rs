5d32dfd5b18cc57a0e94e716a748cffcb759359c
Jannis Pohlmann <jannis@thegraph.com>
2019-12-12 15:10:55 +0100

    chain/ethereum: Update network indexer tests

--- chain/ethereum/src/network_indexer/mod.rs
+++ chain/ethereum/src/network_indexer/mod.rs
@@ -1,5 +1,6 @@
 use graph::prelude::*;
 use std::fmt;
+use std::ops::Deref;
 use web3::types::{Block, H256};
 
 mod block_writer;
@@ -20,6 +21,48 @@ const NETWORK_INDEXER_VERSION: u32 = 0;
 /// Helper type to represent ommer blocks.
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct Ommer(Block<H256>);
+
+impl From<Block<H256>> for Ommer {
+    fn from(block: Block<H256>) -> Self {
+        Self(block)
+    }
+}
+
+impl From<LightEthereumBlock> for Ommer {
+    fn from(block: LightEthereumBlock) -> Self {
+        Self(Block {
+            hash: block.hash,
+            parent_hash: block.parent_hash,
+            uncles_hash: block.uncles_hash,
+            author: block.author,
+            state_root: block.state_root,
+            transactions_root: block.transactions_root,
+            receipts_root: block.receipts_root,
+            number: block.number,
+            gas_used: block.gas_used,
+            gas_limit: block.gas_limit,
+            extra_data: block.extra_data,
+            logs_bloom: block.logs_bloom,
+            timestamp: block.timestamp,
+            difficulty: block.difficulty,
+            total_difficulty: block.total_difficulty,
+            seal_fields: block.seal_fields,
+            uncles: block.uncles,
+            transactions: block.transactions.into_iter().map(|tx| tx.hash).collect(),
+            size: block.size,
+            mix_hash: block.mix_hash,
+            nonce: block.nonce,
+        })
+    }
+}
+
+impl Deref for Ommer {
+    type Target = Block<H256>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 /// Helper type to bundle blocks and their ommers together.
 #[derive(Clone, Debug, Default, PartialEq)]
