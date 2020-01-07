f01f67b5caad93f42c26b65251747f02c0b69a8d
Jannis Pohlmann <jannis@thegraph.com>
2020-01-07 16:12:48 +0100

    tests: Add integration test for calling overloaded contract functions

---
+++ tests/overloaded-contract-functions/test/test.js
@@ -0,0 +1,103 @@
+const path = require("path");
+const execSync = require("child_process").execSync;
+const { system, patching } = require("gluegun");
+const { createApolloFetch } = require("apollo-fetch");
+
+const Contract = artifacts.require("./Contract.sol");
+
+const srcDir = path.join(__dirname, "..");
+
+const fetchSubgraphs = createApolloFetch({
+  uri: "http://localhost:8030/graphql"
+});
+const fetchSubgraph = createApolloFetch({
+  uri: "http://localhost:8000/subgraphs/name/test/overloaded-contract-functions"
+});
+
+const exec = cmd => {
+  try {
+    return execSync(cmd, { cwd: srcDir, stdio: "inherit" });
+  } catch (e) {
+    throw new Error(`Failed to run command \`${cmd}\``);
+  }
+};
+
+const waitForSubgraphToBeSynced = async () =>
+  new Promise((resolve, reject) => {
+    // Wait for 5s
+    let deadline = Date.now() + 5 * 1000;
+
+    // Function to check if the subgraph is synced
+    const checkSubgraphSynced = async () => {
+      try {
+        let result = await fetchSubgraphs({
+          query: `{ indexingStatuses { synced } }`
+        });
+
+        if (
+          JSON.stringify(result) ===
+          JSON.stringify({ data: { indexingStatuses: [{ synced: true }] } })
+        ) {
+          resolve();
+        } else {
+          throw new Error("reject or retry");
+        }
+      } catch (e) {
+        if (Date.now() > deadline) {
+          reject(new Error(`Timed out waiting for the subgraph to sync`));
+        } else {
+          setTimeout(checkSubgraphSynced, 500);
+        }
+      }
+    };
+
+    // Periodically check whether the subgraph has synced
+    setTimeout(checkSubgraphSynced, 0);
+  });
+
+contract("Contract", accounts => {
+  // Deploy the subgraph once before all tests
+  before(async () => {
+    // Deploy the contract
+    const contract = await Contract.deployed();
+
+    // Insert its address into subgraph manifest
+    await patching.replace(
+      path.join(srcDir, "subgraph.yaml"),
+      "0x0000000000000000000000000000000000000000",
+      contract.address
+    );
+
+    // Create and deploy the subgraph
+    exec(`yarn codegen`);
+    exec(`yarn create:local`);
+    exec(`yarn deploy:local`);
+
+    // Wait for the subgraph to be indexed
+    await waitForSubgraphToBeSynced();
+  });
+
+  it("all overloads of the contract function are called", async () => {
+    let result = await fetchSubgraph({
+      query: `{ calls(orderBy: id) { id value } }`
+    });
+
+    expect(result.errors).to.be.undefined;
+    expect(result.data).to.deep.equal({
+      calls: [
+        {
+          id: "bytes32 -> uint256",
+          value: "256"
+        },
+        {
+          id: "string -> string",
+          value: "string -> string"
+        },
+        {
+          id: "uint256 -> string",
+          value: "uint256 -> string"
+        }
+      ]
+    });
+  });
+});
