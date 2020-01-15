cb8c45593f95d6df8093983fd0f0724aeae7af7b
Jannis Pohlmann <jannis@thegraph.com>
2020-01-15 14:25:06 +0100

    tests: Allow to run individual integration tests

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#
+# Usage:   ./run-tests [<test-name>]
+# Example: ./run-tests overloaded-contract-functions
 
 set -e
+
+TEST="$1"
 
 echo
 echo "------------------------------------------------------------------------"
@@ -12,15 +17,17 @@ cargo build -p graph-node
 
 cd tests
 
-echo
-echo "------------------------------------------------------------------------"
-echo " Overloaded contract functions"
-echo "========================================================================"
-echo
+if [ -z "$TEST" ] || [ "$TEST" = "overloaded-contract-functions" ]; then
+    echo
+    echo "------------------------------------------------------------------------"
+    echo " Overloaded contract functions"
+    echo "========================================================================"
+    echo
 
-cd overloaded-contract-functions
-yarn
-./node_modules/.bin/graph test --standalone-node ../../target/debug/graph-node "yarn test"
-cd ..
+    cd overloaded-contract-functions
+    yarn
+    ./node_modules/.bin/graph test --standalone-node ../../target/debug/graph-node "yarn test"
+    cd ..
+fi
 
 cd ..
