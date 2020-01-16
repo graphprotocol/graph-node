3bce8dca96fc82193cf3a83b4a62724bf7c36d30
Jannis Pohlmann <jannis@thegraph.com>
2020-01-16 17:20:43 +0100

    tests: Update integration test to latest `graph test`

--- tests/integration-tests/overloaded-contract-functions/truffle.js
+++ tests/integration-tests/overloaded-contract-functions/truffle.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     test: {
       host: "localhost",
-      port: 8545,
+      port: 18545,
       network_id: "*",
       gas: "100000000000",
       gasPrice: "1"
