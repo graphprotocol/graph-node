96f3e370827eba8e83b19783f0a6dc9aade9f683
Ford Nickels <ford@thegraph.com>
2020-02-14 13:24:32 -0800

    graph, store: Scrape fulltext columns from entity field directives

--- graph/src/data/schema.rs
+++ graph/src/data/schema.rs
@@ -469,6 +469,16 @@ impl Schema {
             })
     }
 
+    pub fn name_argument_value_from_directive(directive: &Directive) -> Value {
+        directive
+            .arguments
+            .iter()
+            .find(|(name, _value)| name == "name")
+            .expect("full text directive must have name argument")
+            .1
+            .clone()
+    }
+
     fn schema_reference_from_directive_argument(
         &self,
         from: &(Name, Value),
