1f0c3ebe710efa39b3e87b7bdbbecca0a586b757
Jorge Olivero <jorgeaolivero@gmail.com>
2020-02-26 12:29:11 -0600

    graph: Language typo

--- graph/src/data/schema.rs
+++ graph/src/data/schema.rs
@@ -131,7 +131,7 @@ impl TryFrom<&String> for FulltextLanguage {
             "SWEDISH" => Ok(FulltextLanguage::Swedish),
             "TURKISH" => Ok(FulltextLanguage::Turkish),
             invalid => Err(format!(
-                "Provided langauge for fulltext search is invalid: {}",
+                "Provided language for fulltext search is invalid: {}",
                 invalid
             )),
         }
