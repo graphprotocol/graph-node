eda89313442727990d89a70ec8495678b0baf4ea
Leonardo Yvens <leoyvens@gmail.com>
2020-03-25 15:47:44 -0300

    tests: Use `DataSourceContext` class

--- tests/integration-tests/data-source-context/src/mapping.ts
+++ tests/integration-tests/data-source-context/src/mapping.ts
@@ -1,9 +1,9 @@
-import { Entity, Address } from "@graphprotocol/graph-ts";
+import { DataSourceContext, Address } from "@graphprotocol/graph-ts";
 import { Trigger } from "../generated/Contract/Contract";
 import { Dynamic } from "../generated/templates";
 
 export function handleTrigger(event: Trigger): void {
-  let context = new Entity();
+  let context = new DataSourceContext();
   context.setI32("answer", 42);
   Dynamic.createWithContext(
     Address.fromHexString(
