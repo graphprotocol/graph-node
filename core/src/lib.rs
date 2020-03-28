9e6b7e466792cf2952ea24b3eb4e68df27816368
Jannis Pohlmann <jannis@thegraph.com>
2020-03-28 17:24:48 +0100

    core: Add NetworkRegistry implementation

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -13,11 +13,13 @@ extern crate uuid;
 mod graphql;
 mod link_resolver;
 mod metrics;
+mod networks;
 mod subgraph;
 
 pub use crate::graphql::GraphQlRunner;
 pub use crate::link_resolver::LinkResolver;
 pub use crate::metrics::MetricsRegistry;
+pub use crate::networks::NetworkRegistry;
 pub use crate::subgraph::{
     DataSourceLoader, SubgraphAssignmentProvider, SubgraphInstanceManager, SubgraphRegistrar,
 };
---
+++ core/src/networks/mod.rs
@@ -0,0 +1,31 @@
+use graph::prelude::{NetworkInstance, NetworkInstanceId, NetworkRegistry as NetworkRegistryTrait};
+
+pub struct NetworkRegistry {
+  instances: Vec<Box<dyn NetworkInstance>>,
+}
+
+impl NetworkRegistry {
+  pub fn new() -> Self {
+    Self {
+      instances: Default::default(),
+    }
+  }
+}
+
+impl NetworkRegistryTrait for NetworkRegistry {
+  fn register_instance(&mut self, instance: Box<dyn NetworkInstance>) {
+    self.instances.push(instance);
+  }
+
+  fn instances(&self, network: &str) -> Vec<&Box<dyn NetworkInstance>> {
+    self
+      .instances
+      .iter()
+      .filter(|instance| instance.id().network.eq(network))
+      .collect()
+  }
+
+  fn instance(&self, id: NetworkInstanceId) -> Option<&Box<dyn NetworkInstance>> {
+    self.instances.iter().find(|instance| instance.id().eq(&id))
+  }
+}
