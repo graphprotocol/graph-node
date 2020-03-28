9441cd7f66d7ff45e16502702a95e878b4bf55c9
Jannis Pohlmann <jannis@thegraph.com>
2020-03-28 17:24:48 +0100

    core: Add NetworkRegistry implementation

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,12 +1,14 @@
 mod graphql;
 mod link_resolver;
 mod metrics;
+mod networks;
 mod subgraph;
 pub mod three_box;
 
 pub use crate::graphql::GraphQlRunner;
 pub use crate::link_resolver::LinkResolver;
 pub use crate::metrics::MetricsRegistry;
+pub use crate::networks::NetworkRegistry;
 pub use crate::subgraph::{
     DataSourceLoader, SubgraphAssignmentProvider, SubgraphInstanceManager, SubgraphRegistrar,
 };
