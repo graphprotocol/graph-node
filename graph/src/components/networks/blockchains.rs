89d145584f8b9a0d53e0bfad6979f19ea630eef0
Jannis Pohlmann <jannis@thegraph.com>
2020-03-30 00:06:25 +0200

    graph: Implement Display for BlockPointer

--- graph/src/components/networks/blockchains.rs
+++ graph/src/components/networks/blockchains.rs
@@ -1,7 +1,15 @@
+use std::fmt;
+
 use crate::data::store::scalar::Bytes;
 
 #[derive(Debug, PartialEq)]
 pub struct BlockPointer {
-  pub number: u64,
-  pub hash: Bytes,
+    pub number: u64,
+    pub hash: Bytes,
+}
+
+impl fmt::Display for BlockPointer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "#{} ({})", self.number, self.hash.as_hex_string())
+    }
 }
