ce0756f7f22d83ce6e2802a2374c6a3c4be8bcf1
Jannis Pohlmann <jannis@thegraph.com>
2020-03-30 00:09:38 +0200

    chain/ethereum: Rework chain instantiation

--- chain/ethereum/src/chain/mod.rs
+++ chain/ethereum/src/chain/mod.rs
@@ -3,133 +3,47 @@ use std::sync::Arc;
 use async_trait::async_trait;
 
 use graph::prelude::{
-    format_err, info, BlockPointer, BlockchainStore, BlockchainStoreId, ChainStore, CheapClone,
-    Error, EthereumAdapter as EthereumAdapterTrait, Future01CompatExt, Logger, MetricsRegistry,
-    NetworkInstance, NetworkInstanceId, NetworkStoreFactory, ProviderEthRpcMetrics,
+    format_err, BlockPointer, ChainStore, CheapClone, Error,
+    EthereumAdapter as EthereumAdapterTrait, Future01CompatExt, Logger, MetricsRegistry,
+    NetworkInstance, NetworkInstanceId, ProviderEthRpcMetrics, Store,
 };
-use graph::util::security::SafeDisplay;
 
 use crate::{EthereumAdapter, Transport};
 
-pub struct ChainOptions<MR, NSF> {
-    pub logger: Logger,
-    pub store_factory: Arc<NSF>,
+pub struct Descriptor {
+    pub id: NetworkInstanceId,
+    pub url: String,
+}
+
+pub struct ConnectOptions<'a, MR> {
+    pub descriptor: Descriptor,
+    pub logger: &'a Logger,
     pub metrics_registry: Arc<MR>,
 }
 
-pub struct Chain {
-    id: NetworkInstanceId,
-    url: String,
-    adapter: Arc<dyn EthereumAdapterTrait>,
-    store: Arc<Box<dyn BlockchainStore>>,
+pub struct Connection {
+    pub id: NetworkInstanceId,
+    pub url: String,
+    pub version: String,
+    pub genesis_block: BlockPointer,
+    pub adapter: Arc<dyn EthereumAdapterTrait>,
 }
 
-impl Chain {
-    pub async fn from_descriptor<MR, NSF>(
-        descriptor: &str,
-        options: ChainOptions<MR, NSF>,
-    ) -> Result<Self, Error>
-    where
-        MR: MetricsRegistry,
-        NSF: NetworkStoreFactory,
-    {
-        // Try to parse the "mainnet:http://..." type descriptor into a network
-        // instance ID and URL
-        let (id, url) = parse_descriptor(descriptor)?;
-
-        info!(
-            options.logger,
-            "Add Ethereum chain";
-            "url" => SafeDisplay(&url),
-            "name" => &id.name,
-        );
-
-        // Establish an Ethereum adapter for this chain
-        let (transport_event_loop, transport) = Transport::new_rpc(url.as_str());
-        let adapter_metrics = Arc::new(ProviderEthRpcMetrics::new(options.metrics_registry));
-        let adapter = Arc::new(EthereumAdapter::new(transport, adapter_metrics));
-
-        info!(
-            options.logger,
-            "Connecting to Ethereum chain";
-            "url" => SafeDisplay(&url),
-            "name" => &id.name,
-        );
-
-        // Connect to the node and obtain the network ID and version
-        let identifier = adapter
-            .net_identifiers(&options.logger)
-            .compat()
-            .await
-            .map_err(|e| {
-                format_err!(
-                    "Failed to connect to Ethereum chain `{}` at `{}`: {}",
-                    id.name,
-                    SafeDisplay(&url),
-                    e
-                )
-            })?;
-
-        info!(
-             options.logger,
-             "Successfully connected to Ethereum chain";
-             "url" => SafeDisplay(&url),
-             "name" => &id.name,
-             "net_version" => &identifier.net_version,
-        );
-
-        // Establish a store for this chain
-        let store = options
-            .store_factory
-            .blockchain_store(&BlockchainStoreId {
-                network_instance: id.clone(),
-                genesis_block: BlockPointer {
-                    number: 0,
-                    hash: identifier.genesis_block_hash.as_bytes().into(),
-                },
-            })
-            .await?;
-
-        // If we drop the event loop the transport will stop working.
-        // For now it's fine to just leak it.
-        std::mem::forget(transport_event_loop);
-
-        Ok(Self {
-            id,
-            url,
-            adapter,
-            store,
-        })
-    }
+pub struct ChainOptions<MR, S> {
+    pub conn: Connection,
+    pub logger: Logger,
+    pub store: Arc<S>,
+    pub metrics_registry: Arc<MR>,
 }
 
-#[async_trait]
-impl NetworkInstance for Chain {
-    fn id(&self) -> &NetworkInstanceId {
-        &self.id
-    }
-
-    fn url(&self) -> &str {
-        self.url.as_str()
-    }
-
-    fn compat_ethereum_adapter(&self) -> Option<Arc<dyn EthereumAdapterTrait>> {
-        Some(self.adapter.cheap_clone())
-    }
-
-    fn compat_blockchain_store(&self) -> Option<Arc<Box<dyn BlockchainStore>>> {
-        Some(self.store.cheap_clone())
-    }
-}
-
-fn parse_descriptor(descriptor: &str) -> Result<(NetworkInstanceId, String), Error> {
+pub fn parse_descriptor(descriptor: &str) -> Result<Descriptor, Error> {
     if descriptor.starts_with("wss://")
         || descriptor.starts_with("http://")
         || descriptor.starts_with("https://")
     {
         return Err(format_err!(
             "Is your Ethereum node string missing a chain name? \
-                 Try 'mainnet:' + the Ethereum node URL."
+                     Try 'mainnet:' + the Ethereum node URL."
         ));
     }
 
@@ -137,7 +51,7 @@ fn parse_descriptor(descriptor: &str) -> Result<(NetworkInstanceId, String), Err
     let split_at = descriptor.find(':').ok_or_else(|| {
         format_err!(
             "A chain name must be provided alongside the \
-                 Ethereum node location. Try e.g. 'mainnet:URL'."
+                     Ethereum node location. Try e.g. 'mainnet:URL'."
         )
     })?;
 
@@ -152,11 +66,97 @@ fn parse_descriptor(descriptor: &str) -> Result<(NetworkInstanceId, String), Err
         Err(format_err!("Ethereum node URL cannot be an empty string"))?;
     }
 
-    Ok((
-        NetworkInstanceId {
+    Ok(Descriptor {
+        id: NetworkInstanceId {
             network: "ethereum".into(),
             name: name.into(),
         },
-        loc.into(),
-    ))
+        url: loc.into(),
+    })
+}
+
+pub async fn connect<'a, MR>(options: ConnectOptions<'a, MR>) -> Result<Connection, Error>
+where
+    MR: MetricsRegistry,
+{
+    let ConnectOptions {
+        descriptor,
+        logger,
+        metrics_registry,
+    } = options;
+
+    // Establish an Ethereum adapter for this chain
+    let (transport_event_loop, transport) = Transport::new_rpc(descriptor.url.as_str());
+    let adapter_metrics = Arc::new(ProviderEthRpcMetrics::new(metrics_registry));
+    let adapter = Arc::new(EthereumAdapter::new(transport, adapter_metrics));
+
+    // Connect to the node and obtain the network ID and version
+    let identifier = adapter.net_identifiers(&logger).compat().await?;
+
+    // If we drop the event loop the transport will stop working.
+    // For now it's fine to just leak it.
+    std::mem::forget(transport_event_loop);
+
+    Ok(Connection {
+        id: descriptor.id,
+        url: descriptor.url,
+        adapter: adapter,
+        version: identifier.net_version,
+        genesis_block: BlockPointer {
+            number: 0,
+            hash: identifier.genesis_block_hash.as_bytes().into(),
+        },
+    })
+}
+
+pub struct Chain<S> {
+    id: NetworkInstanceId,
+    url: String,
+    version: String,
+    genesis_block: BlockPointer,
+    adapter: Arc<dyn EthereumAdapterTrait>,
+    store: Arc<S>,
+    metrics_registry: Arc<dyn MetricsRegistry>,
+}
+
+impl<S> Chain<S>
+where
+    S: Store + ChainStore,
+{
+    pub async fn new<MR>(options: ChainOptions<MR, S>) -> Result<Self, Error>
+    where
+        MR: MetricsRegistry,
+    {
+        options
+            .store
+            .initialize_chain(&options.conn.version, &options.conn.genesis_block)?;
+
+        Ok(Self {
+            id: options.conn.id,
+            url: options.conn.url,
+            version: options.conn.version,
+            genesis_block: options.conn.genesis_block,
+            adapter: options.conn.adapter,
+            store: options.store,
+            metrics_registry: options.metrics_registry,
+        })
+    }
+}
+
+#[async_trait]
+impl<S> NetworkInstance for Chain<S>
+where
+    S: Store + ChainStore,
+{
+    fn id(&self) -> &NetworkInstanceId {
+        &self.id
+    }
+
+    fn url(&self) -> &str {
+        self.url.as_str()
+    }
+
+    fn compat_ethereum_adapter(&self) -> Option<Arc<dyn EthereumAdapterTrait>> {
+        Some(self.adapter.cheap_clone())
+    }
 }
--- chain/ethereum/src/lib.rs
+++ chain/ethereum/src/lib.rs
@@ -3,13 +3,12 @@ extern crate lazy_static;
 
 mod block_ingestor;
 mod block_stream;
-mod chain;
+pub mod chain;
 mod ethereum_adapter;
 pub mod network_indexer;
 mod transport;
 
 pub use self::block_ingestor::{BlockIngestor, BlockIngestorMetrics};
 pub use self::block_stream::{BlockStream, BlockStreamBuilder};
-pub use self::chain::{Chain, ChainOptions};
 pub use self::ethereum_adapter::EthereumAdapter;
 pub use self::transport::{EventLoopHandle, Transport};
