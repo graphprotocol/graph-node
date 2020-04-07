cdff79e38331d7ae6e1d8e45615741773bf5c0ed
Leonardo Yvens <leoyvens@gmail.com>
2020-04-07 11:32:25 -0300

    tests: Undo accidental change to test contract

--- tests/integration-tests/arweave-and-3box/contracts/Contract.sol
+++ tests/integration-tests/arweave-and-3box/contracts/Contract.sol
@@ -1,21 +1,10 @@
 pragma solidity ^0.6.1;
 
+
 contract Contract {
-  event Trigger();
+    event Trigger();
 
-  constructor() public {
-    emit Trigger();
-  }
-
-  function exampleFunction(string memory) public pure returns (string memory) {
-    return "string -> string";
-  }
-
-  function exampleFunction(uint256) public pure returns (string memory) {
-    return "uint256 -> string";
-  }
-
-  function exampleFunction(bytes32) public pure returns (uint256) {
-    return 256;
-  }
+    constructor() public {
+        emit Trigger();
+    }
 }
