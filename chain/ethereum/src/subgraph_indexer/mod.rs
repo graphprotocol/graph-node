0998d914742a751853d37ebdcd7eaf533cd3a076
Jannis Pohlmann <jannis@thegraph.com>
2020-04-10 16:00:31 +0200

    chain/ethereum: Reorganize crate modules

--- chain/ethereum/src/subgraph_indexer/mod.rs
+++ chain/ethereum/src/subgraph_indexer/mod.rs
@@ -1,4 +1,8 @@
+mod block_ingestor;
+mod block_stream;
 mod indexer;
 mod instance;
 
+pub use self::block_ingestor::{BlockIngestor, BlockIngestorMetrics};
+pub use self::block_stream::{BlockStream, BlockStreamBuilder};
 pub use self::indexer::SubgraphIndexer;
