c8fd0b5b6f6cc0564dae45e6229f9e7e80f4ca9a
Jannis Pohlmann <jannis@thegraph.com>
2020-04-10 16:00:31 +0200

    chain/ethereum: Reorganize crate modules

--- chain/ethereum/src/lib.rs
+++ chain/ethereum/src/lib.rs
@@ -1,16 +1,16 @@
 #[macro_use]
 extern crate lazy_static;
 
-mod block_ingestor;
-mod block_stream;
 pub mod chain;
 mod ethereum_adapter;
 pub mod network_indexer;
 mod subgraph_indexer;
 mod transport;
 
-pub use self::block_ingestor::{BlockIngestor, BlockIngestorMetrics};
-pub use self::block_stream::{BlockStream, BlockStreamBuilder};
+// Internal components
+pub(crate) use self::subgraph_indexer::BlockStreamBuilder;
+
+// Public components
 pub use self::ethereum_adapter::EthereumAdapter;
-pub use self::subgraph_indexer::SubgraphIndexer;
+pub use self::subgraph_indexer::{BlockIngestor, BlockIngestorMetrics, SubgraphIndexer};
 pub use self::transport::{EventLoopHandle, Transport};
