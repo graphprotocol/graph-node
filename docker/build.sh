e4bc51fe0758ebbf7af7e3522f15526bcb893671
David Lutterkort <lutter@watzmann.net>
2020-05-01 14:10:04 -0700

    docker: Mover Docker images to Alpine

---
+++ docker/build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# This file is only here to ease testing/development. The commands in this
+# file should ultimately be moved to the corresponding commands for Cloud Build
+# in the cloudbuild.*.yaml files
+
+type -p podman > /dev/null && docker=podman || docker=docker
+
+for stage in graph-node-bld \
+                 graph-node-run \
+                 query-node \
+                 index-node \
+                 graph-node-dbg
+do
+    $docker build -t $stage --target $stage .
+done
