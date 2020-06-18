439de8b42b4d7bf30b30e9b5db67a30c7354b300
Leonardo Yvens <leoyvens@gmail.com>
2020-06-18 10:19:50 -0300

    ethereum: increase default json rpc timeout

--- chain/ethereum/src/ethereum_adapter.rs
+++ chain/ethereum/src/ethereum_adapter.rs
@@ -46,9 +46,10 @@ lazy_static! {
             .expect("invalid ETHEREUM_BLOCK_BATCH_SIZE env var");
 
     /// This should not be too large that it causes requests to timeout without us catching it, nor
-    /// too small that it causes us to timeout requests that would've succeeded.
+    /// too small that it causes us to timeout requests that would've succeeded. We've seen
+    /// successful `eth_getLogs` requests take over 120 seconds.
     static ref JSON_RPC_TIMEOUT: u64 = std::env::var("GRAPH_ETHEREUM_JSON_RPC_TIMEOUT")
-            .unwrap_or("120".into())
+            .unwrap_or("180".into())
             .parse::<u64>()
             .expect("invalid GRAPH_ETHEREUM_JSON_RPC_TIMEOUT env var");
 
