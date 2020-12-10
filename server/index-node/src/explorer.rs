19e7efa006056ce86409b448ab20a4a17a09bc47
David Lutterkort <lutter@watzmann.net>
2020-12-10 09:34:12 -0800

    server: Cache responses for the explorer API for a fixed amount of time

--- server/index-node/src/explorer.rs
+++ server/index-node/src/explorer.rs
@@ -4,18 +4,46 @@
 use graphql_parser::query as q;
 use http::{Response, StatusCode};
 use hyper::Body;
-use std::sync::Arc;
-
-use graph::{
-    components::server::query::GraphQLServerError,
-    data::subgraph::status,
-    object,
-    prelude::{serde_json, SerializableValue, Store},
+use std::{
+    collections::HashMap,
+    env,
+    str::FromStr,
+    sync::{Arc, RwLock},
+    time::{Duration, Instant},
 };
 
+use graph::{
+    components::server::{index_node::VersionInfo, query::GraphQLServerError},
+    data::subgraph::status,
+    object,
+    prelude::{lazy_static, serde_json, SerializableValue, Store},
+};
+
+lazy_static! {
+    static ref TTL: Duration = {
+        let ttl = env::var("GRAPH_EXPLORER_TTL")
+            .ok()
+            .map(|s| {
+                u64::from_str(&s).unwrap_or_else(|_| {
+                    panic!("GRAPH_EXPLORER_TTL must be a number, but is `{}`", s)
+                })
+            })
+            .unwrap_or(10);
+        Duration::from_secs(ttl)
+    };
+}
+
+// Do not implement `Clone` for this; the IndexNode service puts the `Explorer`
+// behind an `Arc` so we don't have to put each `Cache` into an `Arc`
+//
+// We cache responses for a fixed amount of time with the time given by
+// `GRAPH_EXPLORER_TTL`
 #[derive(Debug)]
 pub struct Explorer<S> {
     store: Arc<S>,
+    versions: Cache<q::Value>,
+    version_infos: Cache<VersionInfo>,
+    entity_counts: Cache<q::Value>,
 }
 
 impl<S> Explorer<S>
@@ -23,7 +51,12 @@ where
     S: Store,
 {
     pub fn new(store: Arc<S>) -> Self {
-        Self { store }
+        Self {
+            store,
+            versions: Cache::new(),
+            version_infos: Cache::new(),
+            entity_counts: Cache::new(),
+        }
     }
 
     pub fn handle(&self, req: &[&str]) -> Result<Response<Body>, GraphQLServerError> {
@@ -42,6 +75,10 @@ where
         &self,
         subgraph_id: &str,
     ) -> Result<Response<Body>, GraphQLServerError> {
+        if let Some(value) = self.versions.get(subgraph_id) {
+            return Ok(as_http_response(value.as_ref()));
+        }
+
         let (current, pending) = self.store.versions_for_subgraph_id(subgraph_id)?;
 
         let value = object! {
@@ -49,39 +86,45 @@ where
             pendingVersion: pending
         };
 
-        Ok(as_http_response(value))
+        let resp = as_http_response(&value);
+        self.versions.set(subgraph_id.to_string(), Arc::new(value));
+        Ok(resp)
     }
 
     fn handle_subgraph_version(&self, version: &str) -> Result<Response<Body>, GraphQLServerError> {
-        let vi = self.store.version_info(version)?;
+        let vi = self.version_info(version)?;
 
         let value = object! {
-            createdAt: vi.created_at,
-            deploymentId: vi.deployment_id,
+            createdAt: vi.created_at.as_str(),
+            deploymentId: vi.deployment_id.as_str(),
             latestEthereumBlockNumber: vi.latest_ethereum_block_number,
             totalEthereumBlocksCount: vi.total_ethereum_blocks_count,
             synced: vi.synced,
             failed: vi.failed,
-            description: vi.description,
-            repository: vi.repository,
+            description: vi.description.as_ref().map(|s| s.as_str()),
+            repository: vi.repository.as_ref().map(|s| s.as_str()),
             schema: vi.schema.document.to_string(),
-            network: vi.network
+            network: vi.network.as_ref().map(|s| s.as_str())
         };
-        Ok(as_http_response(value))
+        Ok(as_http_response(&value))
     }
 
     fn handle_subgraph_repo(&self, version: &str) -> Result<Response<Body>, GraphQLServerError> {
-        let vi = self.store.version_info(version)?;
+        let vi = self.version_info(version)?;
 
         let value = object! {
-            createdAt: vi.created_at,
-            deploymentId: vi.deployment_id,
-            repository: vi.repository
+            createdAt: vi.created_at.as_str(),
+            deploymentId: vi.deployment_id.as_str(),
+            repository: vi.repository.as_ref().map(|s| s.as_str())
         };
-        Ok(as_http_response(value))
+        Ok(as_http_response(&value))
     }
 
     fn handle_entity_count(&self, deployment: &str) -> Result<Response<Body>, GraphQLServerError> {
+        if let Some(value) = self.entity_counts.get(deployment) {
+            return Ok(as_http_response(value.as_ref()));
+        }
+
         let infos = self
             .store
             .status(status::Filter::Deployments(vec![deployment.to_string()]))?;
@@ -95,8 +138,21 @@ where
         let value = object! {
             entityCount: info.entity_count
         };
+        let resp = as_http_response(&value);
+        self.entity_counts
+            .set(deployment.to_string(), Arc::new(value));
+        Ok(resp)
+    }
 
-        Ok(as_http_response(value))
+    fn version_info(&self, version: &str) -> Result<Arc<VersionInfo>, GraphQLServerError> {
+        match self.version_infos.get(version) {
+            Some(vi) => Ok(vi),
+            None => {
+                let vi = Arc::new(self.store.version_info(version)?);
+                self.version_infos.set(version.to_string(), vi.clone());
+                Ok(vi)
+            }
+        }
     }
 }
 
@@ -107,9 +163,9 @@ fn handle_not_found() -> Result<Response<Body>, GraphQLServerError> {
         .unwrap())
 }
 
-fn as_http_response(value: q::Value) -> http::Response<Body> {
+fn as_http_response(value: &q::Value) -> http::Response<Body> {
     let status_code = http::StatusCode::OK;
-    let json = serde_json::to_string(&SerializableValue(&value))
+    let json = serde_json::to_string(&SerializableValue(value))
         .expect("Failed to serialize response to JSON");
     http::Response::builder()
         .status(status_code)
@@ -119,4 +175,48 @@ fn as_http_response(value: q::Value) -> http::Response<Body> {
         .header("Content-Type", "application/json")
         .body(Body::from(json))
         .unwrap()
+}
+
+/// Caching of values for a specified amount of time
+#[derive(Debug)]
+struct CacheEntry<T> {
+    value: Arc<T>,
+    expires: Instant,
+}
+
+#[derive(Debug)]
+struct Cache<T> {
+    ttl: Duration,
+    entries: RwLock<HashMap<String, CacheEntry<T>>>,
+}
+
+impl<T> Cache<T> {
+    fn new() -> Self {
+        Self {
+            ttl: *TTL,
+            entries: RwLock::new(HashMap::new()),
+        }
+    }
+
+    fn get(&self, key: &str) -> Option<Arc<T>> {
+        match self.entries.read().unwrap().get(key) {
+            Some(CacheEntry { value, expires }) => {
+                let now = Instant::now();
+                if *expires < now {
+                    Some(value.clone())
+                } else {
+                    None
+                }
+            }
+            None => None,
+        }
+    }
+
+    fn set(&self, key: String, value: Arc<T>) {
+        let entry = CacheEntry {
+            value,
+            expires: Instant::now() + self.ttl,
+        };
+        self.entries.write().unwrap().insert(key, entry);
+    }
 }
