949abcc124aa17c08065abb178bef3f1c19a106d
David Lutterkort <lutter@watzmann.net>
2021-01-15 11:26:48 -0800

    store: Fix typo in comment for `drop_deployment`

--- store/postgres/src/entities.rs
+++ store/postgres/src/entities.rs
@@ -384,7 +384,7 @@ impl Connection<'_> {
     /// Remove all data and metadata for a deployment. This is an associated
     /// method so that deployment removal can work with deployments that are
     /// incomplete or damaged, e.g., in a way where we can't get the schema
-    /// for hte subgraph
+    /// for the subgraph
     pub(crate) fn drop_deployment(conn: &PgConnection, site: &Site) -> Result<(), StoreError> {
         crate::deployment::drop_schema(conn, &site.namespace)?;
         Layout::drop_metadata(conn, &site.deployment)
