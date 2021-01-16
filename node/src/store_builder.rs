87914935798fb7d35cf240c28e25dd40a3a594e9
David Lutterkort <lutter@watzmann.net>
2021-01-15 18:19:48 -0800

    node, store: Move creating stores for each shard into SubgraphStore

--- node/src/store_builder.rs
+++ node/src/store_builder.rs
@@ -10,7 +10,7 @@ use graph_store_postgres::connection_pool::ConnectionPool;
 use graph_store_postgres::{
     BlockStore as DieselBlockStore, ChainHeadUpdateListener as PostgresChainHeadUpdateListener,
     ChainStore as DieselChainStore, NetworkStore as DieselNetworkStore, Shard as ShardName,
-    Store as DieselStore, SubgraphStore, SubscriptionManager, PRIMARY_SHARD,
+    SubgraphStore, SubscriptionManager, PRIMARY_SHARD,
 };
 
 use crate::config::{Config, Shard};
@@ -56,22 +56,31 @@ impl StoreBuilder {
         let shards: Vec<_> = config
             .stores
             .iter()
-            .map(|(name, shard)| Self::make_shard(logger, name, shard, registry.cheap_clone()))
+            .map(|(name, shard)| {
+                let logger = logger.new(o!("shard" => name.to_string()));
+                let conn_pool = Self::main_pool(&logger, name, shard, registry.cheap_clone());
+
+                let (read_only_conn_pools, weights) =
+                    Self::replica_pools(&logger, name, shard, registry.cheap_clone());
+
+                let name =
+                    ShardName::new(name.to_string()).expect("shard names have been validated");
+                (name, conn_pool, read_only_conn_pools, weights)
+            })
             .collect();
 
         let primary_pool = shards
             .iter()
-            .find(|(name, _, _)| name.as_str() == PRIMARY_SHARD.as_str())
+            .find(|(name, _, _, _)| name.as_str() == PRIMARY_SHARD.as_str())
             .unwrap()
-            .2
+            .1
             .clone();
 
-        let shard_map =
-            HashMap::from_iter(shards.into_iter().map(|(name, shard, _)| (name, shard)));
-
         let store = Arc::new(SubgraphStore::new(
-            shard_map,
+            logger,
+            shards,
             Arc::new(config.deployment.clone()),
+            registry.cheap_clone(),
         ));
 
         (store, primary_pool)
@@ -86,29 +95,6 @@ impl StoreBuilder {
         registry: Arc<dyn MetricsRegistry>,
     ) -> Arc<SubgraphStore> {
         Self::make_sharded_store_and_primary_pool(logger, config, registry).0
-    }
-
-    fn make_shard(
-        logger: &Logger,
-        name: &str,
-        shard: &Shard,
-        registry: Arc<dyn MetricsRegistry>,
-    ) -> (ShardName, Arc<DieselStore>, ConnectionPool) {
-        let logger = logger.new(o!("shard" => name.to_string()));
-        let conn_pool = Self::main_pool(&logger, name, shard, registry.cheap_clone());
-
-        let (read_only_conn_pools, weights) =
-            Self::replica_pools(&logger, name, shard, registry.cheap_clone());
-
-        let shard = Arc::new(DieselStore::new(
-            &logger,
-            conn_pool.clone(),
-            read_only_conn_pools.clone(),
-            weights,
-            registry.cheap_clone(),
-        ));
-        let name = ShardName::new(name.to_string()).expect("shard names have been validated");
-        (name, shard, conn_pool)
     }
 
     /// Create a connection pool for the main database of hte primary shard
