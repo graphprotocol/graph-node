081877de0d5f652a52063ec77c44a3004d60dfda
David Lutterkort <lutter@watzmann.net>
2021-02-25 15:33:45 -0800

    store: Remove block_range and id from dynamic data source table

--- store/postgres/src/dynds.rs
+++ store/postgres/src/dynds.rs
@@ -1,13 +1,11 @@
 //! SQL queries to load dynamic data sources
 
-use std::ops::Bound;
-
 use diesel::{
     delete,
     dsl::sql,
     prelude::{ExpressionMethods, QueryDsl, RunQueryDsl},
     sql_query,
-    sql_types::{Array, Text},
+    sql_types::Text,
 };
 use diesel::{insert_into, pg::PgConnection};
 
@@ -16,13 +14,12 @@ use graph::{
     constraint_violation,
     data::subgraph::Source,
     prelude::{
-        bigdecimal::ToPrimitive, web3::types::H160, BlockNumber, EthereumBlockPointer, StoreError,
-        SubgraphDeploymentId,
+        bigdecimal::ToPrimitive, web3::types::H160, BigDecimal, BlockNumber, EthereumBlockPointer,
+        StoreError, SubgraphDeploymentId,
     },
 };
-use uuid::Uuid;
 
-use crate::block_range::{first_block_in_range, BlockRange, BLOCK_UNVERSIONED};
+use crate::block_range::BLOCK_UNVERSIONED;
 
 // Diesel tables for some of the metadata
 // See also: ed42d219c6704a4aab57ce1ea66698e7
@@ -34,7 +31,6 @@ use crate::block_range::{first_block_in_range, BlockRange, BLOCK_UNVERSIONED};
 table! {
     subgraphs.dynamic_ethereum_contract_data_source (vid) {
         vid -> BigInt,
-        id -> Text,
         name -> Text,
         address -> Binary,
         abi -> Text,
@@ -44,14 +40,13 @@ table! {
         ethereum_block_number -> Numeric,
         deployment -> Text,
         context -> Nullable<Text>,
-        block_range -> Range<Integer>,
     }
 }
 // END GENERATED CODE
 
 fn to_source(
     deployment: &str,
-    ds_id: &str,
+    vid: i64,
     address: Vec<u8>,
     abi: String,
     start_block: BlockNumber,
@@ -59,7 +54,7 @@ fn to_source(
     if address.len() != 20 {
         return Err(constraint_violation!(
             "Data source address 0x`{:?}` for dynamic data source {} in deployment {} should have be 20 bytes long but is {} bytes long",
-            address, ds_id, deployment,
+            address, vid, deployment,
             address.len()
         ));
     }
@@ -70,7 +65,7 @@ fn to_source(
         constraint_violation!(
             "Start block {:?} for dynamic data source {} in deployment {} is not a u64",
             start_block,
-            ds_id,
+            vid,
             deployment
         )
     })?;
@@ -91,34 +86,34 @@ pub fn load(conn: &PgConnection, id: &str) -> Result<Vec<StoredDynamicDataSource
     let dds: Vec<_> = decds::table
         .filter(decds::deployment.eq(id))
         .select((
-            decds::id,
+            decds::vid,
             decds::name,
             decds::context,
             decds::address,
             decds::abi,
             decds::start_block,
-            decds::block_range,
+            decds::ethereum_block_number,
         ))
         .order_by((decds::ethereum_block_number, decds::vid))
         .load::<(
-            String,
+            i64,
             String,
             Option<String>,
             Vec<u8>,
             String,
             BlockNumber,
-            (Bound<i32>, Bound<i32>),
+            BigDecimal,
         )>(conn)?;
 
     let mut data_sources: Vec<StoredDynamicDataSource> = Vec::new();
-    for (ds_id, name, context, address, abi, start_block, range) in dds.into_iter() {
-        let source = to_source(id, &ds_id, address, abi, start_block)?;
-        let creation_block = first_block_in_range(&range);
+    for (vid, name, context, address, abi, start_block, creation_block) in dds.into_iter() {
+        let source = to_source(id, vid, address, abi, start_block)?;
+        let creation_block = creation_block.to_u64();
         let data_source = StoredDynamicDataSource {
             name,
             source,
             context,
-            creation_block: creation_block.map(|n| n as u64),
+            creation_block,
         };
 
         if !(data_sources.last().and_then(|d| d.creation_block) <= data_source.creation_block) {
@@ -130,10 +125,6 @@ pub fn load(conn: &PgConnection, id: &str) -> Result<Vec<StoredDynamicDataSource
         data_sources.push(data_source);
     }
     Ok(data_sources)
-}
-
-pub(crate) fn make_id() -> String {
-    format!("{}-dynamic", Uuid::new_v4().to_simple())
 }
 
 pub(crate) fn insert(
@@ -168,10 +159,8 @@ pub(crate) fn insert(
                     ));
                 }
             };
-            let range = block_ptr.number as i32..;
             Ok((
                 decds::deployment.eq(deployment.as_str()),
-                decds::id.eq(make_id()),
                 decds::name.eq(name),
                 decds::context.eq(context),
                 decds::address.eq(address),
@@ -179,7 +168,6 @@ pub(crate) fn insert(
                 decds::start_block.eq(start_block as i32),
                 decds::ethereum_block_number.eq(sql(&format!("{}::numeric", block_ptr.number))),
                 decds::ethereum_block_hash.eq(block_ptr.hash.as_bytes()),
-                decds::block_range.eq(BlockRange::from(range).as_pair()),
             ))
         })
         .collect::<Result<_, _>>()?;
@@ -196,27 +184,17 @@ pub(crate) fn copy(
     dst: &SubgraphDeploymentId,
 ) -> Result<usize, StoreError> {
     const QUERY: &str = "\
-      insert into subgraphs.dynamic_ethereum_contract_data_source(id, name,
+      insert into subgraphs.dynamic_ethereum_contract_data_source(name,
              address, abi, start_block, ethereum_block_hash,
-             ethereum_block_number, deployment, context, block_range)
-      select xlat.new_id, e.name, e.address, e.abi, e.start_block,
-             e.ethereum_block_hash, e.ethereum_block_number, $3 as deployment,
-             e.context, e.block_range
-        from subgraphs.dynamic_ethereum_contract_data_source e,
-             unnest($1::text[], $2::text[]) as xlat(id, new_id)
-       where xlat.id = e.id";
-    use dynamic_ethereum_contract_data_source as decds;
-
-    let dds = decds::table
-        .select(decds::id)
-        .filter(decds::deployment.eq(src.as_str()))
-        .load::<String>(conn)?;
-    // Create an equal number of brand new ids
-    let new_dds = (0..dds.len()).map(|_| make_id()).collect::<Vec<_>>();
+             ethereum_block_number, deployment, context)
+      select e.name, e.address, e.abi, e.start_block,
+             e.ethereum_block_hash, e.ethereum_block_number, $2 as deployment,
+             e.context
+        from subgraphs.dynamic_ethereum_contract_data_source e
+       where e.deployment = $1";
 
     Ok(sql_query(QUERY)
-        .bind::<Array<Text>, _>(dds)
-        .bind::<Array<Text>, _>(new_dds)
+        .bind::<Text, _>(src.as_str())
         .bind::<Text, _>(dst.as_str())
         .execute(conn)?)
 }
@@ -226,14 +204,14 @@ pub(crate) fn revert(
     id: &SubgraphDeploymentId,
     block: BlockNumber,
 ) -> Result<(), StoreError> {
-    use crate::functions::lower;
     use dynamic_ethereum_contract_data_source as decds;
 
     let dds = decds::table.filter(decds::deployment.eq(id.as_str()));
     if block == BLOCK_UNVERSIONED {
         delete(dds).execute(conn)?;
     } else {
-        delete(dds.filter(lower(decds::block_range).ge(block))).execute(conn)?;
+        delete(dds.filter(decds::ethereum_block_number.ge(sql(&block.to_string()))))
+            .execute(conn)?;
     }
     Ok(())
 }
