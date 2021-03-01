42efa396e96de1d5e3a9c3d397ccce74df1effdc
David Lutterkort <lutter@watzmann.net>
2021-03-01 15:02:51 -0800

    store: Remove metadata printers from layout helper
    Some of the printers in the layout helper were only there to generate
    queries for metadata. That's not necessary anymore, and the metadata schema
    is no longer compatible with how we generate DDL from GraphQL schemas

--- store/postgres/examples/layout.rs
+++ store/postgres/examples/layout.rs
@@ -24,85 +24,6 @@ pub fn ensure<T, E: std::fmt::Display>(res: Result<T, E>, msg: &str) -> T {
     }
 }
 
-fn print_migration(layout: &Layout) {
-    for table in layout.tables.values() {
-        print!("insert into\n  {}(", table.qualified_name);
-        for column in &table.columns {
-            print!("{}, ", column.name.as_str());
-        }
-        print!("block_range)\nselect ");
-        for (i, column) in table.columns.iter().enumerate() {
-            if i > 0 {
-                print!("       ");
-            }
-            if column.is_list() {
-                print!(
-                    "array(select (x->>'data')::{} from jsonb_array_elements(data->'{}'->'data') x)",
-                    column.column_type.sql_type(),
-                    column.field
-                );
-            } else {
-                if column.name.as_str() == "id" {
-                    print!("id");
-                } else if column.column_type == ColumnType::Bytes {
-                    print!(
-                        "decode(replace(data->'{}'->>'data','0x',''),'hex')",
-                        column.field
-                    );
-                } else {
-                    print!(
-                        "(data->'{}'->>'data')::{}",
-                        column.field,
-                        column.column_type.sql_type()
-                    );
-                }
-            }
-            println!(",");
-        }
-        println!("      block_range");
-        println!("  from history_data");
-        println!(" where entity = '{}';\n", table.object);
-    }
-}
-
-fn print_views(layout: &Layout) {
-    for table in layout.tables.values() {
-        println!("\ncreate view {} as", table.qualified_name);
-        print!("select ");
-        for (i, column) in table.columns.iter().enumerate() {
-            if i > 0 {
-                print!(",\n       ");
-            }
-            if column.is_list() {
-                print!(
-                    "array(select (x->>'data')::{} from jsonb_array_elements(data->'{}'->'data') x)",
-                    column.column_type.sql_type(),
-                    column.field
-                );
-            } else {
-                if column.name.as_str() == "id" {
-                    print!("id");
-                } else {
-                    print!(
-                        "(data->'{}'->>'data')::{}",
-                        column.field,
-                        column.column_type.sql_type()
-                    );
-                }
-            }
-            print!(" as {}", column.name.as_str());
-        }
-        println!("\n  from {}.entities", layout.catalog.namespace);
-        println!(" where entity = '{}';", table.object);
-    }
-}
-
-fn print_drop_views(layout: &Layout) {
-    for table in layout.tables.values() {
-        println!("drop view if exists {};", table.qualified_name);
-    }
-}
-
 fn print_drop(layout: &Layout) {
     for table in layout.tables.values() {
         println!("drop table {};", table.qualified_name);
@@ -118,78 +39,6 @@ fn print_delete_all(layout: &Layout) {
 fn print_ddl(layout: &Layout) {
     let ddl = ensure(layout.as_ddl(), "Failed to generate DDL");
     println!("{}", ddl);
-}
-
-/// Generate a SQL statement that copies dynamic data sources and modifies
-/// ids of the data sources and all related objects as it copies the data.
-/// The query will expect three bind variables:
-///   $1 : text[] - the ids of the dynamic data sources to copy
-///   $2 : text[] - the new ids for each of the data sources in $1
-///   $3 : text   - the id of the deployment to which the copies will belong
-fn print_copy_dds(layout: &Layout) {
-    // Generate a SQL snippet that translates the values in `column` into
-    // the new ids using the xlat view we set up earlier in the query
-    fn xlat(column: &str, is_list: bool) -> String {
-        if is_list {
-            // A poor man's map: turn the array into a table, transform
-            // the array element and turn everything back into an array
-            format!(
-                "(select array_agg(x.new_id || right(a.elt, -40)) from unnest(e.{}) a(elt)) as {}",
-                column, column
-            )
-        } else {
-            format!("(x.new_id || right(e.{}, -40)) as {}", column, column)
-        }
-    }
-
-    // The tables relevant for dynamic metadata
-    let mut tables = layout
-        .tables
-        .values()
-        .filter(|table| {
-            !table.name.as_str().starts_with("subgraph")
-                && table.name.as_str() != "dynamic_ethereum_contract_data_source"
-        })
-        .collect::<Vec<_>>();
-    tables.sort_by_key(|table| table.name.as_str());
-
-    // xlat translates old id's (passed in $1) into new id's (passed as $2)
-    println!("with xlat as (");
-    print!("  select * from unnest($1::text[], $2::text[]) as xlat(id, new_id))");
-    for (i, table) in tables.iter().enumerate() {
-        println!(",");
-        println!(" md{} as (", i);
-        print!("    insert into subgraphs.{}(", table.name);
-        for column in table.columns.iter() {
-            print!("{}, ", column.name);
-        }
-        println!("block_range)");
-        print!("    select ");
-        for column in table.columns.iter() {
-            if column.is_primary_key() || column.is_reference() {
-                print!("{}, ", xlat(column.name.as_str(), column.is_list()));
-            } else {
-                print!("{}, ", column.name);
-            }
-        }
-        println!("block_range");
-        print!(
-            "      from subgraphs.{} e, xlat x\n     where left(e.id, 40) = x.id)",
-            table.name
-        );
-    }
-    println!(
-        r#"
-insert into subgraphs.dynamic_ethereum_contract_data_source(id, kind, name,
-              network, address, abi, start_block, ethereum_block_hash,
-              ethereum_block_number, deployment, context, block_range)
-select x.new_id, e.kind, e.name, e.network,
-       e.address, e.abi, e.start_block,
-       e.ethereum_block_hash, e.ethereum_block_number, $3 as deployment,
-       e.context, e.block_range
-  from xlat x, subgraphs.dynamic_ethereum_contract_data_source e
- where x.id = e.id"#
-    );
 }
 
 fn print_diesel_tables(layout: &Layout) {
@@ -268,37 +117,6 @@ fn print_diesel_tables(layout: &Layout) {
     }
 }
 
-fn print_list_metadata(layout: &Layout) {
-    let mut tables = layout
-        .tables
-        .values()
-        .filter(|table| {
-            table.name.as_str() != "subgraph" && table.name.as_str() != "subgraph_version"
-        })
-        .collect::<Vec<_>>();
-    tables.sort_by_key(|table| table.name.as_str());
-
-    println!("with dds as (");
-    println!("  select *");
-    println!("    from subgraphs.dynamic_ethereum_contract_data_source");
-    println!("   where deployment = $1)");
-    for (i, table) in tables.iter().enumerate() {
-        if i > 0 {
-            println!(" union all");
-        }
-        println!("select '{}' as entity, id", &table.object);
-        let name = table.qualified_name.as_str().replace("\"", "");
-        println!("  from {} e", name);
-        if table.object.as_str() == "DynamicEthereumContractDataSource" {
-            println!(" where e.deployment = $1");
-        } else {
-            println!(" where left(e.id, 46) = $1");
-            println!("    or left(e.id, 40) in (select id from dds)");
-        }
-    }
-    println!(" order by entity, id");
-}
-
 pub fn main() {
     let args = App::new("layout")
     .version("1.0")
@@ -330,19 +148,10 @@ pub fn main() {
         "Failed to construct Mapping",
     );
     match kind {
-        "migrate" => {
-            print_drop_views(&layout);
-            print_ddl(&layout);
-            print_migration(&layout);
-        }
         "drop" => print_drop(&layout),
         "delete" => print_delete_all(&layout),
         "ddl" => print_ddl(&layout),
-        "views" => print_views(&layout),
-        "drop-views" => print_drop_views(&layout),
         "diesel" => print_diesel_tables(&layout),
-        "list-metadata" => print_list_metadata(&layout),
-        "copy-dds" => print_copy_dds(&layout),
         _ => {
             usage(&format!("illegal value {} for --generate", kind));
         }
