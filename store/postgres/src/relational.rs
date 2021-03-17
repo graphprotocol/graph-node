642f8f98622ed10a55640c0c9b9ddcc5a66453c1
tilacog <tilacog@gmail.com>
2021-03-17 20:52:58 -0300

    store: get rid of unused parameter

--- store/postgres/src/relational.rs
+++ store/postgres/src/relational.rs
@@ -565,7 +565,7 @@ impl Layout {
         block: BlockNumber,
     ) -> Result<usize, StoreError> {
         let table = self.table_for_entity(entity_type)?;
-        Ok(InsertQuery::new(table, &entity_type, entities, block)?
+        Ok(InsertQuery::new(table, entities, block)?
             .get_results(conn)
             .map(|ids| ids.len())?)
     }
@@ -671,7 +671,7 @@ impl Layout {
         let table = self.table_for_entity(&entity_type)?;
         let entity_keys: Vec<&EntityKey> = entities.iter().map(|(key, _)| key).collect();
         ClampRangeQuery::new(table, &entity_type, &entity_keys, block).execute(conn)?;
-        Ok(InsertQuery::new(table, entity_type, entities, block)?
+        Ok(InsertQuery::new(table, entities, block)?
             .get_results(conn)
             .map(|ids| ids.len())?)
     }
