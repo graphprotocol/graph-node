90b1c6ea1f9b58bf779fbcb4544b3985a7e43df1
David Lutterkort <lutter@watzmann.net>
2021-04-28 17:59:14 -0700

    mock: Remove unused MockBlockStream/Builder

--- mock/src/lib.rs
+++ mock/src/lib.rs
@@ -4,11 +4,8 @@ extern crate graph_graphql;
 extern crate graphql_parser;
 extern crate rand;
 
-mod block_stream;
-
 mod metrics_registry;
 mod store;
 
-pub use self::block_stream::{MockBlockStream, MockBlockStreamBuilder};
 pub use self::metrics_registry::MockMetricsRegistry;
 pub use self::store::MockStore;
