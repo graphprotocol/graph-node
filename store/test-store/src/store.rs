884550a5b20f772104d2df742846e1d44c1479f1
David Lutterkort <lutter@watzmann.net>
2021-06-02 15:50:16 -0700

    store: Separate running tests sequentially from accessing the runtime

--- store/test-store/src/store.rs
+++ store/test-store/src/store.rs
@@ -39,13 +39,12 @@ lazy_static! {
         Some(_) => log::logger(false),
         None => Logger::root(slog::Discard, o!()),
     };
-    pub static ref STORE_RUNTIME: Mutex<Runtime> = Mutex::new(
-        Builder::new()
-            .basic_scheduler()
-            .enable_all()
-            .build()
-            .unwrap()
-    );
+    static ref SEQ_LOCK: Mutex<()> = Mutex::new(());
+    static ref STORE_RUNTIME: Runtime = Builder::new()
+        .threaded_scheduler()
+        .enable_all()
+        .build()
+        .unwrap();
     pub static ref LOAD_MANAGER: Arc<LoadManager> = Arc::new(LoadManager::new(
         &*LOGGER,
         Vec::new(),
@@ -104,16 +103,15 @@ where
     F: FnOnce(Arc<Store>, S) -> R + Send + 'static,
     R: std::future::Future<Output = ()> + Send + 'static,
 {
-    let store = STORE.clone();
-
     // Lock regardless of poisoning. This also forces sequential test execution.
-    let mut runtime = match STORE_RUNTIME.lock() {
+    let _lock = match SEQ_LOCK.lock() {
         Ok(guard) => guard,
         Err(err) => err.into_inner(),
     };
 
-    runtime.block_on(async {
+    STORE_RUNTIME.handle().block_on(async {
         let state = setup();
+        let store = STORE.clone();
         test(store, state).await
     })
 }
@@ -123,14 +121,15 @@ pub fn run_test_with_conn<F>(test: F)
 where
     F: FnOnce(&PgConnection) -> (),
 {
-    let conn = PRIMARY_POOL
-        .get()
-        .expect("failed to get connection for primary database");
-
-    let _runtime = match STORE_RUNTIME.lock() {
+    // Lock regardless of poisoning. This also forces sequential test execution.
+    let _lock = match SEQ_LOCK.lock() {
         Ok(guard) => guard,
         Err(err) => err.into_inner(),
     };
+
+    let conn = PRIMARY_POOL
+        .get()
+        .expect("failed to get connection for primary database");
 
     test(&conn);
 }
@@ -471,7 +470,7 @@ fn build_store() -> (Arc<Store>, ConnectionPool, Config, Arc<SubscriptionManager
     let config = Config::load(&*LOGGER, &opt).expect("config is not valid");
     let registry = Arc::new(MockMetricsRegistry::new());
     std::thread::spawn(move || {
-        STORE_RUNTIME.lock().unwrap().block_on(async {
+        STORE_RUNTIME.handle().block_on(async {
             let builder = StoreBuilder::new(&*LOGGER, &*NODE_ID, &config, registry).await;
             let subscription_manager = builder.subscription_manager();
             let primary_pool = builder.primary_pool();
