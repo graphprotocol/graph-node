98a75063b89a9b449ff24dba86cc6f38398d22ab
Otávio Pace <otaviopp8@gmail.com>
2021-06-18 20:24:40 -0300

    integration-tests: Add one for older apiVersion

--- tests/tests/parallel_tests.rs
+++ tests/tests/parallel_tests.rs
@@ -24,7 +24,8 @@ lazy_static::lazy_static! {
 }
 
 /// All integration tests subdirectories to run
-pub const INTEGRATION_TESTS_DIRECTORIES: [&str; 9] = [
+pub const INTEGRATION_TESTS_DIRECTORIES: [&str; 10] = [
+    "api-version-v0-0-4",
     // "arweave-and-3box",
     "data-source-context",
     "data-source-revert",
