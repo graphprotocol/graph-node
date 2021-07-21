8f2342811045fa925e13c67e01c279ebb3d4fa23
tilacog <tilacog@gmail.com>
2021-07-20 21:02:56 -0300

    graph: [WIP] function outline

--- graph/src/data/subgraph/features.rs
+++ graph/src/data/subgraph/features.rs
@@ -1,4 +1,8 @@
-use crate::prelude::{Deserialize, Serialize};
+use crate::{
+    blockchain::Blockchain,
+    data::{schema::Schema, subgraph::SubgraphManifest},
+    prelude::{Deserialize, Serialize},
+};
 use std::{collections::BTreeSet, fmt, str::FromStr};
 
 #[derive(Debug, Deserialize, Serialize, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -26,5 +30,32 @@ impl FromStr for SubgraphFeature {
             .map_err(|_error| anyhow::anyhow!("Invalid subgraph feature: {}", value))
     }
 }
+// TODO: How can we access the mapping source code? (schema and manifest are ok)
+pub fn detect_features<C: Blockchain>(manifest: &SubgraphManifest<C>) -> BTreeSet<SubgraphFeature> {
+    vec![
+        detect_non_fatal_errors(&manifest),
+        detect_grafting(&manifest),
+        detect_full_text_search(&manifest.schema),
+        detect_ipfs_on_ethereum_contracts(&manifest),
+    ]
+    .into_iter()
+    .filter_map(|x| x)
+    .collect()
+}
 
+fn detect_non_fatal_errors<C: Blockchain>(
+    manifest: &SubgraphManifest<C>,
+) -> Option<SubgraphFeature> {
+    todo!()
+}
+fn detect_grafting<C: Blockchain>(manifest: &SubgraphManifest<C>) -> Option<SubgraphFeature> {
+    todo!()
+}
+fn detect_full_text_search(schema: &Schema) -> Option<SubgraphFeature> {
+    todo!()
+}
+fn detect_ipfs_on_ethereum_contracts<C: Blockchain>(
+    manifest: &SubgraphManifest<C>,
+) -> Option<SubgraphFeature> {
+    todo!()
 }
