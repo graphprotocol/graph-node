a98e700dc4c9dd7c83c6c64ce8a3d53df3f86e15
tilacog <tilacog@gmail.com>
2021-08-03 10:38:12 -0300

    server: Refactor resolve_subgrahp_features function

--- server/index-node/src/resolver.rs
+++ server/index-node/src/resolver.rs
@@ -203,20 +203,25 @@ where
             // The response object will have either:
             // - a list of features for the "feature" key and a null value for the "error" key; OR
             // -. a an empty list for the "feature" key and a string for the "error" key.
-            let mut detected_features = Vec::new();
-            let mut error: q::Value = q::Value::Null;
-
-            match validate_subgraph_features(&subgraph_manifest) {
-                Ok(features) => features
-                    .iter()
-                    .map(ToString::to_string)
-                    .map(q::Value::String)
-                    .for_each(|feature| detected_features.push(feature)),
-                Err(validation_error) => error = q::Value::String(validation_error.to_string()),
-            }
+            let (detected_features, error) = match validate_subgraph_features(&subgraph_manifest) {
+                Ok(features) => (
+                    q::Value::List(
+                        features
+                            .iter()
+                            .map(ToString::to_string)
+                            .map(q::Value::String)
+                            .collect(),
+                    ),
+                    q::Value::Null,
+                ),
+                Err(validation_error) => (
+                    q::Value::List(vec![]),
+                    q::Value::String(validation_error.to_string()),
+                ),
+            };
 
             let mut response: BTreeMap<String, q::Value> = BTreeMap::new();
-            response.insert("features".to_string(), q::Value::List(detected_features));
+            response.insert("features".to_string(), detected_features);
             response.insert("errors".to_string(), error);
             response
         };
