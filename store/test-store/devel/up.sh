ffd6bddc4b11bcce55a76e276f162ba8aa40902b
Matthieu Vachon <matthieu.vachon@dfuse.io>
2021-08-18 17:52:23 -0400

    doc: added further instructions to more easily run the integration tests

---
+++ store/test-store/devel/up.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+set -e
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+keep=
+
+main() {
+  pushd "$ROOT" &> /dev/null
+
+  while getopts "hk" opt; do
+    case $opt in
+      h) usage && exit 0;;
+      k) keep=true;;
+      \?) usage_error "Invalid option: -$OPTARG";;
+    esac
+  done
+  shift $((OPTIND-1))
+
+  if [[ -d "./data" && $keep == "" ]]; then
+    echo "Cleaning data directory"
+    rm -rf ./data 1> /dev/null
+  fi
+
+  prepare
+
+  # Pass execution to docker compose
+  exec docker-compose up
+}
+
+prepare() {
+  if [[ ! -d "./data/ipfs" ]]; then
+    mkdir -p ./data/ipfs 1> /dev/null
+  fi
+
+  if [[ ! -d "./data/postgres" ]]; then
+    mkdir -p ./data/postgres 1> /dev/null
+  fi
+}
+
+usage_error() {
+  message="$1"
+  exit_code="$2"
+
+  echo "ERROR: $message"
+  echo ""
+  usage
+  exit ${exit_code:-1}
+}
+
+usage() {
+  echo "usage: up [-c]"
+  echo ""
+  echo "Setup required files layout and launch 'docker compose up'"
+  echo "spinning up all required development dependencies."
+  echo ""
+  echo "Options"
+  echo "    -k          Clean 'data' directory before launching dependencies"
+  echo "    -h          Display help about this script"
+}
+
+main "$@"
