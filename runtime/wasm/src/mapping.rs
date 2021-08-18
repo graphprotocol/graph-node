7319d7f4e069f321cf9e906748c6fbe17f6c80a1
Otávio Pace <otaviopp8@gmail.com>
2021-08-18 11:36:25 -0300

    runtime: Add env var for maximum stack size

--- runtime/wasm/src/mapping.rs
+++ runtime/wasm/src/mapping.rs
@@ -8,9 +8,18 @@ use std::collections::BTreeMap;
 use std::sync::Arc;
 use std::thread;
 
+const ONE_MIB: usize = 1 << 20; // 1_048_576
+
 lazy_static! {
     /// Verbose logging of mapping inputs
     pub static ref LOG_TRIGGER_DATA: bool = std::env::var("GRAPH_LOG_TRIGGER_DATA").is_ok();
+
+    /// Maximum stack size for the WASM runtime
+    pub static ref MAX_STACK_SIZE: usize = std::env::var("GRAPH_RUNTIME_MAX_STACK_SIZE")
+        .ok()
+        .and_then(|max_stack_size| max_stack_size.parse().ok())
+        // 512KiB
+        .unwrap_or(ONE_MIB / 2);
 }
 
 /// Spawn a wasm module in its own thread.
@@ -140,6 +149,8 @@ impl ValidModule {
         config.interruptable(true); // For timeouts.
         config.cranelift_nan_canonicalization(true); // For NaN determinism.
         config.cranelift_opt_level(wasmtime::OptLevel::None);
+        config.max_wasm_stack(*MAX_STACK_SIZE).unwrap(); // Safe because this only panics if size passed is 0.
+
         let engine = &wasmtime::Engine::new(&config)?;
         let module = wasmtime::Module::from_binary(&engine, raw_module)?;
 
