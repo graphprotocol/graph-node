58c7bd3f7c495d4b43840678ece45534007d29f0
tilacog <tilacog@gmail.com>
2021-08-24 18:15:56 -0300

    store: Use push_sql instead of push_identifier

--- store/postgres/src/transaction_receipt.rs
+++ store/postgres/src/transaction_receipt.rs
@@ -78,7 +78,7 @@ from (
     select jsonb_array_elements(data -> 'transaction_receipts') as receipt
     from"#,
         );
-        out.push_identifier(&self.blocks_table_name)?;
+        out.push_sql(&self.blocks_table_name);
         out.push_sql(" where hash = ");
         out.push_bind_param::<Binary, _>(&self.block_hash)?;
         out.push_sql(") as temp;");
