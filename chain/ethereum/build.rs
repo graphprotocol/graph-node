8f103b887ccacd5107a6289352abecd2cac77db8
Leonardo Yvens <leoyvens@gmail.com>
2021-09-22 17:03:05 -0300

    build: Fix issues post-firehose

--- chain/ethereum/build.rs
+++ chain/ethereum/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     tonic_build::configure()
         .out_dir("src/protobuf")
-        .format(true)
+        .format(cfg!(debug_assertions)) // Release build environments might not have rustfmt installed
         .compile(&["proto/codec.proto"], &["proto"])
         .expect("Failed to compile StreamingFast Ethereum proto(s)");
 }
