558b2b487b794a6b860157c4d0a2ac37651830c8
David Lutterkort <lutter@watzmann.net>
2021-10-14 17:52:26 -0700

    mock: Simplify dependencies

--- mock/src/lib.rs
+++ mock/src/lib.rs
@@ -1,9 +1,3 @@
-extern crate futures;
-extern crate graph;
-extern crate graph_graphql;
-extern crate graphql_parser;
-extern crate rand;
-
 mod metrics_registry;
 
 pub use self::metrics_registry::MockMetricsRegistry;
