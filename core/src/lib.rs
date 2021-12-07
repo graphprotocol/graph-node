ad9fccaddea8b64db28dfe094fd4230573b9a26c
georg-getz <georgi.getz@outlook.com>
2021-12-07 02:51:00 +0200

    switched the approach a bit

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,7 +1,8 @@
 mod link_resolver;
 mod metrics;
-pub mod subgraph;
+mod subgraph;
 
 pub use crate::link_resolver::LinkResolver;
 pub use crate::metrics::MetricsRegistry;
 pub use crate::subgraph::{SubgraphAssignmentProvider, SubgraphInstanceManager, SubgraphRegistrar};
+pub use crate::subgraph::load_dynamic_data_sources;
--- core/src/subgraph/mod.rs
+++ core/src/subgraph/mod.rs
@@ -8,3 +8,4 @@ pub use self::instance::SubgraphInstance;
 pub use self::instance_manager::SubgraphInstanceManager;
 pub use self::provider::SubgraphAssignmentProvider;
 pub use self::registrar::SubgraphRegistrar;
+pub use self::loader::load_dynamic_data_sources;
