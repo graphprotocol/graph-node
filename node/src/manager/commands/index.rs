2cb360aaa9a0bb910557eca86e16a75bea92f402
tilacog <tilacog@gmail.com>
2022-01-21 19:40:45 -0300

    node: Better error messages when validating fields

--- node/src/manager/commands/index.rs
+++ node/src/manager/commands/index.rs
@@ -1,9 +1,6 @@
 use graph::{
     components::store::EntityType,
-    prelude::{
-        anyhow::{self, Context},
-        DeploymentHash, StoreError,
-    },
+    prelude::{anyhow, DeploymentHash, StoreError},
 };
 use graph_store_postgres::SubgraphStore;
 use std::{collections::HashSet, sync::Arc};
@@ -11,12 +8,12 @@ use std::{collections::HashSet, sync::Arc};
 fn validate_fields<T: AsRef<str>>(fields: &[T]) -> Result<(), anyhow::Error> {
     // Must be non-empty. Double checking, since [`StructOpt`] already checks this.
     if fields.is_empty() {
-        anyhow::bail!("ate least one field must be informed")
+        anyhow::bail!("at least one field must be informed")
     }
     // All values must be unique
     let unique: HashSet<_> = fields.iter().map(AsRef::as_ref).collect();
     if fields.len() != unique.len() {
-        anyhow::bail!("fields must be unique")
+        anyhow::bail!("entity fields must be unique")
     }
     Ok(())
 }
@@ -27,7 +24,7 @@ pub async fn create(
     field_names: Vec<String>,
     index_method: String,
 ) -> Result<(), anyhow::Error> {
-    validate_fields(&field_names).context("Failed to validate fields")?;
+    validate_fields(&field_names)?;
     let deployment_hash = DeploymentHash::new(id)
         .map_err(|e| anyhow::anyhow!("Subgraph hash must be a valid IPFS hash: {}", e))?;
     let entity_type = EntityType::new(entity_name);
