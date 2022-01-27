f97317a0744cd1aa11ae2888598aab3c6bce8961
Otávio Pace <otaviopp8@gmail.com>
2022-01-27 16:59:33 -0300

    graphman: Improve --help of `run` command

--- node/src/bin/manager.rs
+++ node/src/bin/manager.rs
@@ -147,7 +147,9 @@ pub enum Command {
         /// The deployments to rewind
         names: Vec<String>,
     },
-    /// Deploy and run an arbitrary subgraph, up to a certain block (for dev and testing purposes) -- WARNING: WILL RUN MIGRATIONS ON THE DB, DO NOT USE IN PRODUCTION
+    /// Deploy and run an arbitrary subgraph up to a certain block, although it can surpass it by a few blocks, it's not exact (use for dev and testing purposes) -- WARNING: WILL RUN MIGRATIONS ON THE DB, DO NOT USE IN PRODUCTION
+    ///
+    /// Also worth noting that the deployed subgraph will be removed at the end.
     Run {
         /// Network name (must fit one of the chain)
         network_name: String,
