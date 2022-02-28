574cf5531ff39dc7d17adc2e02cbf923a3c0f984
Filipe Azevedo <filipe@azevedo.io>
2022-02-28 14:51:54 +0000

    add firehose transform proto (#3298)

--- graph/build.rs
+++ graph/build.rs
@@ -3,6 +3,9 @@ fn main() {
     tonic_build::configure()
         .out_dir("src/firehose")
         .format(true)
-        .compile(&["proto/firehose.proto"], &["proto"])
+        .compile(
+            &["proto/firehose.proto", "proto/transforms.proto"],
+            &["proto"],
+        )
         .expect("Failed to compile Firehose proto(s)");
 }
