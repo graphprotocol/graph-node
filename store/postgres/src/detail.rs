4f3017b7f1422343bbdbdaee2d0ab62719ec977e
Filippo Costa <filippocosta.italy@gmail.com>
2022-03-11 14:56:42 +0100

    store(style): Place ErrorDetail trait impl. near struct def.

--- store/postgres/src/detail.rs
+++ store/postgres/src/detail.rs
@@ -101,38 +101,6 @@ impl ErrorDetail {
     }
 }
 
-pub(crate) fn block(
-    id: &str,
-    name: &str,
-    hash: Option<Vec<u8>>,
-    number: Option<BigDecimal>,
-) -> Result<Option<status::EthereumBlock>, StoreError> {
-    match (&hash, &number) {
-        (Some(hash), Some(number)) => {
-            let hash = H256::from_slice(hash.as_slice());
-            let number = number.to_u64().ok_or_else(|| {
-                constraint_violation!(
-                    "the block number {} for {} in {} is not representable as a u64",
-                    number,
-                    name,
-                    id
-                )
-            })?;
-            Ok(Some(status::EthereumBlock::new(hash, number)))
-        }
-        (None, None) => Ok(None),
-        _ => Err(constraint_violation!(
-            "the hash and number \
-        of a block pointer must either both be null or both have a \
-        value, but for `{}` the hash of {} is `{:?}` and the number is `{:?}`",
-            id,
-            name,
-            hash,
-            number
-        )),
-    }
-}
-
 impl TryFrom<ErrorDetail> for SubgraphError {
     type Error = StoreError;
 
@@ -167,6 +135,38 @@ impl TryFrom<ErrorDetail> for SubgraphError {
             handler,
             deterministic,
         })
+    }
+}
+
+pub(crate) fn block(
+    id: &str,
+    name: &str,
+    hash: Option<Vec<u8>>,
+    number: Option<BigDecimal>,
+) -> Result<Option<status::EthereumBlock>, StoreError> {
+    match (&hash, &number) {
+        (Some(hash), Some(number)) => {
+            let hash = H256::from_slice(hash.as_slice());
+            let number = number.to_u64().ok_or_else(|| {
+                constraint_violation!(
+                    "the block number {} for {} in {} is not representable as a u64",
+                    number,
+                    name,
+                    id
+                )
+            })?;
+            Ok(Some(status::EthereumBlock::new(hash, number)))
+        }
+        (None, None) => Ok(None),
+        _ => Err(constraint_violation!(
+            "the hash and number \
+        of a block pointer must either both be null or both have a \
+        value, but for `{}` the hash of {} is `{:?}` and the number is `{:?}`",
+            id,
+            name,
+            hash,
+            number
+        )),
     }
 }
 
