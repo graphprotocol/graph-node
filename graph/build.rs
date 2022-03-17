e103fa2bb12f0c7f6d9443ddd13531434d11fd5e
Filipe Azevedo <filipe@azevedo.io>
2022-03-17 15:28:46 +0000

    Firehose NEAR filters

--- graph/build.rs
+++ graph/build.rs
@@ -4,7 +4,11 @@ fn main() {
         .out_dir("src/firehose")
         .format(true)
         .compile(
-            &["proto/firehose.proto", "proto/transforms.proto"],
+            &[
+                "proto/firehose.proto",
+                "proto/ethereum/transforms.proto",
+                "proto/near/transforms.proto",
+            ],
             &["proto"],
         )
         .expect("Failed to compile Firehose proto(s)");
