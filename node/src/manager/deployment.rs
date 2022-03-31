8b57f092ffe1ba1a66d358ad6f63ade44408ee8d
David Lutterkort <lutter@watzmann.net>
2022-03-31 11:10:53 -0700

    node: Facility for searching for deployments
    The current way of looking up deployments is pretty adhoc. The
    `DeploymentSearch` struct will make it possible to have the same mechanism
    across all commands.

--- node/src/manager/deployment.rs
+++ node/src/manager/deployment.rs
@@ -1,7 +1,10 @@
 use std::collections::HashSet;
+use std::fmt;
+use std::str::FromStr;
 
 use diesel::{dsl::sql, prelude::*};
 use diesel::{sql_types::Text, PgConnection};
+use regex::Regex;
 
 use graph::components::store::DeploymentId;
 use graph::{
@@ -9,7 +12,7 @@ use graph::{
     data::subgraph::status,
     prelude::{
         anyhow::{self, anyhow, bail},
-        DeploymentHash, Error, SubgraphStore as _,
+        lazy_static, DeploymentHash, Error, SubgraphStore as _,
     },
 };
 use graph_store_postgres::connection_pool::ConnectionPool;
@@ -17,6 +20,127 @@ use graph_store_postgres::{command_support::catalog as store_catalog, Shard, Sub
 
 use crate::manager::deployment;
 use crate::manager::display::List;
+
+lazy_static! {
+    // `Qm...` optionally follow by `:$shard`
+    static ref HASH_RE: Regex = Regex::new("\\A(?P<hash>Qm[^:]+)(:(?P<shard>[a-z0-9_]+))?\\z").unwrap();
+    // `sgdNNN`
+    static ref DEPLOYMENT_RE: Regex = Regex::new("\\A(?P<nsp>sgd[0-9]+)\\z").unwrap();
+}
+
+/// A search for one or multiple deployments to make it possible to search
+/// by subgraph name, IPFS hash, or namespace. Since there can be multiple
+/// deployments for the same IPFS hash, the search term for a hash can
+/// optionally specify a shard.
+#[derive(Clone, Debug)]
+pub enum DeploymentSearch {
+    Name { name: String },
+    Hash { hash: String, shard: Option<String> },
+    Deployment { namespace: String },
+}
+
+impl fmt::Display for DeploymentSearch {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DeploymentSearch::Name { name } => write!(f, "{}", name),
+            DeploymentSearch::Hash {
+                hash,
+                shard: Some(shard),
+            } => write!(f, "{}:{}", hash, shard),
+            DeploymentSearch::Hash { hash, shard: None } => write!(f, "{}", hash),
+            DeploymentSearch::Deployment { namespace } => write!(f, "{}", namespace),
+        }
+    }
+}
+
+impl FromStr for DeploymentSearch {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some(caps) = HASH_RE.captures(s) {
+            let hash = caps.name("hash").unwrap().as_str().to_string();
+            let shard = caps.name("shard").map(|shard| shard.as_str().to_string());
+            Ok(DeploymentSearch::Hash { hash, shard })
+        } else if let Some(caps) = DEPLOYMENT_RE.captures(s) {
+            let namespace = caps.name("nsp").unwrap().as_str().to_string();
+            Ok(DeploymentSearch::Deployment { namespace })
+        } else {
+            Ok(DeploymentSearch::Name {
+                name: s.to_string(),
+            })
+        }
+    }
+}
+
+impl DeploymentSearch {
+    pub fn lookup(&self, primary: &ConnectionPool) -> Result<Vec<Deployment>, anyhow::Error> {
+        let conn = primary.get()?;
+        self.lookup_with_conn(&conn)
+    }
+
+    pub fn lookup_with_conn(&self, conn: &PgConnection) -> Result<Vec<Deployment>, anyhow::Error> {
+        use store_catalog::deployment_schemas as ds;
+        use store_catalog::subgraph as s;
+        use store_catalog::subgraph_deployment_assignment as a;
+        use store_catalog::subgraph_version as v;
+
+        let query = ds::table
+            .inner_join(v::table.on(v::deployment.eq(ds::subgraph)))
+            .inner_join(s::table.on(v::subgraph.eq(s::id)))
+            .left_outer_join(a::table.on(a::id.eq(ds::id)))
+            .select((
+                s::name,
+                sql::<Text>(
+                    "(case
+                    when subgraphs.subgraph.pending_version = subgraphs.subgraph_version.id then 'pending'
+                    when subgraphs.subgraph.current_version = subgraphs.subgraph_version.id then 'current'
+                    else 'unused' end) status",
+                ),
+                v::deployment,
+                ds::name,
+                ds::id,
+                a::node_id.nullable(),
+                ds::shard,
+                ds::network,
+                ds::active,
+            ));
+
+        let deployments: Vec<Deployment> = match self {
+            DeploymentSearch::Name { name } => {
+                let pattern = format!("%{}%", name);
+                query.filter(s::name.ilike(&pattern)).load(conn)?
+            }
+            DeploymentSearch::Hash { hash, shard } => {
+                let query = query.filter(ds::subgraph.eq(&hash));
+                match shard {
+                    Some(shard) => query.filter(ds::shard.eq(shard)).load(conn)?,
+                    None => query.load(conn)?,
+                }
+            }
+            DeploymentSearch::Deployment { namespace } => {
+                query.filter(ds::name.eq(&namespace)).load(conn)?
+            }
+        };
+        Ok(deployments)
+    }
+
+    /// Finds a single deployment locator for the given deployment identifier.
+    pub fn locate_unique(&self, pool: &ConnectionPool) -> anyhow::Result<DeploymentLocator> {
+        let mut locators: Vec<DeploymentLocator> = HashSet::<DeploymentLocator>::from_iter(
+            self.lookup(pool)?
+                .into_iter()
+                .map(|deployment| deployment.locator()),
+        )
+        .into_iter()
+        .collect();
+        let deployment_locator = match locators.len() {
+            0 => anyhow::bail!("Found no deployment for `{}`", self),
+            1 => locators.pop().unwrap(),
+            n => anyhow::bail!("Found {} deployments for `{}`", n, self),
+        };
+        Ok(deployment_locator)
+    }
+}
 
 #[derive(Queryable, PartialEq, Eq, Hash, Debug)]
 pub struct Deployment {
