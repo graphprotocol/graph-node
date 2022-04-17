ef16c8ef99324847aec26eb2ff2beccd3d58e48c
clearloop <26088946+clearloop@users.noreply.github.com>
2022-04-18 00:59:16 +0800

    chore(node): register arweave

--- node/src/main.rs
+++ node/src/main.rs
@@ -10,6 +10,7 @@ use graph::log::logger;
 use graph::prelude::{IndexNodeServer as _, JsonRpcServer as _, *};
 use graph::prometheus::Registry;
 use graph::url::Url;
+use graph_chain_arweave::{self as arweave, Block as ArweaveBlock};
 use graph_chain_ethereum as ethereum;
 use graph_chain_near::{self as near, HeaderOnlyBlock as NearFirehoseHeaderOnlyBlock};
 use graph_chain_tendermint::{self as tendermint, EventList as TendermintFirehoseEventList};
@@ -239,6 +240,14 @@ async fn main() {
         // `blockchain_map`.
         let mut blockchain_map = BlockchainMap::new();
 
+        let (arweave_networks, arweave_idents) = connect_firehose_networks::<ArweaveBlock>(
+            &logger,
+            firehose_networks_by_kind
+                .remove(&BlockchainKind::Arweave)
+                .unwrap_or_else(|| FirehoseNetworks::new()),
+        )
+        .await;
+
         let (eth_networks, ethereum_idents) =
             connect_ethereum_networks(&logger, eth_networks).await;
 
@@ -262,11 +271,21 @@ async fn main() {
 
         let network_identifiers = ethereum_idents
             .into_iter()
+            .chain(arweave_idents)
             .chain(near_idents)
             .chain(tendermint_idents)
             .collect();
 
         let network_store = store_builder.network_store(network_identifiers);
+
+        let arweave_chains = arweave_networks_as_chains(
+            &mut blockchain_map,
+            &logger,
+            &arweave_networks,
+            network_store.as_ref(),
+            &logger_factory,
+            metrics_registry.clone(),
+        );
 
         let ethereum_chains = ethereum_networks_as_chains(
             &mut blockchain_map,
@@ -341,11 +360,18 @@ async fn main() {
                 );
             }
 
+            start_firehose_block_ingestor::<_, ArweaveBlock>(
+                &logger,
+                &network_store,
+                arweave_chains,
+            );
+
             start_firehose_block_ingestor::<_, NearFirehoseHeaderOnlyBlock>(
                 &logger,
                 &network_store,
                 near_chains,
             );
+
             start_firehose_block_ingestor::<_, TendermintFirehoseEventList>(
                 &logger,
                 &network_store,
@@ -527,6 +553,55 @@ async fn main() {
     });
 
     futures::future::pending::<()>().await;
+}
+
+/// Return the hashmap of NEAR chains and also add them to `blockchain_map`.
+fn arweave_networks_as_chains(
+    blockchain_map: &mut BlockchainMap,
+    logger: &Logger,
+    firehose_networks: &FirehoseNetworks,
+    store: &Store,
+    logger_factory: &LoggerFactory,
+    metrics_registry: Arc<MetricsRegistry>,
+) -> HashMap<String, FirehoseChain<arweave::Chain>> {
+    let chains: Vec<_> = firehose_networks
+        .networks
+        .iter()
+        .filter_map(|(chain_id, endpoints)| {
+            store
+                .block_store()
+                .chain_store(chain_id)
+                .map(|chain_store| (chain_id, chain_store, endpoints))
+                .or_else(|| {
+                    error!(
+                        logger,
+                        "No store configured for NEAR chain {}; ignoring this chain", chain_id
+                    );
+                    None
+                })
+        })
+        .map(|(chain_id, chain_store, endpoints)| {
+            (
+                chain_id.clone(),
+                FirehoseChain {
+                    chain: Arc::new(arweave::Chain::new(
+                        logger_factory.clone(),
+                        chain_id.clone(),
+                        chain_store,
+                        endpoints.clone(),
+                        metrics_registry.clone(),
+                    )),
+                    firehose_endpoints: endpoints.clone(),
+                },
+            )
+        })
+        .collect();
+
+    for (chain_id, firehose_chain) in chains.iter() {
+        blockchain_map.insert::<arweave::Chain>(chain_id.clone(), firehose_chain.chain.clone())
+    }
+
+    HashMap::from_iter(chains)
 }
 
 /// Return the hashmap of ethereum chains and also add them to `blockchain_map`.
