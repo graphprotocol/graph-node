7bd83a766597ee4090ff351a1b30826ff0109a34
Robert Ignat <robert.ignat91@gmail.com>
2022-05-06 21:05:37 +0300

    Remove old tendermint firehose transform file

--- graph/src/firehose/fig.tendermint.transform.v1.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct EventFilter {
-    #[prost(string, repeated, tag = "1")]
-    pub event_types: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
-}
