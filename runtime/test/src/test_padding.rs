a300f94f7c1a5239fcc30f1e16c93081d5d9283f
Serguei Oleinik <serguei.oleinik@figment.io>
2022-08-01 20:49:55 +0300

    WIP

--- runtime/test/src/test_padding.rs
+++ runtime/test/src/test_padding.rs
@@ -6,7 +6,7 @@ use graph::prelude::tokio;
 async fn test_bool_ok() {
     let api_version = super::test::API_VERSION_0_0_5;
 
-    let module = super::test::test_module(
+    let mut module = super::test::test_module(
         "bogus_sub_graph_id",
         super::common::mock_data_source(
             &super::test::wasm_file_path("my_test.wasm", api_version.clone()),
@@ -22,10 +22,20 @@ async fn test_bool_ok() {
         str_suff: "suff".into(),
     };
 
-    let new_obj: graph::runtime::AscPtr<AscUnitTestType> =
-        module.invoke_export1("conv_fun_does_not_exists_yet", &parm);
+    let new_obj = module.asc_new(&parm).unwrap();
 
-    //this line does not compile b/c new_obj  has to implement pub unsafe trait WasmParams
-    let res: Result<(), _> = module.get_func("my_test").typed().unwrap().call(new_obj);
+    // let new_obj: graph::runtime::AscPtr<AscUnitTestType> =
+    //     module.invoke_export1("conv_fun_does_not_exists_yet", &parm);
+
+    let func = module.get_func("my_test").typed().unwrap().clone();
+
+    let res: Result<(), _> = func.call(new_obj.wasm_ptr());
+
+    // //this line does not compile b/c new_obj  has to implement pub unsafe trait WasmParams
+    // let res: Result<(), _> = module
+    //     .get_func("my_test")
+    //     .typed()
+    //     .unwrap()
+    //     .call(new_obj.wasm_ptr());
     assert!(res.is_ok());
 }
--- runtime/test/wasm_test/api_version_0_0_5/my_test.ts
+++ runtime/test/wasm_test/api_version_0_0_5/my_test.ts
@@ -1,15 +1,24 @@
 export * from './common/global'
+import { Value, ValueKind, TypedMapEntry, TypedMap, Entity, JSONValueKind, JSONValue } from './common/types'
 
 
 export class UnitTestType{
     str_pref: string;
     under_test: boolean;
     str_suff: string;
+
+    constructor(str_pref: string, under_test: boolean, str_suff:string) {
+        this.str_pref = str_pref;
+        this.under_test = under_test;
+        this.str_suff = str_suff;
+    }
 }
 
 
+//export function my_test(parm: UnitTestType): void {
+export function my_test(p: Value): void {
+    let parm = changetype<UnitTestType>(p as u32);
 
-export function my_test(parm: UnitTestType): void {
     assert(parm.str_pref == "pref`", "parm.str_pref: Assertion failed!")
     assert(parm.under_test == true, "parm.under_test: Assertion failed!")
     assert(parm.str_suff == "suff`", "parm.str_suff: Assertion failed!")
