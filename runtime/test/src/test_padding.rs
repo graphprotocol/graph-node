dc7405b93022c895a982ba2ed9bf0ecb66189150
Serguei Oleinik <serguei.oleinik@figment.io>
2022-08-01 17:51:13 +0300

    WIP

--- runtime/test/src/test_padding.rs
+++ runtime/test/src/test_padding.rs
@@ -2,18 +2,6 @@ use super::test::WasmInstanceExt;
 use crate::protobuf::{self, *};
 use graph::prelude::tokio;
 
-//========= this declaration is in protobuf module ============
-// #[graph_runtime_derive::generate_asc_type()]
-// #[graph_runtime_derive::generate_network_type_id(UnitTestNetwork)]
-// #[graph_runtime_derive::generate_from_rust_type()]
-// #[graph_runtime_derive::generate_array_type(UnitTestNetwork)]
-// #[derive(Debug, PartialEq)]
-// pub struct UnitTestType {
-//     pub str_pref: String,
-//     pub under_test: bool,
-//     pub str_suff: String,
-// }
-
 #[tokio::test]
 async fn test_bool_ok() {
     let api_version = super::test::API_VERSION_0_0_5;
@@ -34,7 +22,8 @@ async fn test_bool_ok() {
         str_suff: "suff".into(),
     };
 
-    let new_obj: graph::runtime::AscPtr<AscUnitTestType> = module.invoke_export1("test_uint", &parm);
+    let new_obj: graph::runtime::AscPtr<AscUnitTestType> =
+        module.invoke_export1("test_uint", &parm);
 
     let res: Result<(), _> = module.get_func("my_test").typed().unwrap().call(new_obj);
     assert!(res.is_ok());
