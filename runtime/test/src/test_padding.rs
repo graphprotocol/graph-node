4bdbae1c7841365e279debdda3c6094b97bc3f29
Serguei Oleinik <serguei.oleinik@figment.io>
2022-08-01 23:23:15 +0300

    WIP

--- runtime/test/src/test_padding.rs
+++ runtime/test/src/test_padding.rs
@@ -24,18 +24,9 @@ async fn test_bool_ok() {
 
     let new_obj = module.asc_new(&parm).unwrap();
 
-    // let new_obj: graph::runtime::AscPtr<AscUnitTestType> =
-    //     module.invoke_export1("conv_fun_does_not_exists_yet", &parm);
-
     let func = module.get_func("my_test").typed().unwrap().clone();
 
     let res: Result<(), _> = func.call(new_obj.wasm_ptr());
 
-    // //this line does not compile b/c new_obj  has to implement pub unsafe trait WasmParams
-    // let res: Result<(), _> = module
-    //     .get_func("my_test")
-    //     .typed()
-    //     .unwrap()
-    //     .call(new_obj.wasm_ptr());
     assert!(res.is_ok(), "{:?}", res.err());
 }
