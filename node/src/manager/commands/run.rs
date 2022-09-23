294ea2289750acde0a8500c107317538722d2335
Filippo Costa <filippo@neysofu.me>
2022-09-23 18:44:00 +0200

    node: code reuse for create_ipfs_clients

--- node/src/manager/commands/run.rs
+++ node/src/manager/commands/run.rs
@@ -4,6 +4,7 @@ use std::time::Duration;
 
 use crate::chain::{
     connect_ethereum_networks, create_ethereum_networks_for_chain, create_firehose_networks,
+    create_ipfs_clients,
 };
 use crate::config::Config;
 use crate::manager::PanicSubscriptionManager;
@@ -11,20 +12,17 @@ use crate::store_builder::StoreBuilder;
 use crate::MetricsContext;
 use ethereum::chain::{EthereumAdapterSelector, EthereumStreamBuilder};
 use ethereum::{ProviderEthRpcMetrics, RuntimeAdapter as EthereumRuntimeAdapter};
-use futures::TryFutureExt;
 use graph::anyhow::{bail, format_err};
 use graph::blockchain::{BlockchainKind, BlockchainMap};
 use graph::cheap_clone::CheapClone;
 use graph::components::store::{BlockStore as _, DeploymentLocator};
 use graph::env::EnvVars;
 use graph::firehose::FirehoseEndpoints;
-use graph::ipfs_client::IpfsClient;
 use graph::prelude::{
     anyhow, tokio, BlockNumber, DeploymentHash, LoggerFactory, NodeId, SubgraphAssignmentProvider,
     SubgraphName, SubgraphRegistrar, SubgraphStore, SubgraphVersionSwitchingMode, ENV_VARS,
 };
-use graph::slog::{debug, error, info, Logger};
-use graph::util::security::SafeDisplay;
+use graph::slog::{debug, info, Logger};
 use graph_chain_ethereum::{self as ethereum};
 use graph_core::polling_monitor::ipfs_service::IpfsService;
 use graph_core::{
@@ -259,71 +257,4 @@ pub async fn run(
     }
 
     Ok(())
-}
-
-fn create_ipfs_clients(logger: &Logger, ipfs_addresses: &Vec<String>) -> Vec<IpfsClient> {
-    // Parse the IPFS URL from the `--ipfs` command line argument
-    let ipfs_addresses: Vec<_> = ipfs_addresses
-        .iter()
-        .map(|uri| {
-            if uri.starts_with("http://") || uri.starts_with("https://") {
-                String::from(uri)
-            } else {
-                format!("http://{}", uri)
-            }
-        })
-        .collect();
-
-    ipfs_addresses
-        .into_iter()
-        .map(|ipfs_address| {
-            info!(
-                logger,
-                "Trying IPFS node at: {}",
-                SafeDisplay(&ipfs_address)
-            );
-
-            let ipfs_client = match IpfsClient::new(&ipfs_address) {
-                Ok(ipfs_client) => ipfs_client,
-                Err(e) => {
-                    error!(
-                        logger,
-                        "Failed to create IPFS client for `{}`: {}",
-                        SafeDisplay(&ipfs_address),
-                        e
-                    );
-                    panic!("Could not connect to IPFS");
-                }
-            };
-
-            // Test the IPFS client by getting the version from the IPFS daemon
-            let ipfs_test = ipfs_client.cheap_clone();
-            let ipfs_ok_logger = logger.clone();
-            let ipfs_err_logger = logger.clone();
-            let ipfs_address_for_ok = ipfs_address.clone();
-            let ipfs_address_for_err = ipfs_address.clone();
-            graph::spawn(async move {
-                ipfs_test
-                    .test()
-                    .map_err(move |e| {
-                        error!(
-                            ipfs_err_logger,
-                            "Is there an IPFS node running at \"{}\"?",
-                            SafeDisplay(ipfs_address_for_err),
-                        );
-                        panic!("Failed to connect to IPFS: {}", e);
-                    })
-                    .map_ok(move |_| {
-                        info!(
-                            ipfs_ok_logger,
-                            "Successfully connected to IPFS node at: {}",
-                            SafeDisplay(ipfs_address_for_ok)
-                        );
-                    })
-                    .await
-            });
-
-            ipfs_client
-        })
-        .collect()
 }
