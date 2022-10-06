dabfc1e6cb2cde0b0a018b271c190653cf45d307
Leonardo Yvens <leoyvens@gmail.com>
2022-10-06 11:54:28 +0100

    tests: Failing test for file data sources duplication

--- tests/integration-tests/file-data-sources/src/mapping.ts
+++ tests/integration-tests/file-data-sources/src/mapping.ts
@@ -2,7 +2,9 @@ import { ethereum, dataSource, BigInt, Bytes } from '@graphprotocol/graph-ts'
 import { IpfsFile } from '../generated/schema'
 
 export function handleBlock(block: ethereum.Block): void {
-  if (block.number == BigInt.fromI32(0)) {
+  // This will create the same data source twice, once at block 0 and another at block 2.
+  // The creation at block 2 should be detected as a duplicate and therefore a noop.
+  if (block.number == BigInt.fromI32(0) || block.number == BigInt.fromI32(2)) {
     // CID QmVkvoPGi9jvvuxsHDVJDgzPEzagBaWSZRYoRDzU244HjZ is the file
     // `file-data-sources/abis/Contract.abi` after being processed by graph-cli.
     dataSource.create("File", ["QmVkvoPGi9jvvuxsHDVJDgzPEzagBaWSZRYoRDzU244HjZ"])
