d25a13a1316183e7a5f705d6d1478c2801bfb18e
poonai <rbalajis25@gmail.com>
2022-10-12 10:19:24 +0000

    minor
    Signed-off-by: poonai <rbalajis25@gmail.com>

--- tests/integration-tests/file-data-sources/src/mapping.ts
+++ tests/integration-tests/file-data-sources/src/mapping.ts
@@ -10,7 +10,7 @@ export function handleBlock(block: ethereum.Block): void {
     dataSource.create("File", ["QmVkvoPGi9jvvuxsHDVJDgzPEzagBaWSZRYoRDzU244HjZ"])
   }
 
-  // This will invoke File1 datasource with same CID, which will be used 
+  // This will invoke File1 data source with same CID, which will be used 
   // to test whether same cid is triggered across different data source.
   if (block.number == BigInt.fromI32(3)) {
     dataSource.create("File1", ["QmVkvoPGi9jvvuxsHDVJDgzPEzagBaWSZRYoRDzU244HjZ"])
