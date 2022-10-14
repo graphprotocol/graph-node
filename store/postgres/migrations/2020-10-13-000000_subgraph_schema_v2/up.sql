0f04ba3955fd11682c57bdb72336428c48b05b2b
poonai <rbalajis25@gmail.com>
2022-10-14 04:56:18 +0000

    update migration script
    Signed-off-by: poonai <rbalajis25@gmail.com>

--- store/postgres/migrations/2020-10-13-000000_subgraph_schema_v2/up.sql
+++ store/postgres/migrations/2020-10-13-000000_subgraph_schema_v2/up.sql
@@ -1,10 +1,23 @@
+-- add done_at column to data_sources$ table for each subgraph deployment
 do $$
-declare 
-   deployments cursor for select * from deployment_schemas where version = 1;
+declare
+  deployments cursor for
+     select t.table_schema as sgd
+       from information_schema.tables t
+      where t.table_schema like 'sgd%'
+        and t.table_name = 'data_sources$'
+        and not exists (select 1 from information_schema.columns c
+                         where c.table_name = t.table_name
+                           and c.table_schema = t.table_schema
+                           and c.column_name = 'done_at');
 begin
-   for deployment in deployments loop
-      execute 'alter table ' || deployment.name || '.data_sources$ add done_at int';
-      execute 'update deployment_schemas set version = 2 where id = ' || deployment.id;
-   end loop;
+  for d in deployments loop
+    execute 'alter table ' || d.sgd || '.data_sources$ add done_at int';
+  end loop;
 end;
 $$;
+
+-- update deployment_schema version from 2 to 1
+update deployment_schemas
+   set version = 2
+ where version = 1;
