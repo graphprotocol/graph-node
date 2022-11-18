7bf9dc11bfff2c13f5c5a41bf80dec1530a2b5fe
vutran1710 <me@vutr.io>
2022-11-18 22:24:32 +0700

    up

--- store/postgres/src/writable.rs
+++ store/postgres/src/writable.rs
@@ -23,7 +23,7 @@ use graph::{
         BlockPtr, DeploymentHash, EntityModification, Error, Logger, StopwatchMetrics, StoreError,
         StoreEvent, UnfailOutcome, ENV_VARS,
     },
-    slog::{debug, error, warn},
+    slog::{error, warn},
     util::backoff::ExponentialBackoff,
 };
 use store::StoredDynamicDataSource;
@@ -576,8 +576,7 @@ impl Queue {
                 };
                 let res = {
                     let _section = queue.stopwatch.start_section("queue_execute");
-                    let clone_logger = logger.cheap_clone();
-                    graph::spawn_blocking_allow_panic(move || req.execute(&clone_logger)).await
+                    graph::spawn_blocking_allow_panic(move || req.execute()).await
                 };
 
                 let _section = queue.stopwatch.start_section("queue_pop");
