a4f6d6207d1785e7949d7e12bec3ad8797c6ef25
Stéphane Duchesneau <stephane.duchesneau@gmail.com>
2023-04-28 10:52:15 -0400

    bump substreams client to sf.substreams.rpc.v2.Stream/Blocks (#4556)
    * bump substreams client to sf.substreams.rpc.v2.Stream/Blocks
    
    This breaks compatibility with previous substreams servers that only
    support 'sf.substreams.v1.Stream/Blocks'
    
    * adjust substreams mapper code branches for clarity
    
    * add comments to clarify intent when ignoring useless substreams messages
    
    ---------
    
    Co-authored-by: Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
    Co-authored-by: Eduard Voiculescu <eduardvoiculescu95@gmail.com>

--- graph/build.rs
+++ graph/build.rs
@@ -18,4 +18,11 @@ fn main() {
         .out_dir("src/substreams")
         .compile(&["proto/substreams.proto"], &["proto"])
         .expect("Failed to compile Substreams proto(s)");
+
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .extern_path(".sf.substreams.v1", "crate::substreams")
+        .out_dir("src/substreams_rpc")
+        .compile(&["proto/substreams-rpc.proto"], &["proto"])
+        .expect("Failed to compile Substreams RPC proto(s)");
 }
