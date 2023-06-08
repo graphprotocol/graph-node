5c889d5078f0597ae507221df66eafaef2da8aea
incrypto32 <vpkrishnanand@gmail.com>
2023-06-08 20:05:12 +0900

    graph, store: use unified_api_version in subgraph_features

--- store/postgres/migrations/2023-06-06-054643_create_subgraph_features_table/up.sql
+++ store/postgres/migrations/2023-06-06-054643_create_subgraph_features_table/up.sql
@@ -2,7 +2,7 @@
 create table if not exists subgraphs.subgraph_features (
   id text primary key,
   spec_version text not null,
-  api_versions text [] not null,
+  api_version text null,
   features text [] not null DEFAULT '{}',
   data_sources text [] not null DEFAULT '{}'
 );
