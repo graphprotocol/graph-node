d3768ac3cbce7c98e098bce10dae121786d5a319
Zoran Cvetkov <zoran@edgeandnode.com>
2023-11-13 17:28:01 +0100

    new version of big number libs

--- graph/src/data/store/scalar.rs
+++ graph/src/data/store/scalar.rs
@@ -262,7 +262,7 @@ mod big_int {
         pub fn new(inner: num_bigint::BigInt) -> Result<Self, anyhow::Error> {
             // `inner.bits()` won't include the sign bit, so we add 1 to account for it.
             let bits = inner.bits() + 1;
-            if bits > Self::MAX_BITS as usize {
+            if bits > Self::MAX_BITS as u64 {
                 anyhow::bail!(
                     "BigInt is too big, total bits {} (max {})",
                     bits,
@@ -294,7 +294,7 @@ mod big_int {
         }
 
         pub fn bits(&self) -> usize {
-            self.0.bits()
+            self.0.bits() as usize
         }
 
         pub(super) fn inner(self) -> num_bigint::BigInt {
@@ -564,7 +564,7 @@ impl Shl<u8> for BigInt {
     type Output = Self;
 
     fn shl(self, bits: u8) -> Self {
-        BigInt::unchecked_new(self.inner().shl(bits.into()))
+        BigInt::unchecked_new(self.inner().shl(<u8 as Into<u8>>::into(bits)))
     }
 }
 
@@ -572,7 +572,7 @@ impl Shr<u8> for BigInt {
     type Output = Self;
 
     fn shr(self, bits: u8) -> Self {
-        BigInt::unchecked_new(self.inner().shr(bits.into()))
+        BigInt::unchecked_new(self.inner().shr(<u8 as Into<u8>>::into(bits)))
     }
 }
 
