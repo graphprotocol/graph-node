a81013027f47b37964e033dcac902cf6fd62282a
Zoran Cvetkov <zoran@edgeandnode.com>
2023-11-16 17:52:10 +0200

    compile graph crate

--- graph/src/data/store/sql.rs
+++ graph/src/data/store/sql.rs
@@ -10,7 +10,7 @@ use super::{scalar, Value};
 impl ToSql<Bool, Pg> for Value {
     fn to_sql(&self, out: &mut Output<Pg>) -> serialize::Result {
         match self {
-            Value::Bool(b) => <bool as ToSql<Bool, Pg>>::to_sql(b, out),
+            Value::Bool(b) => <bool as ToSql<Bool, Pg>>::to_sql(b, &mut out.reborrow()),
             v => Err(anyhow!(
                 "Failed to convert non-boolean attribute value to boolean in SQL: {}",
                 v
@@ -23,7 +23,7 @@ impl ToSql<Bool, Pg> for Value {
 impl ToSql<Integer, Pg> for Value {
     fn to_sql(&self, out: &mut Output<Pg>) -> serialize::Result {
         match self {
-            Value::Int(i) => <i32 as ToSql<Integer, Pg>>::to_sql(i, out),
+            Value::Int(i) => <i32 as ToSql<Integer, Pg>>::to_sql(i, &mut out.reborrow()),
             v => Err(anyhow!(
                 "Failed to convert non-int attribute value to int in SQL: {}",
                 v
@@ -36,7 +36,7 @@ impl ToSql<Integer, Pg> for Value {
 impl ToSql<Int8, Pg> for Value {
     fn to_sql(&self, out: &mut Output<Pg>) -> serialize::Result {
         match self {
-            Value::Int8(i) => <i64 as ToSql<Int8, Pg>>::to_sql(i, out),
+            Value::Int8(i) => <i64 as ToSql<Int8, Pg>>::to_sql(i, &mut out.reborrow()),
             Value::Int(i) => <i64 as ToSql<Int8, Pg>>::to_sql(&(*i as i64), out),
             v => Err(anyhow!(
                 "Failed to convert non-int8 attribute value to int8 in SQL: {}",
@@ -50,8 +50,10 @@ impl ToSql<Int8, Pg> for Value {
 impl ToSql<Text, Pg> for Value {
     fn to_sql(&self, out: &mut Output<Pg>) -> serialize::Result {
         match self {
-            Value::String(s) => <String as ToSql<Text, Pg>>::to_sql(s, out),
-            Value::Bytes(h) => <String as ToSql<Text, Pg>>::to_sql(&h.to_string(), out),
+            Value::String(s) => <String as ToSql<Text, Pg>>::to_sql(s, &mut out.reborrow()),
+            Value::Bytes(h) => {
+                <String as ToSql<Text, Pg>>::to_sql(&h.to_string(), &mut out.reborrow())
+            }
             v => Err(anyhow!(
                 "Failed to convert attribute value to String or Bytes in SQL: {}",
                 v
@@ -64,10 +66,11 @@ impl ToSql<Text, Pg> for Value {
 impl ToSql<Binary, Pg> for Value {
     fn to_sql(&self, out: &mut Output<Pg>) -> serialize::Result {
         match self {
-            Value::Bytes(h) => <_ as ToSql<Binary, Pg>>::to_sql(&h.as_slice(), out),
-            Value::String(s) => {
-                <_ as ToSql<Binary, Pg>>::to_sql(scalar::Bytes::from_str(s)?.as_slice(), out)
-            }
+            Value::Bytes(h) => <_ as ToSql<Binary, Pg>>::to_sql(&h.as_slice(), &mut out.reborrow()),
+            Value::String(s) => <_ as ToSql<Binary, Pg>>::to_sql(
+                scalar::Bytes::from_str(s)?.as_slice(),
+                &mut out.reborrow(),
+            ),
             v => Err(anyhow!("Failed to convert attribute value to Bytes in SQL: {}", v).into()),
         }
     }
