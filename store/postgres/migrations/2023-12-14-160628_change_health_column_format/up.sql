bdce00c896b950d73118a746f4eabdffded813cb
Zoran Cvetkov <zoran@edgeandnode.com>
2023-12-14 18:29:09 +0200

    remove hack, proper migration

---
+++ store/postgres/migrations/2023-12-14-160628_change_health_column_format/up.sql
@@ -0,0 +1,24 @@
+alter table
+    subgraphs.subgraph_deployment
+add
+    column if not exists health_new text not null default 'failed';
+
+update
+    subgraphs.subgraph_deployment
+set
+    health_new = health;
+
+alter table 
+    subgraphs.subgraph_deployment 
+alter column 
+    health_new drop default;
+
+alter table 
+    subgraphs.subgraph_deployment 
+drop column 
+    health;
+
+alter table 
+    subgraphs.subgraph_deployment 
+rename column
+    health_new to health;
