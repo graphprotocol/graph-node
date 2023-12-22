d657af73d506e1023ef8d7ee33855bb109254520
incrypto32 <vpkrishnanand@gmail.com>
2023-12-22 14:57:44 +0530

    graphql: wire SQLParser in StoreResolver::handle_sql

--- graphql/src/store/resolver.rs
+++ graphql/src/store/resolver.rs
@@ -14,6 +14,8 @@ use graph::schema::{
     META_FIELD_NAME, META_FIELD_TYPE, SQL_FIELD_TYPE,
 };
 use graph::schema::{ErrorPolicy, BLOCK_FIELD_TYPE};
+use graph_sql::parser::SqlParser;
+
 
 use crate::execution::{ast as a, Query};
 use crate::metrics::GraphQLMetrics;
@@ -34,6 +36,7 @@ pub struct StoreResolver {
     error_policy: ErrorPolicy,
     graphql_metrics: Arc<GraphQLMetrics>,
     load_manager: Arc<LoadManager>,
+    pub sql_parser: Arc<SqlParser>,
 }
 
 #[derive(Clone, Debug)]
@@ -84,6 +87,7 @@ impl StoreResolver {
             error_policy: ErrorPolicy::Deny,
             graphql_metrics,
             load_manager,
+            sql_parser: Arc::new(SqlParser::new()),
         }
     }
 
@@ -123,6 +127,7 @@ impl StoreResolver {
             error_policy,
             graphql_metrics,
             load_manager,
+            sql_parser: Arc::new(SqlParser::new()),
         };
         Ok(resolver)
     }
@@ -307,6 +312,10 @@ impl StoreResolver {
                 ))
             }
         };
+
+        let query = self
+            .sql_parser
+            .parse_and_validate(query, self.store.deployment_id().0)?;
 
         let result = self.store.execute_sql(&query)?;
         let result = result.into_iter().map(|q| q.0).collect::<Vec<_>>();
