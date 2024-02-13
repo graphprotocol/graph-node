3898646d34f49a8aa68dbf73d5682610f5807ec1
David Lutterkort <lutter@watzmann.net>
2024-02-13 10:49:43 -0800

    chain/near: Timestamps are in nanoseconds

--- chain/near/src/codec.rs
+++ chain/near/src/codec.rs
@@ -77,8 +77,7 @@ impl BlockchainBlock for Block {
     }
 
     fn timestamp(&self) -> BlockTime {
-        let ts = i64::try_from(self.header().timestamp).unwrap();
-        BlockTime::since_epoch(ts, 0)
+        block_time_from_header(self.header())
     }
 }
 
@@ -108,8 +107,7 @@ impl BlockchainBlock for HeaderOnlyBlock {
     }
 
     fn timestamp(&self) -> BlockTime {
-        let ts = i64::try_from(self.header().timestamp).unwrap();
-        BlockTime::since_epoch(ts, 0)
+        block_time_from_header(self.header())
     }
 }
 
@@ -121,4 +119,26 @@ impl execution_outcome::Status {
             SuccessValue(_) | SuccessReceiptId(_) => true,
         }
     }
+}
+
+fn block_time_from_header(header: &BlockHeader) -> BlockTime {
+    // The timstamp is in ns since the epoch
+    let ts = i64::try_from(header.timestamp_nanosec).unwrap();
+    let secs = ts / 1_000_000_000;
+    let ns: u32 = (ts % 1_000_000_000) as u32;
+    BlockTime::since_epoch(secs, ns)
+}
+
+#[test]
+fn timestamp_conversion() {
+    // 2020-07-21T21:50:10Z in ns
+    let ts = 1_595_368_210_762_782_796;
+    let header = BlockHeader {
+        timestamp_nanosec: ts,
+        ..Default::default()
+    };
+    assert_eq!(
+        1595368210,
+        block_time_from_header(&header).as_secs_since_epoch()
+    );
 }
