graphprotocol/graph-node

commit e76ad68d7b624596ec44c3e055858fb8e20bb429

graph: Do not include the _Schema_ type in the API schema

graph/src/schema/api.rs
@@ -11,7 +11,7 @@ use crate::cheap_clone::CheapClone;
 use crate::data::graphql::{ObjectOrInterface, ObjectTypeExt, TypeExt};
 use crate::data::store::IdType;
 use crate::env::ENV_VARS;
-use crate::schema::{ast, META_FIELD_NAME, META_FIELD_TYPE};
+use crate::schema::{ast, META_FIELD_NAME, META_FIELD_TYPE, SCHEMA_TYPE_NAME};
 
 use crate::data::graphql::ext::{
     camel_cased_names, DefinitionExt, DirectiveExt, DocumentExt, ValueExt,
@@ -391,11 +391,13 @@ fn init_api_schema(input_schema: &InputSchema) -> Result<Schema, APISchemaError>
     ) -> Result<(), APISchemaError> {
         match type_def {
             s::TypeDefinition::Object(ot) => {
-                let mut ot = ot.clone();
-                add_collection_arguments(&mut ot.fields, input_schema);
-                let typedef = s::TypeDefinition::Object(ot);
-                let def = s::Definition::TypeDefinition(typedef);
-                api.definitions.push(def);
+                if ot.name != SCHEMA_TYPE_NAME {
+                    let mut ot = ot.clone();
+                    add_collection_arguments(&mut ot.fields, input_schema);
+                    let typedef = s::TypeDefinition::Object(ot);
+                    let def = s::Definition::TypeDefinition(typedef);
+                    api.definitions.push(def);
+                }
             }
             s::TypeDefinition::Interface(it) => {
                 let mut it = it.clone();

commit b1bf41cd63f9636296b210992abdbd0196f6b70e

graph: Fix suppression of _Schema_ type
Commit e76ad68d caused fulltext search fields to not show up in the Query
type

graph/src/schema/api.rs
@@ -1051,7 +1051,7 @@ fn add_query_type(api: &mut s::Document, input_schema: &InputSchema) -> Result<(
         .map(|(name, _)| name)
         .flat_map(query_fields_for_agg_type)
         .collect::<Vec<s::Field>>();
-    let mut fulltext_fields = api
+    let mut fulltext_fields = input_schema
         .get_fulltext_directives()
         .map_err(|_| APISchemaError::FulltextSearchNonDeterministic)?
         .iter()
@@ -1311,7 +1311,7 @@ mod tests {
             subgraph::LATEST_VERSION,
         },
         prelude::{s, DeploymentHash},
-        schema::InputSchema,
+        schema::{InputSchema, SCHEMA_TYPE_NAME},
     };
     use graphql_parser::schema::*;
     use lazy_static::lazy_static;
@@ -2131,6 +2131,9 @@ type Gravatar @entity {
 "#;
         let schema = parse(SCHEMA);
 
+        // The _Schema_ type must not be copied to the API schema as it will
+        // cause GraphQL validation failures on clients
+        assert_eq!(None, schema.get_named_type(SCHEMA_TYPE_NAME));
         let query_type = schema
             .get_named_type("Query")
             .expect("Query type is missing in derived API schema");