a7606e4d5399e8b5731b47c8c197aa6ac1e85c88
Jerry Okolo <73024324+itsjerryokolo@users.noreply.github.com>
2024-03-12 13:18:03 +0100

    Update node/src/manager/commands/rewind.rs
    Co-authored-by: Krishnanand V P <44740264+incrypto32@users.noreply.github.com>

--- node/src/manager/commands/rewind.rs
+++ node/src/manager/commands/rewind.rs
@@ -107,7 +107,7 @@ pub async fn run(
 
     println!("Pausing deployments");
     for deployment in &deployments {
-        let paused = pause_or_resume(primary.clone(), &sender, &deployment.locator(), true);
+        let paused = pause_or_resume(primary.clone(), &sender, &deployment.locator(), true)?;
 
         if paused.is_ok() {
             // There's no good way to tell that a subgraph has in fact stopped
