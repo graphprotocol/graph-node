graphprotocol/graph-node

commit f06c606178f6c4b50ebb2b8ab2c8cd5881e1787a

all: Upgrade hyper to version 1 for GraphQL and index node servers

graph/src/data/query/result.rs
@@ -1,12 +1,12 @@
 use super::error::{QueryError, QueryExecutionError};
 use super::trace::{HttpTrace, TRACE_NONE};
 use crate::cheap_clone::CheapClone;
+use crate::components::server::query::GraphQLResponse;
 use crate::data::value::Object;
 use crate::prelude::{r, CacheWeight, DeploymentHash};
-use http::header::{
-    ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_ORIGIN,
-    CONTENT_TYPE,
-};
+use http_body_util::Full;
+use hyper::header::{ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE};
+use hyper::Response;
 use serde::ser::*;
 use serde::Serialize;
 use std::convert::TryFrom;
@@ -82,6 +82,10 @@ impl QueryResults {
 
     pub fn errors(&self) -> Vec<QueryError> {
         self.results.iter().flat_map(|r| r.errors.clone()).collect()
+    }
+
+    pub fn is_attestable(&self) -> bool {
+        self.results.iter().all(|r| r.is_attestable())
     }
 }
 
@@ -194,23 +198,13 @@ impl QueryResults {
         self.results.push(other);
     }
 
-    pub fn as_http_response<T: From<String>>(&mut self) -> http::Response<T> {
-        let status_code = http::StatusCode::OK;
-
-        let json =
-            serde_json::to_string(self).expect("Failed to serialize GraphQL response to JSON");
-
-        http::Response::builder()
-            .status(status_code)
+    pub fn as_http_response(&self) -> GraphQLResponse {
+        let json = serde_json::to_string(&self).unwrap();
+        Response::builder()
+            .status(200)
             .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-            .header(ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, User-Agent")
-            .header(ACCESS_CONTROL_ALLOW_METHODS, "GET, OPTIONS, POST")
             .header(CONTENT_TYPE, "application/json")
-            .header(
-                "Graph-Attestable",
-                self.results.iter().all(|r| r.is_attestable()).to_string(),
-            )
-            .body(T::from(json))
+            .body(Full::from(json))
             .unwrap()
     }
 }

graph/src/components/server/server.rs
@@ -1,0 +1,70 @@
+use std::future::Future;
+use std::net::SocketAddr;
+use std::sync::atomic::AtomicBool;
+use std::sync::Arc;
+
+use hyper::body::Incoming;
+use hyper::Request;
+
+use crate::cheap_clone::CheapClone;
+use crate::hyper::server::conn::http1;
+use crate::hyper::service::service_fn;
+use crate::hyper_util::rt::TokioIo;
+use crate::slog::error;
+use crate::tokio::net::TcpListener;
+use crate::tokio::task::JoinHandle;
+use crate::{anyhow, tokio};
+
+use crate::prelude::Logger;
+
+use super::query::GraphQLResult;
+
+/// A handle to the server that can be used to shut it down. The `accepting`
+/// field is only used in tests to check if the server is running
+pub struct ServerHandle {
+    pub handle: JoinHandle<()>,
+    pub accepting: Arc<AtomicBool>,
+}
+
+pub async fn start<F, S>(
+    logger: Logger,
+    port: u16,
+    handler: F,
+) -> Result<ServerHandle, anyhow::Error>
+where
+    F: Fn(Request<Incoming>) -> S + Send + Clone + 'static,
+    S: Future<Output = GraphQLResult> + Send + 'static,
+{
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = TcpListener::bind(addr).await?;
+    let accepting = Arc::new(AtomicBool::new(false));
+    let accepting2 = accepting.cheap_clone();
+    let handle = crate::spawn(async move {
+        accepting2.store(true, std::sync::atomic::Ordering::SeqCst);
+        loop {
+            let (stream, _) = match listener.accept().await {
+                Ok(res) => res,
+                Err(e) => {
+                    error!(logger, "Error accepting connection"; "error" => e.to_string());
+                    continue;
+                }
+            };
+
+            // Use an adapter to access something implementing `tokio::io` traits as if they implement
+            // `hyper::rt` IO traits.
+            let io = TokioIo::new(stream);
+
+            let handler = handler.clone();
+            // Spawn a tokio task to serve multiple connections concurrently
+            tokio::task::spawn(async move {
+                let new_service = service_fn(handler);
+                // Finally, we bind the incoming connection to our `hello` service
+                http1::Builder::new()
+                    // `service_fn` converts our function in a `Service`
+                    .serve_connection(io, new_service)
+                    .await
+            });
+        }
+    });
+    Ok(ServerHandle { handle, accepting })
+}

server/http/src/server.rs
@@ -1,24 +1,19 @@
-use std::net::{Ipv4Addr, SocketAddrV4};
 use std::sync::Arc;
 
-use futures::future::Future;
 use graph::anyhow;
+use graph::cheap_clone::CheapClone;
+use graph::components::server::server::{start, ServerHandle};
 use graph::log::factory::{ComponentLoggerConfig, ElasticComponentLoggerConfig};
-use graph::prelude::TryFutureExt;
 use graph::slog::{error, info};
-use hyper::service::make_service_fn;
-use hyper::Server;
 
 use crate::service::GraphQLService;
-use graph::prelude::{
-    futures03, thiserror, thiserror::Error, GraphQlRunner, Logger, LoggerFactory, NodeId,
-};
+use graph::prelude::{thiserror, thiserror::Error, GraphQlRunner, Logger, LoggerFactory, NodeId};
 
 /// Errors that may occur when starting the server.
 #[derive(Debug, Error)]
 pub enum GraphQLServeError {
     #[error("Bind error: {0}")]
-    BindError(#[from] hyper::Error),
+    BindError(#[from] graph::hyper::Error),
 }
 
 /// A GraphQL server based on Hyper.
@@ -46,11 +41,7 @@ impl<Q: GraphQlRunner> GraphQLServer<Q> {
         }
     }
 
-    pub fn serve(
-        &mut self,
-        port: u16,
-        ws_port: u16,
-    ) -> Result<Box<dyn Future<Item = (), Error = ()> + Send>, GraphQLServeError> {
+    pub async fn serve(&self, port: u16, ws_port: u16) -> Result<ServerHandle, anyhow::Error> {
         let logger = self.logger.clone();
 
         info!(
@@ -58,29 +49,20 @@ impl<Q: GraphQlRunner> GraphQLServer<Q> {
             "Starting GraphQL HTTP server at: http://localhost:{}", port
         );
 
-        let addr = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), port);
-
-        // On every incoming request, launch a new GraphQL service that writes
-        // incoming queries to the query sink.
-        let logger_for_service = self.logger.clone();
         let graphql_runner = self.graphql_runner.clone();
         let node_id = self.node_id.clone();
-        let new_service = make_service_fn(move |_| {
-            let graphql_service = GraphQLService::new(
-                logger_for_service.clone(),
-                graphql_runner.clone(),
-                ws_port,
-                node_id.clone(),
-            );
 
-            futures03::future::ok::<_, anyhow::Error>(graphql_service)
-        });
+        let service = Arc::new(GraphQLService::new(
+            logger.clone(),
+            graphql_runner,
+            ws_port,
+            node_id,
+        ));
 
-        // Create a task to run the server and handle HTTP requests
-        let task = Server::try_bind(&addr.into())?
-            .serve(new_service)
-            .map_err(move |e| error!(logger, "Server error"; "error" => format!("{}", e)));
-
-        Ok(Box::new(task.compat()))
+        start(logger, port, move |req| {
+            let service = service.cheap_clone();
+            async move { Ok::<_, _>(service.cheap_clone().call(req).await) }
+        })
+        .await
     }
 }

server/index-node/src/server.rs
@@ -1,8 +1,11 @@
-use hyper::service::make_service_fn;
-use hyper::Server;
-use std::net::{Ipv4Addr, SocketAddrV4};
-
-use graph::{blockchain::BlockchainMap, components::store::Store, prelude::*};
+use graph::{
+    blockchain::BlockchainMap,
+    components::{
+        server::server::{start, ServerHandle},
+        store::Store,
+    },
+    prelude::*,
+};
 
 use crate::service::IndexNodeService;
 use thiserror::Error;
@@ -11,7 +14,7 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 pub enum IndexNodeServeError {
     #[error("Bind error: {0}")]
-    BindError(#[from] hyper::Error),
+    BindError(#[from] graph::hyper::Error),
 }
 
 /// A GraphQL server based on Hyper.
@@ -54,10 +57,7 @@ where
         }
     }
 
-    pub fn serve(
-        &mut self,
-        port: u16,
-    ) -> Result<Box<dyn Future<Item = (), Error = ()> + Send>, IndexNodeServeError> {
+    pub async fn serve(&mut self, port: u16) -> Result<ServerHandle, anyhow::Error> {
         let logger = self.logger.clone();
 
         info!(
@@ -65,28 +65,23 @@ where
             "Starting index node server at: http://localhost:{}", port
         );
 
-        let addr = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), port);
-
         // On every incoming request, launch a new GraphQL service that writes
         // incoming queries to the query sink.
         let logger_for_service = self.logger.clone();
         let graphql_runner = self.graphql_runner.clone();
         let store = self.store.clone();
-        let service = IndexNodeService::new(
+        let service = Arc::new(IndexNodeService::new(
             logger_for_service.clone(),
             self.blockchain_map.clone(),
             graphql_runner,
             store,
             self.link_resolver.clone(),
-        );
-        let new_service =
-            make_service_fn(move |_| futures03::future::ok::<_, Error>(service.clone()));
+        ));
 
-        // Create a task to run the server and handle HTTP requests
-        let task = Server::try_bind(&addr.into())?
-            .serve(new_service)
-            .map_err(move |e| error!(logger, "Server error"; "error" => format!("{}", e)));
-
-        Ok(Box::new(task.compat()))
+        start(logger_for_service.clone(), port, move |req| {
+            let service = service.clone();
+            async move { Ok::<_, _>(service.call(req).await) }
+        })
+        .await
     }
 }

graph/Cargo.toml
@@ -25,6 +25,9 @@ reqwest = { version = "0.11.18", features = ["json", "stream", "multipart"] }
 ethabi = "17.2"
 hex = "0.4.3"
 http = "0.2.3"
+hyper = { version = "1", features = ["full"] }
+http-body-util = "0.1"
+hyper-util = { version = "0.1", features = ["full"] }
 futures = { package = "futures", version = "0.1.31" }
 lru_time_cache = "0.11"
 graphql-parser = "0.4.0"

graph/src/components/server/mod.rs
@@ -6,3 +6,5 @@ pub mod subscription;
 
 /// Component for the index node server.
 pub mod index_node;
+
+pub mod server;

graph/src/components/server/query.rs
@@ -1,8 +1,15 @@
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::Response;
+
 use crate::data::query::QueryError;
 use std::error::Error;
 use std::fmt;
 
 use crate::components::store::StoreError;
+
+pub type GraphQLResponse = Response<Full<Bytes>>;
+pub type GraphQLResult = Result<GraphQLResponse, GraphQLServerError>;
 
 /// Errors that can occur while processing incoming requests.
 #[derive(Debug)]

commit dd9bf52ba0501c610640bf1ef3f8ad6677c331ea

graph, server: Reinstitute Graph-Attestable header
Commit f06c6061 mistakenly dropped several HTTP headers from GraphQL
responses, including the Graph-Attestable header. This commit adds them
back and also adds a test for their presence.

graph/src/data/query/result.rs
@@ -5,7 +5,10 @@ use crate::components::server::query::ServerResponse;
 use crate::data::value::Object;
 use crate::prelude::{r, CacheWeight, DeploymentHash};
 use http_body_util::Full;
-use hyper::header::{ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE};
+use hyper::header::{
+    ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS, ACCESS_CONTROL_ALLOW_ORIGIN,
+    CONTENT_TYPE,
+};
 use hyper::Response;
 use serde::ser::*;
 use serde::Serialize;
@@ -200,10 +203,15 @@ impl QueryResults {
 
     pub fn as_http_response(&self) -> ServerResponse {
         let json = serde_json::to_string(&self).unwrap();
+        let attestable = self.results.iter().all(|r| r.is_attestable());
         Response::builder()
             .status(200)
             .header(ACCESS_CONTROL_ALLOW_ORIGIN, "*")
             .header(CONTENT_TYPE, "application/json")
+            .header(ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, User-Agent")
+            .header(ACCESS_CONTROL_ALLOW_METHODS, "GET, OPTIONS, POST")
+            .header(CONTENT_TYPE, "application/json")
+            .header("Graph-Attestable", attestable.to_string())
             .body(Full::from(json))
             .unwrap()
     }

server/http/tests/server.rs
@@ -82,6 +82,7 @@ mod test {
 
     use super::*;
     use graph::http::header::{ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE};
+    use graph::hyper::header::{ACCESS_CONTROL_ALLOW_HEADERS, ACCESS_CONTROL_ALLOW_METHODS};
     use graph::prelude::reqwest::{Client, Response};
 
     lazy_static! {
@@ -91,7 +92,7 @@ mod test {
     pub async fn assert_successful_response(
         response: Response,
     ) -> serde_json::Map<String, serde_json::Value> {
-        assert_expected_headers(&response);
+        assert_expected_headers(&response, true);
         let body = response.bytes().await.unwrap().to_vec();
         let json: serde_json::Value =
             serde_json::from_slice(&body).expect("GraphQL response is not valid JSON");
@@ -111,7 +112,7 @@ mod test {
         graphql_response: bool,
     ) -> Vec<serde_json::Value> {
         assert_eq!(response.status(), expected_status);
-        assert_expected_headers(&response);
+        assert_expected_headers(&response, false);
         let body = response.bytes().await.unwrap().to_vec();
         let body = String::from_utf8(body).unwrap();
 
@@ -133,14 +134,35 @@ mod test {
     }
 
     #[track_caller]
-    pub fn assert_expected_headers(response: &Response) {
-        assert_eq!(
-            response
-                .headers()
-                .get(ACCESS_CONTROL_ALLOW_ORIGIN)
-                .expect("Missing CORS Header"),
-            &"*"
-        );
+    pub fn assert_expected_headers(response: &Response, success: bool) {
+        #[track_caller]
+        fn assert_header(response: &Response, header: &str, value: &str) {
+            let hdrs = response.headers();
+            let value = Some(value.parse().unwrap());
+            assert_eq!(
+                value.as_ref(),
+                hdrs.get(header),
+                "Header {} has unexpected value",
+                header
+            );
+        }
+
+        assert_header(response, ACCESS_CONTROL_ALLOW_ORIGIN.as_str(), "*");
+        if success {
+            assert_header(
+                response,
+                ACCESS_CONTROL_ALLOW_HEADERS.as_str(),
+                "Content-Type, User-Agent",
+            );
+            assert_header(
+                response,
+                ACCESS_CONTROL_ALLOW_METHODS.as_str(),
+                "GET, OPTIONS, POST",
+            );
+            assert_header(response, CONTENT_TYPE.as_str(), "application/json");
+
+            assert_header(response, "Graph-Attestable", "true");
+        }
     }
 
     #[tokio::test]