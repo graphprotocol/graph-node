a33850b19722a730d2555f9a5d60fcaddc9e384a
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-04-16 12:42:10 -0400

    fix: Repair missing sudo

--- build-install.sh
+++ build-install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p /usr/local/bin
 cargo build --release --bins -j 1
-cp target/release/graph-node target/release/graphman /usr/local/bin/
+sudo cp target/release/graph-node target/release/graphman /usr/local/bin/
