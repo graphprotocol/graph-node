16f9ad13670b8bc0e7f5d2f499b718375238e319
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-04-19 11:40:29 -0400

    fix: Test new archive node

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+export GRAPH_LOG=info
+export GRAPH_ALLOW_NON_DETERMINISTIC_IPFS=true
+export GRAPH_ALLOW_NON_DETERMINISTIC_FULLTEXT_SEARCH=true
+
+export NODE="34.141.208.199"
+# export NODE="127.0.0.1"
+# export NODE="34.147.84.221"
+export NODE=34.32.175.76
+
+exec cargo run \
+  --bin=graph-node \
+  --package=graph-node \
+  --profile=dev \
+  -- \
+  --node-id default \
+  --postgres-url "postgresql://graph-node:let-me-in@127.0.0.1:5432/graph-node" \
+  --ethereum-rpc "mainnet:http://$NODE:8545" \
+  --ipfs "localhost:5001" 2>&1
