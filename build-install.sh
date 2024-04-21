f71bd6662127a4522f4327ee26caa4fac7a547e9
Andreas Richter <708186+richtera@users.noreply.github.com>
2024-04-20 20:24:18 -0400

    fix: Add upgrade script

--- build-install.sh
+++ build-install.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 mkdir -p /usr/local/bin
 cargo build --release --bins -j 1
+HAS_SERVICE=
+if which systemctl
+then
+    HAS_SERVICE=1
+fi
+if [ -n "$HAS_SERVICE" ]
+then
+    sudo systemctl stop graph
+fi
 sudo cp target/release/graph-node target/release/graphman /usr/local/bin/
+if [ -n "$HAS_SERVICE" ]
+then
+    sudo systemctl start graph
+fi
