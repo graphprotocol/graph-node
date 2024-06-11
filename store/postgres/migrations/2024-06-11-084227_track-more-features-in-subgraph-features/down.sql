0426f3c693586dc2e82046ae52137ffa59af08be
incrypto32 <vpkrishnanand@gmail.com>
2024-06-11 16:55:07 +0530

    graph: Track bytes ids, aggregrations and immutable entities in subgraph features table

--- store/postgres/migrations/2024-06-11-084227_track-more-features-in-subgraph-features/down.sql
+++ store/postgres/migrations/2024-06-11-084227_track-more-features-in-subgraph-features/down.sql
@@ -1,2 +1,5 @@
 ALTER TABLE subgraphs.subgraph_features
-  DROP COLUMN IF EXISTS has_declared_calls;
+  DROP COLUMN IF EXISTS has_declared_calls,
+  DROP COLUMN IF EXISTS has_bytes_as_ids,
+  DROP COLUMN IF EXISTS has_aggregations,
+  DROP COLUMN IF EXISTS immutable_entities;
