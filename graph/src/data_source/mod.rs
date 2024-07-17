fad68d7875ff30ff66df8891a4d28f4765d19a54
incrypto32 <vpkrishnanand@gmail.com>
2024-07-17 14:57:37 +0530

    graph,core: Add subgraph_hosts to RuntimeHostBuilder

--- graph/src/data_source/mod.rs
+++ graph/src/data_source/mod.rs
@@ -257,11 +257,15 @@ impl<C: Blockchain> DataSource<C> {
             (Self::Offchain(ds), TriggerData::Offchain(trigger)) => {
                 Ok(ds.match_and_decode(trigger))
             }
+            (Self::Subgraph(ds), TriggerData::Subgraph(trigger)) => {
+                Ok(ds.match_and_decode(block, trigger))
+            }
             (Self::Onchain(_), TriggerData::Offchain(_))
             | (Self::Offchain(_), TriggerData::Onchain(_))
             | (Self::Onchain(_), TriggerData::Subgraph(_))
-            | (Self::Offchain(_), TriggerData::Subgraph(_)) => Ok(None),
-            (Self::Subgraph(_), _) => todo!(), // TODO(krishna)
+            | (Self::Offchain(_), TriggerData::Subgraph(_))
+            | (Self::Subgraph(_), TriggerData::Onchain(_))
+            | (Self::Subgraph(_), TriggerData::Offchain(_)) => Ok(None),
         }
     }
 
@@ -585,7 +589,7 @@ impl<C: Blockchain> MappingTrigger<C> {
         match self {
             Self::Onchain(trigger) => Some(trigger),
             Self::Offchain(_) => None,
-            Self::Subgraph(_) => todo!(), // TODO(krishna)
+            Self::Subgraph(_) => None, // TODO(krishna)
         }
     }
 }
