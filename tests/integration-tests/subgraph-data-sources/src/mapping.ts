22e9ea87a9f11911d1d387728fed44f201d60116
incrypto32 <vpkrishnanand@gmail.com>
2024-07-24 12:57:09 +0530

    chain/ethereum: Mock implementation of subgraph_triggers for ethereum

--- tests/integration-tests/subgraph-data-sources/src/mapping.ts
+++ tests/integration-tests/subgraph-data-sources/src/mapping.ts
@@ -1,6 +1,14 @@
 import { Entity, log } from '@graphprotocol/graph-ts';
+import { Block } from '../generated/schema';
 
 export function handleEntity(blockEntity: Entity): void {
-  let blockNumberString = blockEntity.getBigInt('number').toString();
-  log.info('===> Block: {}', [blockNumberString]);
+  let blockNumber = blockEntity.getBigInt('number');
+  let blockHash = blockEntity.getBytes('hash');
+
+  log.info('Block number: {}', [blockNumber.toString()]);
+
+  let block = new Block(blockHash);
+  block.number = blockNumber;
+  block.hash = blockHash;
+  block.save();
 }
