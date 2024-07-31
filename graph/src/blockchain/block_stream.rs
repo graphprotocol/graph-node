cdbdab2f69ce8a1ea3c4347a4b7e840c5c907cf9
incrypto32 <vpkrishnanand@gmail.com>
2024-07-31 08:14:02 +0530

    graph: subgraph_triggers to take block_number hashset

--- graph/src/blockchain/block_stream.rs
+++ graph/src/blockchain/block_stream.rs
@@ -369,9 +369,16 @@ impl<C: Blockchain> TriggersAdapterWrapper<C> {
 
                             let br: Range<BlockNumber> = from..to;
                             let entities = store.get_range(&et, br)?;
+                            let block_numbers = entities
+                                .iter()
+                                .map(|(bn, _)| bn)
+                                .cloned()
+                                .collect::<HashSet<_>>();
+
                             return self
                                 .subgraph_triggers(
                                     Logger::root(slog::Discard, o!()),
+                                    block_numbers,
                                     from,
                                     to,
                                     filter,
@@ -412,7 +419,8 @@ impl<C: Blockchain> TriggersAdapterWrapper<C> {
     async fn subgraph_triggers(
         &self,
         logger: Logger,
-        from: BlockNumber,
+        block_numbers: HashSet<BlockNumber>,
+        _from: BlockNumber,
         to: BlockNumber,
         filter: &Arc<TriggerFilterWrapper<C>>,
         entities: BTreeMap<BlockNumber, Vec<Entity>>,
@@ -421,7 +429,7 @@ impl<C: Blockchain> TriggersAdapterWrapper<C> {
         let adapter = self.adapter.clone();
         let first_filter = filter.subgraph_filter.first().unwrap();
         let blocks = adapter
-            .load_blocks_by_numbers(logger, HashSet::from_iter(from..to))
+            .load_blocks_by_numbers(logger, block_numbers)
             .await?
             .into_iter()
             .map(|block| {
