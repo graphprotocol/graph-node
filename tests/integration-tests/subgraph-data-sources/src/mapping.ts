87d54ae9b3f8e5beca82e63759e1726244f94208
incrypto32 <vpkrishnanand@gmail.com>
2024-08-16 16:57:36 +0530

    tests for multiple entities

--- tests/integration-tests/subgraph-data-sources/src/mapping.ts
+++ tests/integration-tests/subgraph-data-sources/src/mapping.ts
@@ -4,10 +4,11 @@ import { MirrorBlock } from '../generated/schema';
 export function handleEntity(blockEntity: Entity): void {
   let blockNumber = blockEntity.getBigInt('number');
   let blockHash = blockEntity.getBytes('hash');
+  let id = blockEntity.getString('id');
 
   log.info('Block number: {}', [blockNumber.toString()]);
 
-  let block = new MirrorBlock(blockHash);
+  let block = new MirrorBlock(id);
   block.number = blockNumber;
   block.hash = blockHash;
   block.save();
