59f683e79d31d632d3b4aedd3898634749d5bd28
incrypto32 <vpkrishnanand@gmail.com>
2024-09-09 16:18:01 +0530

    graph, runtime: Support subgraph entity operation detection in composed subgraphs

--- tests/integration-tests/subgraph-data-sources/src/mapping.ts
+++ tests/integration-tests/subgraph-data-sources/src/mapping.ts
@@ -1,7 +1,17 @@
 import { Entity, log } from '@graphprotocol/graph-ts';
 import { MirrorBlock } from '../generated/schema';
 
-export function handleEntity(blockEntity: Entity): void {
+export class EntityTrigger {
+  constructor(
+    public entityOp: u32,
+    public entityType: string,
+    public entity: Entity,
+    public vid: i64,
+  ) {}
+}
+
+export function handleEntity(trigger: EntityTrigger): void {
+  let blockEntity = trigger.entity;
   let blockNumber = blockEntity.getBigInt('number');
   let blockHash = blockEntity.getBytes('hash');
   let id = blockEntity.getString('id');
--- tests/runner-tests/subgraph-data-sources/src/mapping.ts
+++ tests/runner-tests/subgraph-data-sources/src/mapping.ts
@@ -1,6 +1,35 @@
 import { Entity, log } from '@graphprotocol/graph-ts';
 
-export function handleBlock(content: Entity): void {
-  let stringContent = content.getString('val');
+export const SubgraphEntityOpCreate: u32 = 0;
+export const SubgraphEntityOpModify: u32 = 1;
+export const SubgraphEntityOpDelete: u32 = 2;
+
+export class EntityTrigger {
+  constructor(
+    public entityOp: u32,
+    public entityType: string,
+    public entity: Entity,
+    public vid: i64,
+  ) {}
+}
+
+export function handleBlock(content: EntityTrigger): void {
+  let stringContent = content.entity.getString('val');
   log.info('Content: {}', [stringContent]);
+  log.info('EntityOp: {}', [content.entityOp.toString()]);
+
+  switch (content.entityOp) {
+    case SubgraphEntityOpCreate: {
+      log.info('Entity created: {}', [content.entityType]);
+      break
+    }
+    case SubgraphEntityOpModify: {
+      log.info('Entity modified: {}', [content.entityType]);
+      break;
+    }
+    case SubgraphEntityOpDelete: {
+      log.info('Entity deleted: {}', [content.entityType]);
+      break;
+    }
+  }
 }
