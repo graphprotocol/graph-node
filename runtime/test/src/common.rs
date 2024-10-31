fef9640c4f9048f3085a7baeaf09b0cc8f2c3a2a
incrypto32 <vpkrishnanand@gmail.com>
2024-10-31 14:35:26 +0400

    graph: enable ethereum host fns for subgraph datasource

--- runtime/test/src/common.rs
+++ runtime/test/src/common.rs
@@ -3,13 +3,12 @@ use graph::blockchain::BlockTime;
 use graph::components::store::DeploymentLocator;
 use graph::data::subgraph::*;
 use graph::data_source;
+use graph::data_source::common::MappingABI;
 use graph::env::EnvVars;
 use graph::ipfs_client::IpfsClient;
 use graph::log;
 use graph::prelude::*;
-use graph_chain_ethereum::{
-    Chain, DataSource, DataSourceTemplate, Mapping, MappingABI, TemplateSource,
-};
+use graph_chain_ethereum::{Chain, DataSource, DataSourceTemplate, Mapping, TemplateSource};
 use graph_runtime_wasm::host_exports::DataSourceDetails;
 use graph_runtime_wasm::{HostExports, MappingContext};
 use semver::Version;
