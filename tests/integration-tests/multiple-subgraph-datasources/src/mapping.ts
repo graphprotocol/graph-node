e162d8c7c798fa9d11a6cbaf32f7db034213771b
incrypto32 <vpkrishnanand@gmail.com>
2025-01-06 18:04:46 +0530

    update multiple subgraph datasources integration test

--- tests/integration-tests/multiple-subgraph-datasources/src/mapping.ts
+++ tests/integration-tests/multiple-subgraph-datasources/src/mapping.ts
@@ -1,13 +1,14 @@
-import { dataSource, EntityTrigger } from '@graphprotocol/graph-ts'
+import { dataSource, EntityTrigger, log } from '@graphprotocol/graph-ts'
 import { AggregatedData } from '../generated/schema'
-import { SourceAData } from '../generated/subgraph-QmU35YwAsv59gJxhejp3qqUrSFMaoBXuskNLX7wJHNUzyA'
-import { SourceBData } from '../generated/subgraph-QmXjHeC7j5iWF49oEngV3tqFrHvb4NhkFpAdJYVJ1SFPNk'
+import { SourceAData } from '../generated/subgraph-QmcYeEnoRzLgrwwW1StY7wTAGSS8Qt5G78RYAHHyLJWYBy'
+import { SourceBData } from '../generated/subgraph-QmetPUZD9SNGjdpkgefBGe8uWEcLYxggVaixkYKd22AKr6'
 
 export function handleSourceAData(data: EntityTrigger<SourceAData>): void {
-  let aggregated = AggregatedData.load('1')
+  let aggregated = AggregatedData.load(data.data.id)
   if (!aggregated) {
-    aggregated = new AggregatedData('1')
+    aggregated = new AggregatedData(data.data.id)
     aggregated.sourceA = data.data.data
+    aggregated.first = 'sourceA'
   } else {
     aggregated.sourceA = data.data.data
   }
@@ -15,10 +16,11 @@ export function handleSourceAData(data: EntityTrigger<SourceAData>): void {
 }
 
 export function handleSourceBData(data: EntityTrigger<SourceBData>): void {
-  let aggregated = AggregatedData.load('1')
+  let aggregated = AggregatedData.load(data.data.id)
   if (!aggregated) {
-    aggregated = new AggregatedData('1')
+    aggregated = new AggregatedData(data.data.id)
     aggregated.sourceB = data.data.data
+    aggregated.first = 'sourceB'
   } else {
     aggregated.sourceB = data.data.data
   }
--- tests/integration-tests/source-subgraph-b/src/mapping.ts
+++ tests/integration-tests/source-subgraph-b/src/mapping.ts
@@ -2,7 +2,7 @@ import { ethereum } from '@graphprotocol/graph-ts'
 import { SourceBData } from '../generated/schema'
 
 export function handleBlock(block: ethereum.Block): void {
-  let entity = new SourceBData('1')
+  let entity = new SourceBData(block.number.toString())
   entity.data = 'from source B'
   entity.blockNumber = block.number
   entity.save()
