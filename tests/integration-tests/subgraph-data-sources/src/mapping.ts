cf4951c3009575b719852d3391172f4d15e31706
incrypto32 <vpkrishnanand@gmail.com>
2025-01-31 16:44:14 +0400

    Subgraph Composition: Entity Ops Detection in Handlers

--- tests/integration-tests/subgraph-data-sources/src/mapping.ts
+++ tests/integration-tests/subgraph-data-sources/src/mapping.ts
@@ -1,15 +1,46 @@
-import { Entity, log } from '@graphprotocol/graph-ts';
+import { Entity, log, store } from '@graphprotocol/graph-ts';
 import { MirrorBlock } from '../generated/schema';
 
-export function handleEntity(blockEntity: Entity): void {
+export class EntityTrigger {
+  constructor(
+    public entityOp: u32,
+    public entityType: string,
+    public entity: Entity,
+    public vid: i64,
+  ) {}
+}
+
+export function handleEntity(trigger: EntityTrigger): void {
+  let blockEntity = trigger.entity;
   let blockNumber = blockEntity.getBigInt('number');
   let blockHash = blockEntity.getBytes('hash');
+  let testMessage = blockEntity.get('testMessage');
   let id = blockEntity.getString('id');
 
   log.info('Block number: {}', [blockNumber.toString()]);
 
-  let block = new MirrorBlock(id);
+  if (trigger.entityOp == 2) {
+    log.info('Removing block entity with id: {}', [id]);
+    store.remove('MirrorBlock', id);
+    return;
+  }
+
+  let block = loadOrCreateMirrorBlock(id);
   block.number = blockNumber;
   block.hash = blockHash;
+  if (testMessage) {
+    block.testMessage = testMessage.toString();
+  }
+
   block.save();
+}
+
+export function loadOrCreateMirrorBlock(id: string): MirrorBlock {
+  let block = MirrorBlock.load(id);
+  if (!block) {
+    log.info('Creating new block entity with id: {}', [id]);
+    block = new MirrorBlock(id);
+  }
+
+  return block;
 }
