818624976ce16df1d346185064d4b48bae8fc7aa
Shiyas Mohammed <shiyasmohd.official@gmail.com>
2025-09-03 13:07:18 +0530

    server: handle empty arr in indexing status query

--- server/index-node/src/resolver.rs
+++ server/index-node/src/resolver.rs
@@ -136,6 +136,10 @@ impl<S: Store> IndexNodeResolver<S> {
             })
             .unwrap_or_else(Vec::new);
 
+        if deployments.is_empty() {
+            return Ok(r::Value::List(vec![]));
+        }
+
         let infos = self
             .store
             .status(status::Filter::Deployments(deployments))?;
