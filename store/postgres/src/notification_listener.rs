25caea7cd44edeadef1515f41eb3d2fefea2bdda
David Lutterkort <lutter@watzmann.net>
2025-09-17 09:43:55 -0700

    store: Log more details about notifications

--- store/postgres/src/notification_listener.rs
+++ store/postgres/src/notification_listener.rs
@@ -222,6 +222,9 @@ impl NotificationListener {
                     // longer than 500ms for new notifications to arrive,
                     // but limit the size of each batch to 128 to guarantee
                     // progress on a busy system
+                    if store_events {
+                        debug!(logger, "Waiting for store event"; "queue_size" => queue_size);
+                    }
                     let notifications: Vec<_> = conn
                         .notifications()
                         .timeout_iter(Duration::from_millis(500))
@@ -243,7 +246,9 @@ impl NotificationListener {
                         })
                         .filter(|notification| notification.channel() == channel_name.0)
                         .collect();
-
+                    if store_events {
+                        debug!(logger, "Received store events"; "num_events" => notifications.len(), "queue_size" => conn.notifications().len());
+                    }
                     // Read notifications until there hasn't been one for 500ms
                     for notification in notifications {
                         // Terminate the thread if desired
@@ -254,6 +259,9 @@ impl NotificationListener {
                         match JsonNotification::parse(&notification, &mut conn) {
                             Ok(json_notification) => {
                                 let timeout = ENV_VARS.store.notification_broadcast_timeout;
+                                if store_events {
+                                    debug!(logger, "Broadcasting store event"; "notification" => format!("{:?}", json_notification));
+                                }
                                 match graph::block_on(
                                     sender.send_timeout(json_notification, timeout),
                                 ) {
