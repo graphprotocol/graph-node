fa24b05595c86b1b7a3ab1385da4cf2a0ca8992d
David Lutterkort <lutter@watzmann.net>
2025-10-13 23:00:36 +0200

    core: Use async connection in graphman::src::deployment

--- store/postgres/src/lib.rs
+++ store/postgres/src/lib.rs
@@ -63,8 +63,8 @@ pub use self::detail::DeploymentDetail;
 pub use self::jobs::register as register_jobs;
 pub use self::notification_listener::NotificationSender;
 pub use self::pool::{
-    AsyncConnection, ConnectionPool, ForeignServer, PgConnection, PoolCoordinator, PoolRole,
-    ScopedFutureExt,
+    AsyncConnection, AsyncPgConnection, ConnectionPool, ForeignServer, PgConnection,
+    PoolCoordinator, PoolRole, ScopedFutureExt,
 };
 pub use self::primary::{db_version, UnusedDeployment};
 pub use self::store::Store;
