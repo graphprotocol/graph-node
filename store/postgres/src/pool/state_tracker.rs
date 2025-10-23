f188afe226e856507dbc586ffcc09e379893ce29
David Lutterkort <lutter@watzmann.net>
2025-10-23 12:49:44 +0200

    store: Replicate the metrics that EventHandler used to collect with deadpool

--- store/postgres/src/pool/state_tracker.rs
+++ store/postgres/src/pool/state_tracker.rs
@@ -1,7 +1,7 @@
 //! Event/error handlers for our r2d2 pools
 
 use deadpool::managed::Hook;
-use diesel::r2d2::{self, event as e, HandleEvent};
+use diesel::r2d2;
 
 use diesel_async::pooled_connection::AsyncDieselConnectionManager;
 use diesel_async::AsyncPgConnection;
@@ -9,7 +9,9 @@ use graph::prelude::error;
 use graph::prelude::Counter;
 use graph::prelude::Gauge;
 use graph::prelude::MetricsRegistry;
+use graph::prelude::MovingStats;
 use graph::prelude::PoolWaitStats;
+use graph::slog::info;
 use graph::slog::Logger;
 
 use std::collections::HashMap;
@@ -17,30 +19,48 @@ use std::fmt;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
+use std::sync::RwLock;
 use std::time::Duration;
+
+use crate::pool::AsyncPool;
 
 /// Track whether a database is available or not using the event and error
 /// handlers from this module. The pool must be set up with these handlers
 /// when it is created
 #[derive(Clone)]
 pub(super) struct StateTracker {
+    logger: Logger,
     available: Arc<AtomicBool>,
     ignore_timeout: Arc<AtomicBool>,
 }
 
 impl StateTracker {
-    pub(super) fn new() -> Self {
+    pub(super) fn new(logger: Logger) -> Self {
         Self {
+            logger,
             available: Arc::new(AtomicBool::new(true)),
             ignore_timeout: Arc::new(AtomicBool::new(false)),
         }
     }
 
     pub(super) fn mark_available(&self) {
+        if !self.is_available() {
+            info!(self.logger, "Conection checkout"; "event" => "available");
+        }
         self.available.store(true, Ordering::Relaxed);
     }
 
-    pub(super) fn mark_unavailable(&self) {
+    pub(super) fn mark_unavailable(&self, waited: Duration) {
+        if self.is_available() {
+            if waited.as_nanos() > 0 {
+                error!(self.logger, "Connection checkout timed out";
+                   "event" => "unavailable",
+                   "wait_ms" => waited.as_millis()
+                )
+            } else {
+                error!(self.logger, "Connection checkout"; "event" => "unavailable");
+            }
+        }
         self.available.store(false, Ordering::Relaxed);
     }
 
@@ -48,10 +68,12 @@ impl StateTracker {
         self.available.load(Ordering::Relaxed)
     }
 
-    fn timeout_is_ignored(&self) -> bool {
+    pub(super) fn timeout_is_ignored(&self) -> bool {
         self.ignore_timeout.load(Ordering::Relaxed)
     }
 
+    /// Run the given async function while ignoring timeouts; if `f` causes
+    /// a timeout, the database is not marked as unavailable
     pub(super) async fn ignore_timeout<F, R>(&self, f: F) -> R
     where
         F: AsyncFnOnce() -> R,
@@ -62,6 +84,7 @@ impl StateTracker {
         res
     }
 
+    /// Return a deadpool hook that marks the database as available
     pub(super) fn mark_available_hook(
         &self,
     ) -> Hook<AsyncDieselConnectionManager<AsyncPgConnection>> {
@@ -123,104 +146,7 @@ impl r2d2::HandleError<r2d2::Error> for ErrorHandler {
         if self.state_tracker.is_available() {
             error!(self.logger, "Postgres connection error"; "error" => msg);
         }
-        self.state_tracker.mark_unavailable();
-    }
-}
-
-#[derive(Clone)]
-pub(super) struct EventHandler {
-    logger: Logger,
-    count_gauge: Gauge,
-    wait_gauge: Gauge,
-    size_gauge: Gauge,
-    wait_stats: PoolWaitStats,
-    state_tracker: StateTracker,
-}
-
-impl EventHandler {
-    pub(super) fn new(
-        logger: Logger,
-        registry: Arc<MetricsRegistry>,
-        wait_stats: PoolWaitStats,
-        const_labels: HashMap<String, String>,
-        state_tracker: StateTracker,
-    ) -> Self {
-        let count_gauge = registry
-            .global_gauge(
-                "store_connection_checkout_count",
-                "The number of Postgres connections currently checked out",
-                const_labels.clone(),
-            )
-            .expect("failed to create `store_connection_checkout_count` counter");
-        let wait_gauge = registry
-            .global_gauge(
-                "store_connection_wait_time_ms",
-                "Average connection wait time",
-                const_labels.clone(),
-            )
-            .expect("failed to create `store_connection_wait_time_ms` counter");
-        let size_gauge = registry
-            .global_gauge(
-                "store_connection_pool_size_count",
-                "Overall size of the connection pool",
-                const_labels,
-            )
-            .expect("failed to create `store_connection_pool_size_count` counter");
-        EventHandler {
-            logger,
-            count_gauge,
-            wait_gauge,
-            wait_stats,
-            size_gauge,
-            state_tracker,
-        }
-    }
-
-    fn add_conn_wait_time(&self, duration: Duration) {
-        self.wait_stats
-            .write()
-            .unwrap()
-            .add_and_register(duration, &self.wait_gauge);
-    }
-}
-
-impl std::fmt::Debug for EventHandler {
-    fn fmt(&self, _f: &mut fmt::Formatter) -> fmt::Result {
-        fmt::Result::Ok(())
-    }
-}
-
-impl HandleEvent for EventHandler {
-    fn handle_acquire(&self, _: e::AcquireEvent) {
-        self.size_gauge.inc();
-        self.state_tracker.mark_available();
-    }
-
-    fn handle_release(&self, _: e::ReleaseEvent) {
-        self.size_gauge.dec();
-    }
-
-    fn handle_checkout(&self, event: e::CheckoutEvent) {
-        self.count_gauge.inc();
-        self.add_conn_wait_time(event.duration());
-        self.state_tracker.mark_available();
-    }
-
-    fn handle_timeout(&self, event: e::TimeoutEvent) {
-        if self.state_tracker.timeout_is_ignored() {
-            return;
-        }
-        self.add_conn_wait_time(event.timeout());
-        if self.state_tracker.is_available() {
-            error!(self.logger, "Connection checkout timed out";
-               "wait_ms" => event.timeout().as_millis()
-            )
-        }
-        self.state_tracker.mark_unavailable();
-    }
-
-    fn handle_checkin(&self, _: e::CheckinEvent) {
-        self.count_gauge.dec();
+        self.state_tracker.mark_unavailable(Duration::from_secs(0));
     }
 }
 
@@ -237,4 +163,63 @@ fn brief_error_msg(error: &dyn std::error::Error) -> String {
         .next()
         .unwrap_or("no error details provided")
         .to_string()
+}
+
+pub(crate) fn spawn_size_stat_collector(
+    pool: AsyncPool,
+    registry: &MetricsRegistry,
+    const_labels: HashMap<String, String>,
+) {
+    let count_gauge = registry
+        .global_gauge(
+            "store_connection_checkout_count",
+            "The number of Postgres connections currently checked out",
+            const_labels.clone(),
+        )
+        .expect("failed to create `store_connection_checkout_count` counter");
+    let size_gauge = registry
+        .global_gauge(
+            "store_connection_pool_size_count",
+            "Overall size of the connection pool",
+            const_labels,
+        )
+        .expect("failed to create `store_connection_pool_size_count` counter");
+    tokio::task::spawn(async move {
+        loop {
+            let status = pool.status();
+            count_gauge.set((status.size - status.available) as f64);
+            size_gauge.set(status.size as f64);
+            tokio::time::sleep(Duration::from_secs(15)).await;
+        }
+    });
+}
+
+pub(crate) struct WaitMeter {
+    wait_gauge: Gauge,
+    pub(crate) wait_stats: PoolWaitStats,
+}
+
+impl WaitMeter {
+    pub(crate) fn new(registry: &MetricsRegistry, const_labels: HashMap<String, String>) -> Self {
+        let wait_gauge = registry
+            .global_gauge(
+                "store_connection_wait_time_ms",
+                "Average connection wait time",
+                const_labels,
+            )
+            .expect("failed to create `store_connection_wait_time_ms` counter");
+        let wait_stats = Arc::new(RwLock::new(MovingStats::default()));
+
+        Self {
+            wait_gauge,
+            wait_stats,
+        }
+    }
+
+    pub(crate) fn add_conn_wait_time(&self, duration: Duration) {
+        self.wait_stats
+            .write()
+            .unwrap()
+            .add_and_register(duration, &self.wait_gauge);
+    }
 }
