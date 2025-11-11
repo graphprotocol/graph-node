d4e3cb7766fbf8b3ff1956d6037da0587b8959e7
Ion Suman <47307091+isum@users.noreply.github.com>
2025-11-11 16:46:21 +0200

    fix(graph): extend the list of common column aliases

--- graph/src/amp/common/mod.rs
+++ graph/src/amp/common/mod.rs
@@ -3,18 +3,22 @@ pub(super) mod column_aliases {
         "_block_num",   // Meta column present in all tables
         "block_num",    // Standard column in most raw tables
         "blockNum",     // Common alternative name
+        "blocknum",     // Common alternative name
         "block",        // Common alternative name
         "block_number", // Common alternative name
         "blockNumber",  // Common alternative name
+        "blocknumber",  // Common alternative name
     ];
     pub(in crate::amp) static BLOCK_HASH: &[&str] = &[
         "hash",       // Standard column in some raw tables
         "block_hash", // Standard column in most raw tables and common alternative name
         "blockHash",  // Common alternative name
+        "blockhash",  // Common alternative name
     ];
     pub(in crate::amp) static BLOCK_TIMESTAMP: &[&str] = &[
         "timestamp",       // Standard column in most raw tables
         "block_timestamp", // Common alternative name
         "blockTimestamp",  // Common alternative name
+        "blocktimestamp",  // Common alternative name
     ];
 }
